84812a8cf9d57d112398f4adaac0512b05fcb59c
Jörn Friedrich Dreyer <jfd@butonic.de>
2019-03-11 14:20:30 +0100

    log token strategy header config

--- cmd/revad/svcs/httpsvcs/handlers/auth/token/strategy/header/header.go
+++ cmd/revad/svcs/httpsvcs/handlers/auth/token/strategy/header/header.go
@@ -1,12 +1,17 @@
 package header
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/cernbox/reva/cmd/revad/svcs/httpsvcs/handlers/auth/token/registry"
 	"github.com/cernbox/reva/pkg/auth"
+	"github.com/cernbox/reva/pkg/log"
 	"github.com/mitchellh/mapstructure"
+	"github.com/pkg/errors"
 )
+
+var logger = log.New("token-strategy-header")
 
 func init() {
 	registry.Register("header", New)
@@ -19,10 +24,20 @@ type strategy struct {
 	header string
 }
 
+func parseConfig(m map[string]interface{}) (*config, error) {
+	c := &config{}
+	if err := mapstructure.Decode(m, c); err != nil {
+		logger.Error(context.Background(), errors.Wrap(err, "error decoding conf"))
+		return nil, err
+	}
+	logger.Println(context.Background(), "config: ", c)
+	return c, nil
+}
+
 // New returns a new auth strategy that checks for basic auth.
 func New(m map[string]interface{}) (auth.TokenStrategy, error) {
-	conf := &config{}
-	if err := mapstructure.Decode(m, conf); err != nil {
+	conf, err := parseConfig(m)
+	if err != nil {
 		return nil, err
 	}
 	return &strategy{header: conf.Header}, nil
