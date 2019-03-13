5c86a32e2ae0e844d79d9cde6b9ff76c8142b043
Jörn Friedrich Dreyer <jfd@butonic.de>
2019-03-13 13:22:15 +0100

    rudimentary recursive copy
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

--- cmd/revad/svcs/httpsvcs/ocdavsvc/copy.go
+++ cmd/revad/svcs/httpsvcs/ocdavsvc/copy.go
@@ -1,8 +1,11 @@
 package ocdavsvc
 
 import (
+	"context"
+	"io"
 	"net/http"
 	"net/url"
+	"path"
 	"strings"
 
 	rpcpb "github.com/cernbox/go-cs3apis/cs3/rpc"
@@ -56,60 +59,148 @@ func (s *svc) doCopy(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	dst := urlPath[len(baseURI):]
-
-	if overwrite == "F" {
-		dstStatReq := &storageproviderv0alphapb.StatRequest{Filename: dst}
-		dstStatRes, err := client.Stat(ctx, dstStatReq)
-		if err != nil {
-			logger.Error(ctx, err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		if dstStatRes.Status.Code == rpcpb.Code_CODE_OK {
-			logger.Println(ctx, "destination already exists: ", dst)
-			w.WriteHeader(http.StatusPreconditionFailed)
-			return
-		}
+	// check src exists
+	srcStatReq := &storageproviderv0alphapb.StatRequest{Filename: src}
+	srcStatRes, err := client.Stat(ctx, srcStatReq)
+	if err != nil {
+		logger.Error(ctx, err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 
-	w.WriteHeader(http.StatusNotImplemented)
-	return
-	/*
-		req := &storageproviderv0alphapb.CopyRequest{SourceFilename: src, TargetFilename: dst}
-		res, err := client.Copy(ctx, req)
+	if srcStatRes.Status.Code != rpcpb.Code_CODE_OK {
+		if srcStatRes.Status.Code == rpcpb.Code_CODE_NOT_FOUND {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		logger.Println(ctx, srcStatRes.Status)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	dst := urlPath[len(baseURI):]
+
+	// check dst exists
+	dstStatReq := &storageproviderv0alphapb.StatRequest{Filename: dst}
+	dstStatRes, err := client.Stat(ctx, dstStatReq)
+	if err != nil {
+		logger.Error(ctx, err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var successCode int
+	if dstStatRes.Status.Code == rpcpb.Code_CODE_OK {
+		successCode = http.StatusNoContent // 204 if target already existed
+	} else {
+		successCode = http.StatusCreated // 201 if new resource was created
+	}
+
+	if overwrite == "F" && successCode == http.StatusNoContent {
+		logger.Println(ctx, "destination already exists: ", dst)
+		w.WriteHeader(http.StatusPreconditionFailed)
+		return
+	}
+
+	err = descend(ctx, client, srcStatRes.Metadata, dst)
+	if err != nil {
+		logger.Error(ctx, err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(successCode)
+}
+
+func descend(ctx context.Context, client storageproviderv0alphapb.StorageProviderServiceClient, src *storageproviderv0alphapb.Metadata, dst string) error {
+	logger.Println(ctx, "descend src:", src, " dst:", dst)
+	if src.IsDir {
+		// create dir
+		createReq := &storageproviderv0alphapb.CreateDirectoryRequest{Filename: dst}
+		createRes, err := client.CreateDirectory(ctx, createReq)
+		if err != nil || createRes.Status.Code != rpcpb.Code_CODE_OK {
+			return err
+		}
+
+		// descend for children
+		listReq := &storageproviderv0alphapb.ListRequest{
+			Filename: src.Filename,
+		}
+		stream, err := client.List(ctx, listReq)
 		if err != nil {
-			logger.Error(ctx, err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
+			return err
 		}
 
-		if res.Status.Code != rpcpb.Code_CODE_OK {
-			logger.Println(ctx, res.Status)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
+		for {
+			res, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+
+			if err != nil || res.Status.Code != rpcpb.Code_CODE_OK {
+				return err
+			}
+
+			dst = path.Join(dst, path.Base(res.Metadata.Filename))
+			if res.Metadata.IsDir {
+				dst = dst + "/"
+			}
+			descend(ctx, client, res.Metadata, dst)
 		}
 
-		req2 := &storageproviderv0alphapb.StatRequest{Filename: dst}
-		res2, err := client.Stat(ctx, req2)
+	} else {
+		// copy file
+
+		readReq := &storageproviderv0alphapb.ReadRequest{Filename: src.Filename}
+		readStream, err := client.Read(ctx, readReq)
 		if err != nil {
-			logger.Error(ctx, err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
+			return err
 		}
 
-		if res2.Status.Code != rpcpb.Code_CODE_OK {
-			logger.Println(ctx, res2.Status)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
+		startReq := &storageproviderv0alphapb.StartWriteSessionRequest{}
+		writeSess, err := client.StartWriteSession(ctx, startReq)
+		if err != nil || writeSess.Status.Code != rpcpb.Code_CODE_OK {
+			return err
 		}
 
-		md := res2.Metadata
-		w.Header().Set("Content-Type", md.Mime)
-		w.Header().Set("ETag", md.Etag)
-		w.Header().Set("OC-FileId", md.Id)
-		w.Header().Set("OC-ETag", md.Etag)
-		w.WriteHeader(http.StatusCreated)
-	*/
+		sessID := writeSess.SessionId
+		logger.Build().Str("sessID", sessID).Msg(ctx, "got write session id")
+
+		writeStream, err := client.Write(ctx)
+		if err != nil {
+			return err
+		}
+
+		for {
+			res, err := readStream.Recv()
+			if err == io.EOF {
+				break
+			}
+
+			if err != nil || res.Status.Code != rpcpb.Code_CODE_OK {
+				return err
+			}
+
+			dc := res.DataChunk
+			if dc.Length > 0 {
+				req := &storageproviderv0alphapb.WriteRequest{Data: dc.Data, Length: dc.Length, SessionId: sessID, Offset: dc.Offset}
+				err = writeStream.Send(req)
+				if err != nil {
+					return err
+				}
+			}
+		}
+
+		closeRes, err := writeStream.CloseAndRecv()
+		if err != nil || closeRes.Status.Code != rpcpb.Code_CODE_OK {
+			return err
+		}
+
+		finishReq := &storageproviderv0alphapb.FinishWriteSessionRequest{Filename: dst, SessionId: sessID}
+		finishRes, err := client.FinishWriteSession(ctx, finishReq)
+		if err != nil || finishRes.Status.Code != rpcpb.Code_CODE_OK {
+			return err
+		}
+
+	}
+	return nil
 }
