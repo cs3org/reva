fec175dc1ee08bdc0d2a9916f7f1db5e86d3eba4
Hugo Gonzalez Labrador <github@hugo.labkode.com>
2019-04-17 09:00:51 +0200

    add license checker

---
+++ tools/check-license.go
@@ -0,0 +1,102 @@
+// Copyright 2018-2019 CERN
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// In applying this license, CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization 
+// or submit itself to any jurisdiction.
+
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"flag"
+	"regexp"
+	"fmt"
+)
+
+var fix = flag.Bool("fix", false, "add header if not present")
+
+var licenseText = `// Copyright 2018-2019 CERN
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// In applying this license, CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization 
+// or submit itself to any jurisdiction.
+
+`
+
+var license = regexp.MustCompile(licenseText)
+
+const prefix = "// Copyright "
+
+var skip = map[string]bool{}
+
+func main() {
+	flag.Parse()
+	err := filepath.Walk(".", func(path string, fi os.FileInfo, err error) error {
+		if skip[path] {
+			return nil
+		}
+
+		if err != nil {
+			return err
+		}
+
+		if filepath.Ext(path) != ".go" {
+			return nil
+		}
+
+		src, err := ioutil.ReadFile(path)
+		if err != nil {
+			return nil
+		}
+
+		// Find license
+		if !license.Match(src) {
+			err := fmt.Errorf("%v: license header not present", path)
+			if *fix == true {
+				newSrc := licenseText + string(src)
+				ioutil.WriteFile(path, []byte(newSrc), 644)
+				src = []byte(newSrc)
+			} else {
+				return err
+			}
+		}
+
+		// Also check it is at the top of the file.
+		if !bytes.HasPrefix(src, []byte(prefix)) {
+			return fmt.Errorf("%v: license header not at the top", path)
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
