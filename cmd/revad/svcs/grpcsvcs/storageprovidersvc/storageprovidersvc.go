802b8d415ac14091e5de431aa97852990b55781f
Hugo Gonzalez Labrador <github@hugo.labkode.com>
2019-08-14 13:19:16 +0200

    remove fmt.Println

--- cmd/revad/svcs/grpcsvcs/storageprovidersvc/storageprovidersvc.go
+++ cmd/revad/svcs/grpcsvcs/storageprovidersvc/storageprovidersvc.go
@@ -432,7 +432,6 @@ func (s *service) ListContainer(ctx context.Context, req *storageproviderv0alpha
 		s.fillInfo(md)
 		infos = append(infos, md)
 	}
-	fmt.Println(len(infos), len(mds), infos)
 	res := &storageproviderv0alphapb.ListContainerResponse{
 		Status: &rpcpb.Status{Code: rpcpb.Code_CODE_OK},
 		Infos:  infos,
