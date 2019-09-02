d903c6b977a36c62eb293a5326b9ca14a7b2205e
Giuseppe Lo Presti <glopresti@gmail.com>
2019-09-02 16:10:49 +0200

    Compile on all non-Windows following @tboerger's suggestion

--- pkg/storage/fs/owncloud/owncloud_unix.go
+++ pkg/storage/fs/owncloud/owncloud_unix.go
@@ -16,7 +16,7 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-// +build linux
+// +build !windows
 
 package owncloud
 
