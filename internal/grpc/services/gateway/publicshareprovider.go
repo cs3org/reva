9d96845c13053950c3d1d88bc4d6a2e0953fc84f
A.Unger <zyxancf@gmail.com>
2019-11-06 12:19:30 +0100

    grpc wiring for public links

--- internal/grpc/services/gateway/publicshareprovider.go
+++ internal/grpc/services/gateway/publicshareprovider.go
@@ -42,6 +42,8 @@ func (s *svc) CreatePublicShare(ctx context.Context, req *publicshareproviderv0a
 	if err != nil {
 		return nil, err
 	}
+
+	// TODO(refs) commit to storage if configured
 	return res, nil
 }
 
@@ -75,11 +77,11 @@ func (s *svc) GetPublicShare(ctx context.Context, req *publicshareproviderv0alph
 
 func (s *svc) ListPublicShares(ctx context.Context, req *publicshareproviderv0alphapb.ListPublicSharesRequest) (*publicshareproviderv0alphapb.ListPublicSharesResponse, error) {
 	log := appctx.GetLogger(ctx)
-	log.Info().Msg("list public share")
+	log.Info().Msg("listing public shares")
 
-	pClient, err := pool.GetPublicShareProviderClient(s.c.UserShareProviderEndpoint)
+	pClient, err := pool.GetPublicShareProviderClient(s.c.PublicShareProviderEndpoint)
 	if err != nil {
-		log.Err(err).Msg("gateway: error getting usershareprovider client")
+		log.Err(err).Msg("error connecting to a public share provider")
 		return &publicshareproviderv0alphapb.ListPublicSharesResponse{
 			Status: &rpcpb.Status{
 				Code: rpcpb.Code_CODE_INTERNAL,
@@ -89,7 +91,7 @@ func (s *svc) ListPublicShares(ctx context.Context, req *publicshareproviderv0al
 
 	res, err := pClient.ListPublicShares(ctx, req)
 	if err != nil {
-		return nil, errors.Wrap(err, "gateway: error calling ListShares")
+		return nil, errors.Wrap(err, "error calling ListShares")
 	}
 
 	// res := &publicshareproviderv0alphapb.ListPublicSharesResponse{
