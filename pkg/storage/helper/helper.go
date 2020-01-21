70960ead0dcf06cd50ee60134008d0c69c8a8eb4
Michael D'Silva <michael.dsilva@aarnet.edu.au>
2020-01-21 03:10:36 +0000

    add UsernamePrefixCount

--- pkg/storage/helper/helper.go
+++ pkg/storage/helper/helper.go
@@ -21,6 +21,8 @@ package helper
 import (
 	"bytes"
 	"fmt"
+	"regexp"
+	"strconv"
 	"strings"
 	"text/template"
 
@@ -31,10 +33,11 @@ import (
 )
 
 type layoutTemplate struct {
-	Username      string //the username
-	UsernameLower string //the username in lowercase
-	FirstLetter   string //first letter of username in lowercase
-	Provider      string //Provider/domain of user in lowercase
+	Username            string //the username
+	UsernameLower       string //the username in lowercase
+	UsernamePrefixCount string //first letters of username in lowercase eg: {{.UsernamePrefixCount.3}} will take the first 3 chars and make them lowercase, defaults to 1
+	UsernameFirstLetter string //first letter of username in lowercase, equivalent as {{.UsernamePrefixCount.1}} but easy to read
+	Provider            string //Provider/domain of user in lowercase
 }
 
 // GetUserHomePath converts username into user's home path according to layout
@@ -51,11 +54,26 @@ func GetUserHomePath(u *userpb.User, layout string) (string, error) {
 		usernameSplit[1] = "_unknown"
 	}
 
+	// handle {{.UsernamePrefixCount.x}}
+	// where x is an int, pull it out and remove it from the go template
+	letters := 1
+	reg := regexp.MustCompile(`\{\{\.UsernamePrefixCount\.[0-9]+\}\}`)
+	rmatches := reg.FindAllString(layout, -1)
+	if rmatches != nil {
+		reg := regexp.MustCompile("[^0-9]+")
+		f, _ := strconv.ParseInt(reg.ReplaceAllString(rmatches[0], ""), 10, 64)
+		if f > 1 {
+			letters = int(f)
+		}
+		layout = strings.Replace(layout, "{{.UsernamePrefixCount."+strconv.Itoa(letters)+"}}", "{{.UsernamePrefixCount}}", -1)
+	}
+
 	pathTemplate := layoutTemplate{
-		Username:      u.Username,
-		UsernameLower: strings.ToLower(u.Username),
-		FirstLetter:   strings.ToLower(string([]rune(usernameSplit[0])[0])),
-		Provider:      strings.ToLower(usernameSplit[1]),
+		Username:            u.Username,
+		UsernameLower:       strings.ToLower(u.Username),
+		UsernamePrefixCount: strings.ToLower(string([]rune(usernameSplit[0])[0:letters])),
+		UsernameFirstLetter: strings.ToLower(string([]rune(usernameSplit[0])[0])),
+		Provider:            strings.ToLower(usernameSplit[1]),
 	}
 	tmpl, err := template.New("userhomepath").Parse(layout)
 	if err != nil {
