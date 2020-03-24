6bb59688940736c4c3b1addf89bd177524f97ec2
Ishank Arora <ishank.arora@cern.ch>
2020-03-24 17:29:12 +0100

    Implement createShare and error handling

--- internal/http/services/ocmd/shares.go
+++ internal/http/services/ocmd/shares.go
@@ -24,8 +24,15 @@ import (
 	"fmt"
 	"net/http"
 	"path"
+	"strconv"
 
+	userpb "github.com/cs3org/go-cs3apis/cs3/identity/user/v1beta1"
+	rpc "github.com/cs3org/go-cs3apis/cs3/rpc/v1beta1"
 	link "github.com/cs3org/go-cs3apis/cs3/sharing/link/v1beta1"
+	ocm "github.com/cs3org/go-cs3apis/cs3/sharing/ocm/v1beta1"
+	provider "github.com/cs3org/go-cs3apis/cs3/storage/provider/v1beta1"
+	types "github.com/cs3org/go-cs3apis/cs3/types/v1beta1"
+	"github.com/cs3org/reva/internal/http/services/owncloud/ocs/conversions"
 	"github.com/cs3org/reva/pkg/appctx"
 	"github.com/cs3org/reva/pkg/rgrpc/todo/pool"
 	"github.com/rs/zerolog"
@@ -96,6 +103,139 @@ func (h *sharesHandler) Handler() http.Handler {
 }
 
 func (h *sharesHandler) createShare(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	log := appctx.GetLogger(ctx)
+
+	// TODO: Check if the user is allowed to share the file once the invitation workflow has been implemented.
+	gatewayClient, err := pool.GetGatewayServiceClient(h.gatewayAddr)
+	if err != nil {
+		WriteError(w, r, APIErrorServerError, fmt.Sprintf("error getting storage grpc client on addr: %v", h.gatewayAddr), err)
+		return
+	}
+
+	hRes, err := gatewayClient.GetHome(ctx, &provider.GetHomeRequest{})
+	if err != nil {
+		WriteError(w, r, APIErrorServerError, "error sending a grpc get home request", err)
+		return
+	}
+	prefix := hRes.GetPath()
+
+	shareWith := r.FormValue("shareWith")
+	if shareWith == "" {
+		WriteError(w, r, APIErrorInvalidParameter, "missing shareWith", nil)
+		return
+	}
+
+	userRes, err := gatewayClient.GetUser(ctx, &userpb.GetUserRequest{
+		UserId: &userpb.UserId{OpaqueId: shareWith},
+	})
+
+	if err != nil {
+		WriteError(w, r, APIErrorInvalidParameter, "error searching recipient", err)
+		return
+	}
+
+	if userRes.Status.Code != rpc.Code_CODE_OK {
+		WriteError(w, r, APIErrorNotFound, "user not found", err)
+		return
+	}
+
+	var permissions conversions.Permissions
+
+	role := r.FormValue("role")
+	if role == "" {
+		pval := r.FormValue("permissions")
+		if pval == "" {
+			// by default only allow read permissions / assign viewer role
+			role = conversions.RoleViewer
+		} else {
+			pint, err := strconv.Atoi(pval)
+			if err != nil {
+				WriteError(w, r, APIErrorInvalidParameter, "permissions must be an integer", err)
+				return
+			}
+			permissions, err = conversions.NewPermissions(pint)
+			if err != nil {
+				WriteError(w, r, APIErrorInvalidParameter, err.Error(), nil)
+				return
+			}
+			role = conversions.Permissions2Role(permissions)
+		}
+	}
+
+	var resourcePermissions *provider.ResourcePermissions
+	resourcePermissions, err = h.role2CS3Permissions(role)
+	if err != nil {
+		WriteError(w, r, APIErrorInvalidParameter, "unknown role", err)
+		return
+	}
+
+	roleMap := map[string]string{"name": role}
+	val, err := json.Marshal(roleMap)
+	if err != nil {
+		WriteError(w, r, APIErrorServerError, "could not encode role", err)
+		return
+	}
+
+	statReq := &provider.StatRequest{
+		Ref: &provider.Reference{
+			Spec: &provider.Reference_Path{
+				Path: path.Join(prefix, r.FormValue("path")),
+			},
+		},
+	}
+
+	statRes, err := gatewayClient.Stat(ctx, statReq)
+	if err != nil {
+		WriteError(w, r, APIErrorServerError, "error sending a grpc stat request", err)
+		return
+	}
+
+	if statRes.Status.Code != rpc.Code_CODE_OK {
+		if statRes.Status.Code == rpc.Code_CODE_NOT_FOUND {
+			WriteError(w, r, APIErrorNotFound, "not found", nil)
+			return
+		}
+		WriteError(w, r, APIErrorServerError, "grpc stat request failed", err)
+		return
+	}
+
+	createShareReq := &ocm.CreateOCMShareRequest{
+		Opaque: &types.Opaque{
+			Map: map[string]*types.OpaqueEntry{
+				"role": &types.OpaqueEntry{
+					Decoder: "json",
+					Value:   val,
+				},
+			},
+		},
+		ResourceId: statRes.Info.Id,
+		Grant: &ocm.ShareGrant{
+			Grantee: &provider.Grantee{
+				Type: provider.GranteeType_GRANTEE_TYPE_USER,
+				Id:   userRes.User.GetId(),
+			},
+			Permissions: &ocm.SharePermissions{
+				Permissions: resourcePermissions,
+			},
+		},
+	}
+
+	createShareResponse, err := gatewayClient.CreateOCMShare(ctx, createShareReq)
+	if err != nil {
+		WriteError(w, r, APIErrorServerError, "error sending a grpc create share request", err)
+		return
+	}
+	if createShareResponse.Status.Code != rpc.Code_CODE_OK {
+		if createShareResponse.Status.Code == rpc.Code_CODE_NOT_FOUND {
+			WriteError(w, r, APIErrorNotFound, "not found", nil)
+			return
+		}
+		WriteError(w, r, APIErrorServerError, "grpc create share request failed", err)
+		return
+	}
+
+	log.Info().Msg("Share created.")
 }
 
 func (h *sharesHandler) getShare(w http.ResponseWriter, r *http.Request, shareID string) {
@@ -156,6 +296,66 @@ func (h *sharesHandler) getShares(ctx context.Context, logger *zerolog.Logger, u
 
 	logger.Debug().Str("shares", fmt.Sprintf("%+v", shares)).Msg("GetShares")
 	return shares, nil
+}
+
+func (h *sharesHandler) role2CS3Permissions(r string) (*provider.ResourcePermissions, error) {
+	switch r {
+	case conversions.RoleViewer:
+		return &provider.ResourcePermissions{
+			ListContainer:        true,
+			ListGrants:           true,
+			ListFileVersions:     true,
+			ListRecycle:          true,
+			Stat:                 true,
+			GetPath:              true,
+			GetQuota:             true,
+			InitiateFileDownload: true,
+		}, nil
+	case conversions.RoleEditor:
+		return &provider.ResourcePermissions{
+			ListContainer:        true,
+			ListGrants:           true,
+			ListFileVersions:     true,
+			ListRecycle:          true,
+			Stat:                 true,
+			GetPath:              true,
+			GetQuota:             true,
+			InitiateFileDownload: true,
+
+			Move:               true,
+			InitiateFileUpload: true,
+			RestoreFileVersion: true,
+			RestoreRecycleItem: true,
+			CreateContainer:    true,
+			Delete:             true,
+			PurgeRecycle:       true,
+		}, nil
+	case conversions.RoleCoowner:
+		return &provider.ResourcePermissions{
+			ListContainer:        true,
+			ListGrants:           true,
+			ListFileVersions:     true,
+			ListRecycle:          true,
+			Stat:                 true,
+			GetPath:              true,
+			GetQuota:             true,
+			InitiateFileDownload: true,
+
+			Move:               true,
+			InitiateFileUpload: true,
+			RestoreFileVersion: true,
+			RestoreRecycleItem: true,
+			CreateContainer:    true,
+			Delete:             true,
+			PurgeRecycle:       true,
+
+			AddGrant:    true,
+			RemoveGrant: true, // TODO when are you able to unshare / delete
+			UpdateGrant: true,
+		}, nil
+	default:
+		return nil, fmt.Errorf("unknown role: %s", r)
+	}
 }
 
 func convertPublicShareToShare(publicShare *link.PublicShare) *share {
