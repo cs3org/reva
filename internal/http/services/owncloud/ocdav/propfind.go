353b7dc88428726a1f34a93839356e8ffb1c44e3
Jörn Friedrich Dreyer <jfd@butonic.de>
2020-04-03 12:43:10 +0200

    Revert "escape special xml characters in propfind response (#627)"
    This reverts commit 17d43e3bf71e17c28131274b6e151609a9ed7803.

--- internal/http/services/owncloud/ocdav/propfind.go
+++ internal/http/services/owncloud/ocdav/propfind.go
@@ -19,7 +19,6 @@
 package ocdav
 
 import (
-	"bytes"
 	"context"
 	"encoding/xml"
 	"fmt"
@@ -170,12 +169,10 @@ func (s *svc) formatPropfind(ctx context.Context, pf *propfindXML, mds []*provid
 }
 
 func (s *svc) newProp(key, val string) *propertyXML {
-	escaped := new(bytes.Buffer)
-	xml.Escape(escaped, []byte(val))
 	return &propertyXML{
 		XMLName:  xml.Name{Space: "", Local: key},
 		Lang:     "",
-		InnerXML: escaped.Bytes(),
+		InnerXML: []byte(val),
 	}
 }
 
