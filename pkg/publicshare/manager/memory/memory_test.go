9c96594ad72d69ef3e3b9b5227a5406e4266dba2
Hugo Gonzalez Labrador <github@hugo.labkode.com>
2020-06-02 12:28:32 +0200

    wipi

--- pkg/publicshare/manager/memory/memory_test.go
+++ pkg/publicshare/manager/memory/memory_test.go
@@ -18,154 +18,153 @@
 
 package memory
 
-// import (
-// 	"context"
-// 	"testing"
+import (
+	"context"
+	"testing"
 
-// 	userpb "github.com/cs3org/go-cs3apis/cs3/identity/user/v1beta1"
-// 	link "github.com/cs3org/go-cs3apis/cs3/sharing/link/v1beta1"
-// 	provider "github.com/cs3org/go-cs3apis/cs3/storage/provider/v1beta1"
-// 	types "github.com/cs3org/go-cs3apis/cs3/types/v1beta1"
-// )
+	userpb "github.com/cs3org/go-cs3apis/cs3/identity/user/v1beta1"
+	link "github.com/cs3org/go-cs3apis/cs3/sharing/link/v1beta1"
+	provider "github.com/cs3org/go-cs3apis/cs3/storage/provider/v1beta1"
+	types "github.com/cs3org/go-cs3apis/cs3/types/v1beta1"
+)
 
-// func TestMemoryProvider(t *testing.T) {
-// 	// table driven tests is perhaps more readable
-// 	// setup a new public shares manager
-// 	manager, err := New(make(map[string]interface{}))
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
+func TestMemoryProvider(t *testing.T) {
+	// table driven tests is perhaps more readable
+	// setup a new public shares manager
+	manager, err := New(make(map[string]interface{}))
+	if err != nil {
+		t.Error(err)
+	}
 
-// 	// Setup dat
-// 	user := userpb.User{}
-// 	rInfo := provider.ResourceInfo{}
-// 	grant := link.Grant{}
+	// Setup dat
+	user := userpb.User{}
+	rInfo := provider.ResourceInfo{}
+	grant := link.Grant{}
 
-// 	rInfo.ArbitraryMetadata = &provider.ArbitraryMetadata{
-// 		Metadata: map[string]string{
-// 			"name": "woof",
-// 		},
-// 	}
+	rInfo.ArbitraryMetadata = &provider.ArbitraryMetadata{
+		Metadata: map[string]string{
+			"name": "woof",
+		},
+	}
 
-// 	// create a new public share
-// 	share, _ := manager.CreatePublicShare(context.Background(), &user, &rInfo, &grant)
+	// create a new public share
+	share, _ := manager.CreatePublicShare(context.Background(), &user, &rInfo, &grant)
 
-// 	// store its token for further retrieval
-// 	shareToken := share.GetToken()
+	// store its token for further retrieval
+	shareToken := share.GetToken()
 
-// 	// Test updating a public share.
-// 	existingRefToken := link.PublicShareReference{
-// 		Spec: &link.PublicShareReference_Token{
-// 			Token: shareToken,
-// 		},
-// 	}
+	// Test updating a public share.
+	existingRefToken := link.PublicShareReference{
+		Spec: &link.PublicShareReference_Token{
+			Token: shareToken,
+		},
+	}
 
-// 	nonExistingPublicShareRef := link.PublicShareReference{
-// 		Spec: &link.PublicShareReference_Token{Token: "somethingsomething"},
-// 	}
+	nonExistingPublicShareRef := link.PublicShareReference{
+		Spec: &link.PublicShareReference_Token{Token: "somethingsomething"},
+	}
 
-// 	updatedMtime := &types.Timestamp{Seconds: uint64(46800)}
+	updatedMtime := &types.Timestamp{Seconds: uint64(46800)}
 
-// 	newGrant := link.Grant{
-// 		Permissions: &link.PublicSharePermissions{},
-// 		Expiration:  updatedMtime,
-// 	}
+	newGrant := link.Grant{
+		Permissions: &link.PublicSharePermissions{},
+		Expiration:  updatedMtime,
+	}
 
-// 	// attempt to update an invalid public share. we expect an error
-// 	_, err = manager.UpdatePublicShare(context.Background(), &user, &nonExistingPublicShareRef, &newGrant)
-// 	if err == nil {
-// 		t.Error(err)
-// 	}
+	// attempt to update an invalid public share. we expect an error
+	_, err = manager.UpdatePublicShare(context.Background(), &user, &nonExistingPublicShareRef, &newGrant)
+	if err == nil {
+		t.Error(err)
+	}
 
-// 	// update an existing public share
-// 	updatedShare, err := manager.UpdatePublicShare(context.Background(), &user, &existingRefToken, &newGrant)
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
+	// update an existing public share
+	updatedShare, err := manager.UpdatePublicShare(context.Background(), &user, &existingRefToken, &newGrant)
+	if err != nil {
+		t.Error(err)
+	}
 
-// 	// verify the expiration was updated to 01/01/1970 @ 1:00pm (UTC)
-// 	if updatedShare.Expiration == updatedMtime {
-// 		t.Error("")
-// 	}
+	// verify the expiration was updated to 01/01/1970 @ 1:00pm (UTC)
+	if updatedShare.Expiration == updatedMtime {
+		t.Error("")
+	}
 
-// 	// test getting an invalid token
-// 	_, err = manager.GetPublicShareByToken(context.Background(), "xxxxxxxx")
-// 	if err == nil {
-// 		t.Error(err)
-// 	}
+	// test getting an invalid token
+	_, err = manager.GetPublicShareByToken(context.Background(), "xxxxxxxx")
+	if err == nil {
+		t.Error(err)
+	}
 
-// 	// test getting a valid token
-// 	fetchedPs, err := manager.GetPublicShareByToken(context.Background(), shareToken)
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
+	// test getting a valid token
+	fetchedPs, err := manager.GetPublicShareByToken(context.Background(), shareToken)
+	if err != nil {
+		t.Error(err)
+	}
 
-// 	if fetchedPs.GetToken() != shareToken {
-// 		t.Error("mismatching public share tokens")
-// 	}
+	if fetchedPs.GetToken() != shareToken {
+		t.Error("mismatching public share tokens")
+	}
 
-// 	// test listing public shares
-// 	listPs, err := manager.ListPublicShares(context.Background(), &user, nil, &rInfo)
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
+	// test listing public shares
+	listPs, err := manager.ListPublicShares(context.Background(), &user, nil, &rInfo)
+	if err != nil {
+		t.Error(err)
+	}
 
-// 	if len(listPs) != 1 {
-// 		t.Errorf("expected list of length 1, but got %v", len(listPs))
-// 	}
+	if len(listPs) != 1 {
+		t.Errorf("expected list of length 1, but got %v", len(listPs))
+	}
 
-// 	// holds a reference of hte public share with the previously fetched token
-// 	publicShareRef := link.PublicShareReference{
-// 		Spec: &link.PublicShareReference_Token{Token: shareToken},
-// 	}
+	// holds a reference of hte public share with the previously fetched token
+	publicShareRef := link.PublicShareReference{
+		Spec: &link.PublicShareReference_Token{Token: shareToken},
+	}
 
-// 	// error expected
-// 	_, err = manager.GetPublicShare(context.Background(), &user, &nonExistingPublicShareRef)
-// 	if err == nil {
-// 		t.Error(err)
-// 	}
+	// error expected
+	_, err = manager.GetPublicShare(context.Background(), &user, &nonExistingPublicShareRef)
+	if err == nil {
+		t.Error(err)
+	}
 
-// 	// expected error to be nil
-// 	pShare, err := manager.GetPublicShare(context.Background(), &user, &publicShareRef)
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
+	// expected error to be nil
+	pShare, err := manager.GetPublicShare(context.Background(), &user, &publicShareRef)
+	if err != nil {
+		t.Error(err)
+	}
 
-// 	existingRefID := link.PublicShareReference{
-// 		Spec: &link.PublicShareReference_Id{
-// 			Id: pShare.GetId(),
-// 		},
-// 	}
+	existingRefID := link.PublicShareReference{
+		Spec: &link.PublicShareReference_Id{
+			Id: pShare.GetId(),
+		},
+	}
 
-// 	nonExistingRefID := link.PublicShareReference{
-// 		Spec: &link.PublicShareReference_Id{
-// 			Id: &link.PublicShareId{
-// 				OpaqueId: "doesnt_exist",
-// 			},
-// 		},
-// 	}
+	nonExistingRefID := link.PublicShareReference{
+		Spec: &link.PublicShareReference_Id{
+			Id: &link.PublicShareId{
+				OpaqueId: "doesnt_exist",
+			},
+		},
+	}
+	// get public share by ID... we don't expect an error
+	_, err = manager.GetPublicShare(context.Background(), &user, &existingRefID)
+	if err != nil {
+		t.Error(err)
+	}
 
-// 	// get public share by ID... we don't expect an error
-// 	_, err = manager.GetPublicShare(context.Background(), &user, &existingRefID)
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
+	// get public share by ID... we expect an error
+	_, err = manager.GetPublicShare(context.Background(), &user, &nonExistingRefID)
+	if err == nil {
+		t.Error(err)
+	}
 
-// 	// get public share by ID... we expect an error
-// 	_, err = manager.GetPublicShare(context.Background(), &user, &nonExistingRefID)
-// 	if err == nil {
-// 		t.Error(err)
-// 	}
+	// attempts to revoke a public share that does not exist, we expect an error
+	err = manager.RevokePublicShare(context.Background(), &user, "ref_does_not_exist")
+	if err == nil {
+		t.Error("expected a failure when revoking a public share that does not exist")
+	}
 
-// 	// attempts to revoke a public share that does not exist, we expect an error
-// 	err = manager.RevokePublicShare(context.Background(), &user, "ref_does_not_exist")
-// 	if err == nil {
-// 		t.Error("expected a failure when revoking a public share that does not exist")
-// 	}
-
-// 	// revoke an existing public share
-// 	err = manager.RevokePublicShare(context.Background(), &user, fetchedPs.GetToken())
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-// }
+	// revoke an existing public share
+	err = manager.RevokePublicShare(context.Background(), &user, fetchedPs.GetToken())
+	if err != nil {
+		t.Error(err)
+	}
+}
