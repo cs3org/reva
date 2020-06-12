3ea89d3245b861bb008e77259725303dba368988
Lovisa Lugnegard <lovisa.lugnegard@gmail.com>
2020-06-12 13:59:22 +0200

    Initial commit for open call

--- internal/grpc/services/appprovider/appprovider.go
+++ internal/grpc/services/appprovider/appprovider.go
@@ -21,14 +21,19 @@ package appprovider
 import (
 	"context"
 	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
 
 	providerpb "github.com/cs3org/go-cs3apis/cs3/app/provider/v1beta1"
 	"github.com/cs3org/reva/pkg/app"
 	"github.com/cs3org/reva/pkg/app/provider/demo"
 	"github.com/cs3org/reva/pkg/rgrpc"
 	"github.com/cs3org/reva/pkg/rgrpc/status"
+	"github.com/cs3org/reva/pkg/rhttp"
 	"github.com/mitchellh/mapstructure"
 	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc"
 )
 
@@ -94,7 +99,96 @@ func getProvider(c *config) (app.Provider, error) {
 }
 
 func (s *service) Open(ctx context.Context, req *providerpb.OpenRequest) (*providerpb.OpenResponse, error) {
-	iframeLocation, err := s.provider.GetIFrame(ctx, req.ResourceInfo.Id, req.AccessToken)
+	// 1. Get the application URL for the file's mimetype.
+	//This is going to be the "Bring Your Own App registry" service in Reva,
+	//for now it's implemented by the wopiserver at /wopi/cbox/endpoints.
+	// 2. Call the wopiserver's /wopi/cbox/open and pass the requested arguments
+	// (documented at https://github.com/cs3org/wopiserver/blob/master/src/wopiserver.py#L238).
+	// 3. Output all as the iframe_url field of the OpenResponse.
+	//For reference, https://github.com/cs3org/wopiserver/blob/master/tools/wopiopen.py does a similar job
+
+	// 1. applicationURL = GET /wopi/cbox/endpoints
+	// # get the application server URLs
+	wopiurl := ""   //TODO!
+	iopsecret := "" //TODO!
+	filename := ""
+	eos := ""
+	//res, err := http.Get(wopiurl + "/wopi/cbox/endpoints")
+
+	var canedit bool
+
+	if req.GetViewMode == providerpb.OpenRequest.ViewMode.VIEW_MODE_READ_WRITE {
+		canedit = true
+	} else {
+		canedit = false
+	}
+
+	//2. tagert, accessToken = WOPI open(userId,canedit, username (optional), filename/fileid, folderUrl, endpoint(optional))
+	// def cboxOpen():
+	// '''Generates a WOPISrc target and an access token to be passed to a WOPI-compatible Office-like app
+	// for accessing a given file for a given user.
+	// Request arguments:
+	// - string userid: user identity, typically an x-access-token;
+	//   - OR int ruid, rgid: a real Unix user identity (id:group); this is for legacy compatibility
+	// - bool canedit: True if full access should be given to the user, otherwise read-only access is granted
+	// - string username (optional): user's full name, typically shown by the Office app
+	// - string filename OR fileid: the full path of the filename to be opened, or its fileid
+	// - string folderurl: the URL to come back to the containing folder for this file, typically shown by the Office app
+	// - string endpoint (optional): the storage endpoint to be used to look up the file or the storage id, in case of
+	//   multi-instance underlying storage; defaults to 'default'
+
+	// client := &http.Client{
+	// 	CheckRedirect: redirectPolicyFunc,
+	// }
+	httpClient := rhttp.GetHTTPClient(ctx)
+
+	// params := url.Values{
+	// 	"grant_type": {"client_credentials"},
+	// 	"audience":   {m.conf.TargetAPI},
+	// }
+	foourl := ""
+	params := url.Values{"x-access-token": {req.AccessToken}, "filename": {filename}, "endpoint": {eos},
+		"canedit": {"true"}, "username": {"Operator"}, "folderurl": {"foo"}}
+	httpReq, err := rhttp.NewRequest(ctx, "GET", foourl, strings.NewReader(params.Encode()))
+	if err != nil {
+		return nil, err
+	}
+
+	httpReq.Header.Set("Authorization", "Bearer "+iopsecret)
+
+	httpRes, err := httpClient.Do(httpReq)
+	// resp, err := client.Get("http://example.com")
+	// // ...
+
+	// req, err := http.NewRequest("GET", "http://example.com", nil)
+	// // ...
+
+	//wopisrc = http.Get(wopiurl + "/wopi/cbox/open",
+	//httpRes, err := httpClient.Do(httpReq)
+	if err != nil {
+		log.Error().Err(err).Msg("error performing http request")
+		// w.WriteHeader(http.StatusInternalServerError)
+		// return
+	}
+	defer httpRes.Body.Close()
+
+	if httpRes.StatusCode != http.StatusOK {
+		log.Error().Err(err).Msg("error performing http request")
+		// w.WriteHeader(http.StatusInternalServerError)
+		// return
+	}
+	// if httpRes.StatusCode != http.StatusOK {
+	// 	print("WOPI open request failed:\n%s" % httpRes.Body)
+	// 	//	sys.exit(-1)
+	// }
+
+	//# return the full URL to the user
+	// try:
+	// 	url = apps[os.path.splitext(filename)[1]]["edit"]
+	// 	url += '?'                              //if '?' not in url else '&'
+	// 	print("App URL:\n%sWOPISrc=%s\n" + url) // % (url, httpRes.content.decode()))
+
+	iframeLocation, err := s.provider.GetIFrame(ctx, req.Ref.GetId(), req.AccessToken)
 	if err != nil {
 		err := errors.Wrap(err, "appprovidersvc: error calling GetIFrame")
 		res := &providerpb.OpenResponse{
@@ -102,9 +196,9 @@ func (s *service) Open(ctx context.Context, req *providerpb.OpenRequest) (*provi
 		}
 		return res, nil
 	}
-	res := &providerpb.OpenResponse{
+	res1 := &providerpb.OpenResponse{
 		Status:    status.NewOK(ctx),
 		IframeUrl: iframeLocation,
 	}
-	return res, nil
+	return res1, nil
 }
--- internal/grpc/services/gateway/appprovider.go
+++ internal/grpc/services/gateway/appprovider.go
@@ -24,15 +24,61 @@ import (
 	providerpb "github.com/cs3org/go-cs3apis/cs3/app/provider/v1beta1"
 	registry "github.com/cs3org/go-cs3apis/cs3/app/registry/v1beta1"
 	rpc "github.com/cs3org/go-cs3apis/cs3/rpc/v1beta1"
+	provider "github.com/cs3org/go-cs3apis/cs3/storage/provider/v1beta1"
 	storageprovider "github.com/cs3org/go-cs3apis/cs3/storage/provider/v1beta1"
 	"github.com/cs3org/reva/pkg/errtypes"
 	"github.com/cs3org/reva/pkg/rgrpc/status"
 	"github.com/cs3org/reva/pkg/rgrpc/todo/pool"
 	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
 )
 
 func (s *svc) Open(ctx context.Context, req *providerpb.OpenRequest) (*providerpb.OpenResponse, error) {
-	provider, err := s.findAppProvider(ctx, req.ResourceInfo)
+	// get the metadata about the share
+	c, err := s.findByID(ctx, req.Ref.GetId())
+	if err != nil {
+		if _, ok := err.(errtypes.IsNotFound); ok {
+			return &providerpb.OpenResponse{
+				Status: status.NewInternal(ctx, err, "storage provider not found"),
+			}, nil
+			// return status.NewNotFound(ctx, "storage provider not found"), nil
+		}
+		return &providerpb.OpenResponse{
+			Status: status.NewInternal(ctx, err, "error finding storage provider"),
+		}, nil
+		// return status.NewInternal(ctx, err, "error finding storage provider"), nil
+	}
+	statReq := &provider.StatRequest{
+		Ref: &provider.Reference{
+			Spec: &provider.Reference_Id{
+				Id: req.Ref.GetId(),
+			},
+		},
+	}
+
+	statRes, err := c.Stat(ctx, statReq)
+	if err != nil {
+		log.Err(err).Msg("gateway: error calling Stat for the share resource id:" + req.Ref.GetId().String())
+		return &providerpb.OpenResponse{
+			Status: status.NewInternal(ctx, err, "gateway: error calling Stat for the share resource id"),
+		}, nil
+		// return &rpc.Status{
+		// 	Code: rpc.Code_CODE_INTERNAL,
+		// }, nil
+	}
+
+	if statRes.Status.Code != rpc.Code_CODE_OK {
+		err := status.NewErrorFromCode(statRes.Status.GetCode(), "gateway")
+		log.Err(err).Msg("gateway: error calling Stat for the share resource id:" + req.Ref.GetId().String())
+		return &providerpb.OpenResponse{
+			Status: status.NewInternal(ctx, err, "error updating received share"),
+		}, nil
+		//return status.NewInternal(ctx, err, "error updating received share"), nil
+	}
+
+	fileInfo := statRes.Info
+
+	provider, err := s.findAppProvider(ctx, fileInfo)
 	if err != nil {
 		err = errors.Wrap(err, "gateway: error calling findAppProvider")
 		var st *rpc.Status
@@ -47,15 +93,15 @@ func (s *svc) Open(ctx context.Context, req *providerpb.OpenRequest) (*providerp
 		}, nil
 	}
 
-	c, err := pool.GetAppProviderClient(provider.Address)
-	if err != nil {
-		err = errors.Wrap(err, "gateway: error calling GetAppProviderClient")
+	c1, err1 := pool.GetAppProviderClient(provider.Address)
+	if err1 != nil {
+		err = errors.Wrap(err1, "gateway: error calling GetAppProviderClient")
 		return &providerpb.OpenResponse{
 			Status: status.NewInternal(ctx, err, "error getting appprovider client"),
 		}, nil
 	}
 
-	res, err := c.Open(ctx, req)
+	res, err := c1.Open(ctx, req)
 	if err != nil {
 		return nil, errors.Wrap(err, "gateway: error calling c.Open")
 	}
