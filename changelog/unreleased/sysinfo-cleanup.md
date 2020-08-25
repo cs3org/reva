1a1a0f5c5e61cdb62a9332b348f6b52c51604ba6
Daniel Müller <daniel.mueller@uni-muenster.de>
2020-08-25 15:45:45 +0200

    Some cleanup

--- changelog/unreleased/sysinfo-cleanup.md
+++ changelog/unreleased/sysinfo-cleanup.md
@@ -1,4 +1,4 @@
-Enhancement: System information metrics now use OpenCensus and were moved out of the Prometheus service
+Enhancement: System information metrics cleanup
 
 The system information metrics are now based on OpenCensus instead of the Prometheus client library. Furthermore, its initialization was moved out of the Prometheus HTTP service to keep things clean.
 
--- pkg/sysinfo/metrics.go
+++ pkg/sysinfo/metrics.go
@@ -34,55 +34,43 @@ import (
 
 type sysInfoMetricsLabels = map[tag.Key]string
 
-type sysInfoMetrics struct {
-	sysInfoStats *stats.Int64Measure
-	sysInfoView  *view.View
-
-	labels sysInfoMetricsLabels
-
-	ctx context.Context
-}
-
-var (
-	metrics *sysInfoMetrics
-)
-
-func (m *sysInfoMetrics) init() error {
-	m.labels = m.getLabels("", SysInfo)
+func registerSystemInfoMetrics() error {
+	labels := getSystemInfoMetricsLabels("", SysInfo)
 
 	// Collect all labels and their values; the values are stored as mutators
-	tagKeys := make([]tag.Key, 0, len(m.labels))
-	mutators := make([]tag.Mutator, 0, len(m.labels))
-	for key, value := range m.labels {
+	tagKeys := make([]tag.Key, 0, len(labels))
+	mutators := make([]tag.Mutator, 0, len(labels))
+	for key, value := range labels {
 		tagKeys = append(tagKeys, key)
 		mutators = append(mutators, tag.Insert(key, value))
 	}
 
 	// Create the OpenCensus statistics and a corresponding view
-	m.sysInfoStats = stats.Int64("sys_info", "A metric with a constant '1' value labeled by various system information elements", stats.UnitDimensionless)
-	m.sysInfoView = &view.View{
-		Name:        m.sysInfoStats.Name(),
-		Description: m.sysInfoStats.Description(),
-		Measure:     m.sysInfoStats,
+	sysInfoStats := stats.Int64("sys_info", "A metric with a constant '1' value labeled by various system information elements", stats.UnitDimensionless)
+	sysInfoView := &view.View{
+		Name:        sysInfoStats.Name(),
+		Description: sysInfoStats.Description(),
+		Measure:     sysInfoStats,
 		TagKeys:     tagKeys,
 		Aggregation: view.LastValue(),
 	}
 
-	if err := view.Register(m.sysInfoView); err != nil {
-		return errors.Wrap(err, "unable to register the system info view")
+	if err := view.Register(sysInfoView); err != nil {
+		return errors.Wrap(err, "unable to register the system info metrics view")
 	}
 
 	// Create a new context to serve the metrics
 	if ctx, err := tag.New(context.Background(), mutators...); err == nil {
-		m.ctx = ctx
+		// Just record a simple hardcoded '1' to expose the system info as a metric
+		stats.Record(ctx, sysInfoStats.M(1))
 	} else {
-		return errors.Wrap(err, "unable to create a context")
+		return errors.Wrap(err, "unable to create a context for the system info metrics")
 	}
 
 	return nil
 }
 
-func (m *sysInfoMetrics) getLabels(root string, i interface{}) sysInfoMetricsLabels {
+func getSystemInfoMetricsLabels(root string, i interface{}) sysInfoMetricsLabels {
 	labels := sysInfoMetricsLabels{}
 
 	// Iterate over each field of the given interface, recursively collecting the values as labels
@@ -105,7 +93,7 @@ func (m *sysInfoMetrics) getLabels(root string, i interface{}) sysInfoMetricsLab
 		f := v.Field(i)
 		if f.Kind() == reflect.Struct || (f.Kind() == reflect.Ptr && f.Elem().Kind() == reflect.Struct) {
 			// Merge labels recursively
-			for key, val := range m.getLabels(fieldName, f.Interface()) {
+			for key, val := range getSystemInfoMetricsLabels(fieldName, f.Interface()) {
 				labels[key] = val
 			}
 		} else { // Store the value of the field in the labels
@@ -115,20 +103,4 @@ func (m *sysInfoMetrics) getLabels(root string, i interface{}) sysInfoMetricsLab
 	}
 
 	return labels
-}
-
-func (m *sysInfoMetrics) record() {
-	// Just record a simple hardcoded '1' to expose the system info as a metric
-	stats.Record(m.ctx, m.sysInfoStats.M(1))
-}
-
-func registerSystemInfoMetrics() error {
-	// Create and initialize the local system info metrics object
-	metrics = &sysInfoMetrics{}
-	if err := metrics.init(); err != nil {
-		return errors.Wrap(err, "unable to initialize the system information metrics")
-	}
-
-	metrics.record()
-	return nil
 }
