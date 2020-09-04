492312125771ab2df7d2dc2a97e7aa3d442e62c4
Jörn Friedrich Dreyer <jfd@butonic.de>
2020-09-04 23:53:51 +0200

    list revisions
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

--- pkg/storage/fs/ocis/revisions.go
+++ pkg/storage/fs/ocis/revisions.go
@@ -3,13 +3,38 @@ package ocis
 import (
 	"context"
 	"io"
+	"os"
+	"path/filepath"
 
 	provider "github.com/cs3org/go-cs3apis/cs3/storage/provider/v1beta1"
 	"github.com/cs3org/reva/pkg/errtypes"
 )
 
-func (fs *ocisfs) ListRevisions(ctx context.Context, ref *provider.Reference) ([]*provider.FileVersion, error) {
-	return nil, errtypes.NotSupported("operation not supported: ListRevisions")
+func (fs *ocisfs) ListRevisions(ctx context.Context, ref *provider.Reference) (revisions []*provider.FileVersion, err error) {
+	var node *NodeInfo
+	if node, err = fs.pw.NodeFromResource(ctx, ref); err != nil {
+		return
+	}
+	if !node.Exists {
+		err = errtypes.NotFound(filepath.Join(node.ParentID, node.Name))
+		return
+	}
+
+	revisions = []*provider.FileVersion{}
+	nodePath := filepath.Join(fs.pw.Root(), "nodes", node.ID)
+	if items, err := filepath.Glob(nodePath + ".REV.*"); err == nil {
+		for i := range items {
+			if fi, err := os.Stat(items[i]); err == nil {
+				rev := &provider.FileVersion{
+					Key:   filepath.Base(items[i]),
+					Size:  uint64(fi.Size()),
+					Mtime: uint64(fi.ModTime().Unix()),
+				}
+				revisions = append(revisions, rev)
+			}
+		}
+	}
+	return
 }
 func (fs *ocisfs) DownloadRevision(ctx context.Context, ref *provider.Reference, revisionKey string) (io.ReadCloser, error) {
 	return nil, errtypes.NotSupported("operation not supported: DownloadRevision")
