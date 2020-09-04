e3f47c5dec4407862ecda9e510c68339b5fbd104
Antoon Prins <antoon.prins@surfsara.nl>
2020-09-04 13:01:30 +0200

    Add metrics service to loader.

--- internal/http/services/loader/loader.go
+++ internal/http/services/loader/loader.go
@@ -25,6 +25,7 @@ import (
 	_ "github.com/cs3org/reva/internal/http/services/helloworld"
 	_ "github.com/cs3org/reva/internal/http/services/mentix"
 	_ "github.com/cs3org/reva/internal/http/services/meshdirectory"
+	_ "github.com/cs3org/reva/internal/http/services/metrics"
 	_ "github.com/cs3org/reva/internal/http/services/ocmd"
 	_ "github.com/cs3org/reva/internal/http/services/oidcprovider"
 	_ "github.com/cs3org/reva/internal/http/services/owncloud/ocdav"
