f9b310326d09f131e17a0ed51c416c7c19a91f5c
Antoon Prins <antoon.prins@surfsara.nl>
2020-09-04 13:06:28 +0200

    Use register to get configured driver.

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -20,55 +20,64 @@ package metrics
 
 /*
 Metrics registers OpenCensus data views of the metrics.
-The Reader interface defines the available metrics which are implemented in the drivers that read the metrics data.
-Metrics initializes the correct driver as specified in the configuration.
+Metrics initializes the driver as specified in the configuration.
 */
 import (
 	"context"
-	"errors"
+	"os"
 	"time"
 
-	"github.com/cs3org/reva/pkg/sharedconf"
+	"github.com/cs3org/reva/pkg/metrics/config"
+	"github.com/cs3org/reva/pkg/metrics/driver/registry"
+	"github.com/cs3org/reva/pkg/metrics/reader"
 
-	"github.com/cs3org/reva/pkg/metrics/driver/dummy"
-	"github.com/cs3org/reva/pkg/metrics/driver/json"
-
-	"github.com/rs/zerolog/log"
+	"github.com/cs3org/reva/pkg/logger"
 	"go.opencensus.io/stats"
 	"go.opencensus.io/stats/view"
 )
 
-func init() {
-	m := &Metrics{
-		dataDriver:           nil,
-		NumUsersMeasure:      stats.Int64("cs3_org_sciencemesh_site_total_num_users", "The total number of users within this site", stats.UnitDimensionless),
-		NumGroupsMeasure:     stats.Int64("cs3_org_sciencemesh_site_total_num_groups", "The total number of groups within this site", stats.UnitDimensionless),
-		AmountStorageMeasure: stats.Int64("cs3_org_sciencemesh_site_total_amount_storage", "The total amount of storage used within this site", stats.UnitBytes),
-	}
+// Init intializes metrics according to the specified configuration
+func Init(conf *config.Config) {
+	log := logger.New().With().Int("pid", os.Getpid()).Logger()
 
-	// register the desired measures' views
-	if err := view.Register(
-		m.getNumUsersView(),
-		m.getNumGroupsView(),
-		m.getAmountStorageView(),
-	); err != nil {
-		log.Error().Err(err).Msg("error registering the driver's views with opencensus exporter")
-	}
+	driver := registry.GetDriver(conf.MetricsDataDriverType)
 
-	// periodic record metrics data call; every 5 seconds
-	go func() {
-		for {
-			if err := m.recordMetrics(); err != nil {
-				log.Error().Err(err).Msg("Metrics recording failed.")
-			}
-			<-time.After(time.Millisecond * time.Duration(5000))
+	if driver != nil {
+		// configure the driver
+		driver.Configure(conf)
+
+		m := &Metrics{
+			dataDriver:           driver,
+			NumUsersMeasure:      stats.Int64("cs3_org_sciencemesh_site_total_num_users", "The total number of users within this site", stats.UnitDimensionless),
+			NumGroupsMeasure:     stats.Int64("cs3_org_sciencemesh_site_total_num_groups", "The total number of groups within this site", stats.UnitDimensionless),
+			AmountStorageMeasure: stats.Int64("cs3_org_sciencemesh_site_total_amount_storage", "The total amount of storage used within this site", stats.UnitBytes),
 		}
-	}()
+
+		if err := view.Register(
+			m.getNumUsersView(),
+			m.getNumGroupsView(),
+			m.getAmountStorageView(),
+		); err != nil {
+			log.Error().Msg("error registering the driver's views with opencensus exporter")
+		}
+
+		// periodically record metrics data
+		go func() {
+			for {
+				if err := m.recordMetrics(); err != nil {
+					log.Error().Err(err).Msg("Metrics recording failed.")
+				}
+				<-time.After(time.Millisecond * time.Duration(conf.MetricsRecordInterval))
+			}
+		}()
+	} else {
+		log.Info().Msg("No metrics are being recorded.")
+	}
 }
 
 // Metrics the metrics struct
 type Metrics struct {
-	dataDriver           Reader // the metrics data driver is an implemention of Reader
+	dataDriver           reader.Reader // the metrics data driver is an implemention of Reader
 	NumUsersMeasure      *stats.Int64Measure
 	NumGroupsMeasure     *stats.Int64Measure
 	AmountStorageMeasure *stats.Int64Measure
@@ -76,49 +85,12 @@ type Metrics struct {
 
 // RecordMetrics records the latest metrics from the metrics data source as OpenCensus stats views.
 func (m *Metrics) recordMetrics() error {
-	if err := initDataDriver(m); err != nil {
-		log.Error().Err(err).Msg("Could not set a driver")
-		return err
-	}
 	// record all latest metrics
-	m.recordNumUsers()
-	m.recordNumGroups()
-	m.recordAmountStorage()
-
-	return nil
-}
-
-// initDataDriver initializes a data driver and sets it to be the Metrics.dataDriver
-func initDataDriver(m *Metrics) error {
-	driverType := sharedconf.GetMetricsDataDriverType()
-
-	// create/init a driver depending on driver type
-	if driverType == "json" {
-		// create a json driver
-		jsonDriver, err := json.New()
-		if err != nil {
-			log.Error().Err(err)
-			return err
-		}
-		m.dataDriver = jsonDriver
-		log.Debug().Msgf("Metrics uses json driver")
+	if m.dataDriver != nil {
+		m.recordNumUsers()
+		m.recordNumGroups()
+		m.recordAmountStorage()
 	}
-	if driverType == "dummy" && m.dataDriver == nil {
-		// create the dummy driver
-		dummyDriver, err := dummy.New()
-		if err != nil {
-			log.Error().Err(err)
-			return err
-		}
-		m.dataDriver = dummyDriver
-		log.Debug().Msgf("Metrics uses dummy driver")
-	}
-	// no known driver configured, return error
-	if m.dataDriver == nil {
-		err := errors.New("Unable to initialize a metrics data driver. Has a correct driver type (one of: json, dummy) been configured?")
-		return err
-	}
-
 	return nil
 }
 
@@ -165,25 +137,4 @@ func (m *Metrics) getAmountStorageView() *view.View {
 		Measure:     m.AmountStorageMeasure,
 		Aggregation: view.LastValue(),
 	}
-}
-
-// Reader is the interface that defines the metrics to read.
-// Any metrics data driver must implement this interface.
-// Each function should return the current/latest available metrics figure relevant to that function.
-type Reader interface {
-
-	// GetNumUsersView returns an OpenCensus stats view which records the
-	// number of users registered in the mesh provider.
-	// Metric name: cs3_org_sciencemesh_site_total_num_users
-	GetNumUsers() int64
-
-	// GetNumGroupsView returns an OpenCensus stats view which records the
-	// number of user groups registered in the mesh provider.
-	// Metric name: cs3_org_sciencemesh_site_total_num_groups
-	GetNumGroups() int64
-
-	// GetAmountStorageView returns an OpenCensus stats view which records the
-	// amount of storage in the system.
-	// Metric name: cs3_org_sciencemesh_site_total_amount_storage
-	GetAmountStorage() int64
 }
