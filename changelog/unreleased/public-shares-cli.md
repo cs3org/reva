04840506fe4a2d9e314f37bb4b2ec272bd63f525
Ishank Arora <ishank011@gmail.com>
2020-11-23 19:05:03 +0530

    Add CLI commands for public shares (#1328)

---
+++ changelog/unreleased/public-shares-cli.md
@@ -0,0 +1,3 @@
+Enhancement: Add CLI commands for public shares
+
+https://github.com/cs3org/reva/pull/1328
---
+++ cmd/reva/public-share-create.go
@@ -0,0 +1,107 @@
+// Copyright 2018-2020 CERN
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// In applying this license, CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+package main
+
+import (
+	"io"
+	"os"
+	"time"
+
+	rpc "github.com/cs3org/go-cs3apis/cs3/rpc/v1beta1"
+	link "github.com/cs3org/go-cs3apis/cs3/sharing/link/v1beta1"
+	provider "github.com/cs3org/go-cs3apis/cs3/storage/provider/v1beta1"
+	"github.com/jedib0t/go-pretty/table"
+	"github.com/pkg/errors"
+)
+
+func publicShareCreateCommand() *command {
+	cmd := newCommand("public-share-create")
+	cmd.Description = func() string { return "create a public share" }
+	cmd.Usage = func() string { return "Usage: public-share-create [-flags] <path>" }
+	rol := cmd.String("rol", "viewer", "the permission for the share (viewer or editor)")
+
+	cmd.ResetFlags = func() {
+		*rol = "viewer"
+	}
+
+	cmd.Action = func(w ...io.Writer) error {
+		if cmd.NArg() < 1 {
+			return errors.New("Invalid arguments: " + cmd.Usage())
+		}
+
+		fn := cmd.Args()[0]
+
+		ctx := getAuthContext()
+		client, err := getClient()
+		if err != nil {
+			return err
+		}
+
+		ref := &provider.Reference{
+			Spec: &provider.Reference_Path{Path: fn},
+		}
+
+		req := &provider.StatRequest{Ref: ref}
+		res, err := client.Stat(ctx, req)
+		if err != nil {
+			return err
+		}
+
+		if res.Status.Code != rpc.Code_CODE_OK {
+			return formatError(res.Status)
+		}
+
+		perm, err := getSharePerm(*rol)
+		if err != nil {
+			return err
+		}
+
+		grant := &link.Grant{
+			Permissions: &link.PublicSharePermissions{
+				Permissions: perm,
+			},
+		}
+		shareRequest := &link.CreatePublicShareRequest{
+			ResourceInfo: res.Info,
+			Grant:        grant,
+		}
+
+		shareRes, err := client.CreatePublicShare(ctx, shareRequest)
+		if err != nil {
+			return err
+		}
+
+		if shareRes.Status.Code != rpc.Code_CODE_OK {
+			return formatError(shareRes.Status)
+		}
+
+		t := table.NewWriter()
+		t.SetOutputMirror(os.Stdout)
+		t.AppendHeader(table.Row{"#", "Owner.Idp", "Owner.OpaqueId", "ResourceId", "Permissions", "Token", "Expiration", "Created", "Updated"})
+
+		s := shareRes.Share
+		t.AppendRows([]table.Row{
+			{s.Id.OpaqueId, s.Owner.Idp, s.Owner.OpaqueId, s.ResourceId.String(), s.Permissions.String(), s.Token, s.Expiration.String(), time.Unix(int64(s.Ctime.Seconds), 0), time.Unix(int64(s.Mtime.Seconds), 0)},
+		})
+		t.Render()
+
+		return nil
+	}
+	return cmd
+}
