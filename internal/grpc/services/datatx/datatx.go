36c1d92412af7046022f7b58fd08fb3368bc5f2b
Antoon Prins <antoon.prins@surfsara.nl>
2021-01-06 13:09:53 +0100

    Persist transfer

--- internal/grpc/services/datatx/datatx.go
+++ internal/grpc/services/datatx/datatx.go
@@ -22,11 +22,16 @@ import (
 	"context"
 	"fmt"
 	"strconv"
+	"time"
 
 	storageprovider "github.com/cs3org/go-cs3apis/cs3/storage/provider/v1beta1"
 	datatx "github.com/cs3org/go-cs3apis/cs3/tx/v1beta1"
+	"github.com/cs3org/reva/pkg/appctx"
 	txdriver "github.com/cs3org/reva/pkg/datatx"
 	"github.com/cs3org/reva/pkg/datatx/manager/registry"
+	txpersistency "github.com/cs3org/reva/pkg/datatx/persistency"
+	txpersistencyregistry "github.com/cs3org/reva/pkg/datatx/persistency/registry"
+	"github.com/cs3org/reva/pkg/datatx/registry"
 	"github.com/cs3org/reva/pkg/rgrpc"
 	"github.com/cs3org/reva/pkg/rgrpc/status"
 	"github.com/cs3org/reva/pkg/user"
@@ -44,16 +49,36 @@ func (c *config) init() {
 	if c.Driver == "" {
 		c.Driver = "rclone"
 	}
+	if c.JobStatusCheckInterval == 0 {
+		c.JobStatusCheckInterval = 2000
+	}
+	return nil
 }
 
 type config struct {
-	Driver  string                            `mapstructure:"driver"`
-	Drivers map[string]map[string]interface{} `mapstructure:"drivers"`
+	Driver                 string                            `mapstructure:"driver"`
+	Drivers                map[string]map[string]interface{} `mapstructure:"drivers"`
+	PersistencyDriver      string                            `mapstructure:"persistency_driver"`
+	PersistencyDrivers     map[string]map[string]interface{} `mapstructure:"persistency_drivers"`
+	JobStatusCheckInterval int                               `mapstructure:"job_status_check_interval"`
+	JobTimeout             int                               `mapstructure:"job_timeout"`
 }
 
 type service struct {
-	conf     *config
-	txDriver txdriver.TxDriver
+	conf                *config
+	txDriver            txdriver.TxDriver
+	txPersistencyDriver txpersistency.Driver
+}
+
+// txEndStatuses final statuses that cannot be changed anymore
+var txEndStatuses = map[string]int32{
+	"STATUS_INVALID":                0,
+	"STATUS_DESTINATION_NOT_FOUND":  1,
+	"STATUS_TRANSFER_COMPLETE":      6,
+	"STATUS_TRANSFER_FAILED":        7,
+	"STATUS_TRANSFER_CANCELLED":     8,
+	"STATUS_TRANSFER_CANCEL_FAILED": 9,
+	"STATUS_TRANSFER_EXPIRED":       10,
 }
 
 func (s *service) Register(ss *grpc.Server) {
@@ -76,22 +101,39 @@ func getDriver(c *config) (txdriver.TxDriver, error) {
 	return nil, fmt.Errorf("datatx driver not found: %s", c.Driver)
 }
 
+func getPersistencyDriver(c *config) (txpersistency.Driver, error) {
+	if f, ok := txpersistencyregistry.NewFuncs[c.PersistencyDriver]; ok {
+		return f(c.PersistencyDrivers[c.PersistencyDriver])
+	}
+	return nil, fmt.Errorf("datatx persistency driver not found: %s", c.PersistencyDriver)
+}
+
 // New creates a new datatx svc
 func New(m map[string]interface{}, ss *grpc.Server) (rgrpc.Service, error) {
 	c, err := parseConfig(m)
 	if err != nil {
 		return nil, err
 	}
-	c.init()
+	err = c.init()
+	if err != nil {
+		err = errors.Wrap(err, "error setting config defaults for datatx service")
+		return nil, err
+	}
 
 	driver, err := getDriver(c)
 	if err != nil {
 		return nil, err
 	}
 
+	persistencyDriver, err := getPersistencyDriver(c)
+	if err != nil {
+		return nil, err
+	}
+
 	service := &service{
-		conf:     c,
-		txDriver: driver,
+		conf:                c,
+		txDriver:            driver,
+		txPersistencyDriver: persistencyDriver,
 	}
 
 	return service, nil
@@ -121,9 +163,7 @@ func (s *service) CreateTransfer(ctx context.Context, req *datatx.CreateTransfer
 	// . rclone does NOT fail/error when the src can not be found: TODO ? check if scr exists ?
 	//   however: a get status call should give an error anyway
 	// ----------------------------------------------------------------------------------------
-
-	// TODO create a new transfer id
-	var txID int64
+	log := appctx.GetLogger(ctx)
 
 	u, ok := user.ContextGetUser(ctx)
 	if !ok {
@@ -135,13 +175,27 @@ func (s *service) CreateTransfer(ctx context.Context, req *datatx.CreateTransfer
 
 	// Temp implementation is depending on current rclone version.
 	// (token is username for now, it is used like that in the current rclone driver implementation)
-	// TODO: re-implement when the connection-string-remote syntax is available in rclone
+	// TODO: re-implement once the connection-string-remote syntax is available in rclone
 	// TODO: implement proper token use
+
 	srcToken := u.GetUsername()
 	destToken := req.GetGrantee().GetId().GetOpaqueId()
 
 	destRemote := req.GetGrantee().GetId().GetIdp()
 	destPath := req.GetRef().GetPath()
+
+	transfer := &txpersistency.Transfer{
+		TransferStatus: datatx.TxInfo_Status_value[datatx.TxInfo_STATUS_TRANSFER_NEW.String()],
+		SrcRemote:      srcRemote,
+		SrcPath:        srcPath,
+		DestRemote:     destRemote,
+		DestPath:       destPath,
+	}
+
+	savedTransfer, err := s.txPersistencyDriver.SaveTransfer(transfer)
+	if err != nil {
+		return nil, err
+	}
 
 	jobID, err := s.txDriver.DoTransfer(srcRemote, srcPath, srcToken, destRemote, destPath, destToken)
 	if err != nil {
@@ -157,14 +211,80 @@ func (s *service) CreateTransfer(ctx context.Context, req *datatx.CreateTransfer
 		txStatus = datatx.TxInfo_STATUS_TRANSFER_IN_PROGRESS
 	}
 
-	// TODO return actual transfer id; let job id be the transfer id for now
-	txID = jobID
+	savedTransfer.JobID = jobID
+	savedTransfer.TransferStatus = datatx.TxInfo_Status_value[txStatus.String()]
+
+	// update the new transfer with the job ID and status
+	updatedTransfer, err := s.txPersistencyDriver.SaveTransfer(savedTransfer)
+	if err != nil {
+		return nil, err
+	}
+
+	transferID := updatedTransfer.TransferID
+
+	// Periodically checks the status of the transfer until it is finished,
+	// by consulting the tx driver for the current status and update the transfer accordingly
+	//
+	go func() {
+		// runs as long as no end state or time out has been reached
+		startTimeMs := time.Now().Nanosecond() / 1000
+		timeout := s.conf.JobTimeout
+		// fmt.Printf("start time: %v (timeout: %v)\n", startTimeMs, timeout)
+		for {
+			currentTimeMs := time.Now().Nanosecond() / 1000
+			timePastMs := currentTimeMs - startTimeMs
+			// fmt.Printf("current time: %v (time past: %v)\n", currentTimeMs, timePastMs)
+			if timePastMs > timeout {
+				// set status to EXPIRED
+				fmt.Printf("JOB TIMED OUT\n")
+				_, err := s.expireJob(transferID)
+				if err != nil {
+					log.Err(err).Msg("could not expire transfer job")
+				}
+				break
+			}
+
+			// get and verify the persisted transfer status first;
+			// a transfer end status may have been reached
+			transfer, err := s.txPersistencyDriver.GetTransfer(transferID)
+			if err != nil {
+				log.Err(err).Msg("error retrieving transfer job status")
+				break
+			}
+			currentTransferStatus := transfer.TransferStatus
+			currentTxStatus := datatx.TxInfo_Status(currentTransferStatus)
+			_, endStatusFound := txEndStatuses[currentTxStatus.String()]
+			if endStatusFound {
+				// we're done already
+				fmt.Printf("TRANSFER FINISHED: %v\n", currentTxStatus)
+				break
+			}
+
+			// get the latest transfer status from the txdriver
+			txStatus, err := s.txDriver.GetTransferStatus(jobID)
+			if err != nil {
+				log.Debug().Err(err).Msg("error retrieving transfer job status. Status returned: " + txStatus.String())
+			}
+			latestTransferStatus := datatx.TxInfo_Status_value[txStatus.String()]
+
+			// update (persist) transfer status if it has changed
+			if currentTransferStatus != latestTransferStatus {
+				err := s.updateTransferStatus(transferID, latestTransferStatus)
+				if err != nil {
+					log.Err(err).Msg("could not update transfer status")
+				}
+			}
+
+			fmt.Printf("... checking\n")
+			<-time.After(time.Millisecond * time.Duration(s.conf.JobStatusCheckInterval))
+		}
+	}()
 
 	res := &datatx.CreateTransferResponse{
 		Status: status.NewOK(ctx),
 		TxInfo: &datatx.TxInfo{
 			Id: &datatx.TxId{
-				OpaqueId: strconv.FormatInt(txID, 10),
+				OpaqueId: strconv.FormatInt(transferID, 10),
 			},
 			Ref: &storageprovider.Reference{
 				Spec: &storageprovider.Reference_Path{
@@ -174,20 +294,54 @@ func (s *service) CreateTransfer(ctx context.Context, req *datatx.CreateTransfer
 			Status: txStatus,
 		},
 	}
+
 	return res, nil
 }
 
-func (s *service) GetTransferStatus(ctx context.Context, req *datatx.GetTransferStatusRequest) (*datatx.GetTransferStatusResponse, error) {
-	// the transfer id
-	txID := req.TxId.OpaqueId
+func (s *service) expireJob(transferID int64) (datatx.TxInfo_Status, error) {
+	txStatus := datatx.TxInfo_STATUS_TRANSFER_EXPIRED
+	transfer, err := s.txPersistencyDriver.GetTransfer(transferID)
+	if err != nil {
+		return datatx.TxInfo_STATUS_INVALID, err
+	}
+	transfer.TransferStatus = datatx.TxInfo_Status_value[txStatus.String()]
+
+	expiredTransfer, err := s.txPersistencyDriver.SaveTransfer(transfer)
+	if err != nil {
+		return datatx.TxInfo_STATUS_INVALID, err
+	}
 
 	// TODO find the jobID belonging to the transfer id; let transfer id be the jobID for now
-	jobID, err := strconv.ParseInt(txID, 10, 64)
+	// jobID, err := strconv.ParseInt(txID, 10, 64)
+
+	fmt.Printf("job expired: %v\n", datatx.TxInfo_Status(expiredTransfer.TransferStatus))
+	return datatx.TxInfo_Status(expiredTransfer.TransferStatus), nil
+}
+
+// simple transfer status update
+func (s *service) updateTransferStatus(transferID int64, transferStatus int32) error {
+	transfer, err := s.txPersistencyDriver.GetTransfer(transferID)
+	if err != nil {
+		return err
+	}
+	transfer.TransferStatus = transferStatus
+
+	_, err = s.txPersistencyDriver.SaveTransfer(transfer)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("transfer status updated: %v\n", datatx.TxInfo_Status(transferStatus))
+	return nil
+}
+
+func (s *service) GetTransferStatus(ctx context.Context, req *datatx.GetTransferStatusRequest) (*datatx.GetTransferStatusResponse, error) {
+	transferID, err := strconv.ParseInt(req.GetTxId().GetOpaqueId(), 10, 64)
 	if err != nil {
 		return nil, err
 	}
 
-	txStatus, err := s.txDriver.GetTransferStatus(jobID)
+	txStatus, err := s.getTransferJobStatus(transferID)
 	if err != nil {
 		return nil, err
 	}
@@ -196,7 +350,7 @@ func (s *service) GetTransferStatus(ctx context.Context, req *datatx.GetTransfer
 		Status: status.NewOK(ctx),
 		TxInfo: &datatx.TxInfo{
 			Id: &datatx.TxId{
-				OpaqueId: txID,
+				OpaqueId: strconv.FormatInt(transferID, 10),
 			},
 			Ref:    nil,
 			Status: txStatus,
@@ -206,17 +360,46 @@ func (s *service) GetTransferStatus(ctx context.Context, req *datatx.GetTransfer
 }
 
 func (s *service) CancelTransfer(ctx context.Context, req *datatx.CancelTransferRequest) (*datatx.CancelTransferResponse, error) {
-	// the transfer id
-	txID := req.TxId.OpaqueId
+	transferID, err := strconv.ParseInt(req.GetTxId().GetOpaqueId(), 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	transfer, err := s.txPersistencyDriver.GetTransfer(transferID)
+	if err != nil {
+		return nil, err
+	}
+	jobID := transfer.JobID
 
-	// TODO find the jobID belonging to the transfer id; let transfer id be the jobID for now
-	var jobID int64
-	jobID, err := strconv.ParseInt(txID, 10, 64)
+	currentTxStatus, err := s.getTransferJobStatus(transferID)
 	if err != nil {
 		return nil, err
 	}
 
+	// cannot cancel final statuses
+	endStatus, endStatusFound := txEndStatuses[currentTxStatus.String()]
+	if endStatusFound {
+		fmt.Printf("an end status has already been reached: %v. Cannot cancel this.\n", endStatus)
+		res := &datatx.CancelTransferResponse{
+			Status: status.NewOK(ctx),
+			TxInfo: &datatx.TxInfo{
+				Id: &datatx.TxId{
+					OpaqueId: req.GetTxId().GetOpaqueId(),
+				},
+				Ref:    nil,
+				Status: datatx.TxInfo_Status(endStatus),
+			},
+		}
+		return res, nil
+	}
+
 	txStatus, err := s.txDriver.CancelTransfer(jobID)
+	if err != nil {
+		return nil, err
+	}
+
+	// update the canceled transfer
+	transfer.TransferStatus = datatx.TxInfo_Status_value[txStatus.String()]
+	updatedTransfer, err := s.txPersistencyDriver.SaveTransfer(transfer)
 	if err != nil {
 		return nil, err
 	}
@@ -225,11 +408,23 @@ func (s *service) CancelTransfer(ctx context.Context, req *datatx.CancelTransfer
 		Status: status.NewOK(ctx),
 		TxInfo: &datatx.TxInfo{
 			Id: &datatx.TxId{
-				OpaqueId: txID,
+				OpaqueId: strconv.FormatInt(updatedTransfer.TransferID, 10),
 			},
 			Ref:    nil,
 			Status: txStatus,
 		},
 	}
 	return res, nil
+}
+
+func (s *service) getTransferJobStatus(transferID int64) (datatx.TxInfo_Status, error) {
+	fmt.Printf("checking transfer job status: %v\n", transferID)
+
+	transfer, err := s.txPersistencyDriver.GetTransfer(transferID)
+	if err != nil {
+		return datatx.TxInfo_STATUS_INVALID, err
+	}
+	txStatus := datatx.TxInfo_Status(transfer.TransferStatus)
+
+	return txStatus, nil
 }
