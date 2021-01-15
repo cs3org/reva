d7beb099819d43d9625da8aa5f274fc51e18a8c2
André Duffeck <andre.duffeck@firondu.de>
2021-01-15 13:34:08 +0100

    Initial work on the s3ng storage driver
    This commit also introduces Ginkgo/Gomega for unit testing and testify/mockery
    for mocking.

---
+++ pkg/storage/fs/s3ng/metadata.go
@@ -0,0 +1,106 @@
+// Copyright 2018-2021 CERN
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// In applying this license, CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+package s3ng
+
+import (
+	"context"
+	"path/filepath"
+
+	provider "github.com/cs3org/go-cs3apis/cs3/storage/provider/v1beta1"
+	"github.com/cs3org/reva/pkg/appctx"
+	"github.com/cs3org/reva/pkg/errtypes"
+	"github.com/pkg/errors"
+	"github.com/pkg/xattr"
+)
+
+func (fs *s3ngfs) SetArbitraryMetadata(ctx context.Context, ref *provider.Reference, md *provider.ArbitraryMetadata) (err error) {
+	n, err := fs.lu.NodeFromResource(ctx, ref)
+	if err != nil {
+		return errors.Wrap(err, "s3ngfs: error resolving ref")
+	}
+
+	if !n.Exists {
+		err = errtypes.NotFound(filepath.Join(n.ParentID, n.Name))
+		return err
+	}
+
+	ok, err := fs.p.HasPermission(ctx, n, func(rp *provider.ResourcePermissions) bool {
+		// TODO add explicit SetArbitraryMetadata grant to CS3 api, tracked in https://github.com/cs3org/cs3apis/issues/91
+		return rp.InitiateFileUpload
+	})
+	switch {
+	case err != nil:
+		return errtypes.InternalError(err.Error())
+	case !ok:
+		return errtypes.PermissionDenied(filepath.Join(n.ParentID, n.Name))
+	}
+
+	nodePath := n.lu.toInternalPath(n.ID)
+	for k, v := range md.Metadata {
+		// TODO set etag as temporary etag tmpEtagAttr
+		attrName := metadataPrefix + k
+		if err = xattr.Set(nodePath, attrName, []byte(v)); err != nil {
+			return errors.Wrap(err, "s3ngfs: could not set metadata attribute "+attrName+" to "+k)
+		}
+	}
+	return
+}
+
+func (fs *s3ngfs) UnsetArbitraryMetadata(ctx context.Context, ref *provider.Reference, keys []string) (err error) {
+	n, err := fs.lu.NodeFromResource(ctx, ref)
+	if err != nil {
+		return errors.Wrap(err, "s3ngfs: error resolving ref")
+	}
+
+	if !n.Exists {
+		err = errtypes.NotFound(filepath.Join(n.ParentID, n.Name))
+		return err
+	}
+
+	ok, err := fs.p.HasPermission(ctx, n, func(rp *provider.ResourcePermissions) bool {
+		// TODO use  SetArbitraryMetadata grant to CS3 api, tracked in https://github.com/cs3org/cs3apis/issues/91
+		return rp.InitiateFileUpload
+	})
+	switch {
+	case err != nil:
+		return errtypes.InternalError(err.Error())
+	case !ok:
+		return errtypes.PermissionDenied(filepath.Join(n.ParentID, n.Name))
+	}
+
+	nodePath := n.lu.toInternalPath(n.ID)
+	for i := range keys {
+		attrName := metadataPrefix + keys[i]
+		if err = xattr.Remove(nodePath, attrName); err != nil {
+			// a non-existing attribute will return an error, which we can ignore
+			// (using string compare because the error type is syscall.Errno and not wrapped/recognizable)
+			if e, ok := err.(*xattr.Error); !ok || !(e.Err.Error() == "no data available" ||
+				// darwin
+				e.Err.Error() == "attribute not found") {
+				appctx.GetLogger(ctx).Error().Err(err).
+					Interface("node", n).
+					Str("key", keys[i]).
+					Msg("could not unset metadata")
+			} else {
+				err = nil
+			}
+		}
+	}
+	return
+}
---
+++ pkg/storage/fs/s3ng/s3ng.go
@@ -0,0 +1,511 @@
+// Copyright 2018-2021 CERN
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// In applying this license, CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+package s3ng
+
+//go:generate mockery -name PermissionsChecker
+
+import (
+	"context"
+	"io"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+
+	userv1beta1 "github.com/cs3org/go-cs3apis/cs3/identity/user/v1beta1"
+	provider "github.com/cs3org/go-cs3apis/cs3/storage/provider/v1beta1"
+	"github.com/cs3org/reva/pkg/appctx"
+	"github.com/cs3org/reva/pkg/errtypes"
+	"github.com/cs3org/reva/pkg/logger"
+	"github.com/cs3org/reva/pkg/storage"
+	"github.com/cs3org/reva/pkg/storage/fs/registry"
+	"github.com/cs3org/reva/pkg/storage/utils/chunking"
+	"github.com/cs3org/reva/pkg/storage/utils/templates"
+	"github.com/cs3org/reva/pkg/user"
+	"github.com/mitchellh/mapstructure"
+	"github.com/pkg/errors"
+	"github.com/pkg/xattr"
+)
+
+const (
+	// TODO the below comment is currently copied from the owncloud driver, revisit
+	// Currently,extended file attributes have four separated
+	// namespaces (user, trusted, security and system) followed by a dot.
+	// A non root user can only manipulate the user. namespace, which is what
+	// we will use to store ownCloud specific metadata. To prevent name
+	// collisions with other apps We are going to introduce a sub namespace
+	// "user.ocis."
+
+	ocisPrefix   string = "user.ocis."
+	parentidAttr string = ocisPrefix + "parentid"
+	ownerIDAttr  string = ocisPrefix + "owner.id"
+	ownerIDPAttr string = ocisPrefix + "owner.idp"
+	// the base name of the node
+	// updated when the file is renamed or moved
+	nameAttr string = ocisPrefix + "name"
+
+	// grantPrefix is the prefix for sharing related extended attributes
+	grantPrefix    string = ocisPrefix + "grant."
+	metadataPrefix string = ocisPrefix + "md."
+	// TODO implement favorites metadata flag
+	//favPrefix   string = ocisPrefix + "fav."  // favorite flag, per user
+
+	// a temporary etag for a folder that is removed when the mtime propagation happens
+	tmpEtagAttr   string = ocisPrefix + "tmp.etag"
+	referenceAttr string = ocisPrefix + "cs3.ref" // arbitrary metadata
+	//checksumPrefix    string = ocisPrefix + "cs."   // TODO add checksum support
+	trashOriginAttr string = ocisPrefix + "trash.origin" // trash origin
+
+	// we use a single attribute to enable or disable propagation of both: synctime and treesize
+	propagationAttr string = ocisPrefix + "propagation"
+
+	// the tree modification time of the tree below this node,
+	// propagated when synctime_accounting is true and
+	// user.ocis.propagation=1 is set
+	// stored as a readable time.RFC3339Nano
+	treeMTimeAttr string = ocisPrefix + "tmtime"
+
+	// the size of the tree below this node,
+	// propagated when treesize_accounting is true and
+	// user.ocis.propagation=1 is set
+	//treesizeAttr string = ocisPrefix + "treesize"
+)
+
+func init() {
+	registry.Register("s3ng", NewDefault)
+}
+
+func parseConfig(m map[string]interface{}) (*Options, error) {
+	o := &Options{}
+	if err := mapstructure.Decode(m, o); err != nil {
+		err = errors.Wrap(err, "error decoding conf")
+		return nil, err
+	}
+	return o, nil
+}
+
+func (o *Options) init(m map[string]interface{}) {
+	if o.UserLayout == "" {
+		o.UserLayout = "{{.Id.OpaqueId}}"
+	}
+	// ensure user layout has no starting or trailing /
+	o.UserLayout = strings.Trim(o.UserLayout, "/")
+
+	if o.ShareFolder == "" {
+		o.ShareFolder = "/Shares"
+	}
+	// ensure share folder always starts with slash
+	o.ShareFolder = filepath.Join("/", o.ShareFolder)
+
+	// c.DataDirectory should never end in / unless it is the root
+	o.Root = filepath.Clean(o.Root)
+}
+
+type PermissionsChecker interface {
+	HasPermission(ctx context.Context, n *Node, check func(*provider.ResourcePermissions) bool) (can bool, err error)
+}
+
+func NewDefault(m map[string]interface{}) (storage.FS, error) {
+	lu := &Lookup{}
+	p := &Permissions{lu: lu}
+	return New(m, lu, p)
+}
+
+// New returns an implementation to of the storage.FS interface that talk to
+// a local filesystem.
+func New(m map[string]interface{}, lu *Lookup, permissionsChecker PermissionsChecker) (storage.FS, error) {
+	o, err := parseConfig(m)
+	if err != nil {
+		return nil, err
+	}
+	o.init(m)
+
+	lu.Options = o
+
+	// create data paths for internal layout
+	dataPaths := []string{
+		filepath.Join(o.Root, "nodes"),
+		// notes contain symlinks from nodes/<u-u-i-d>/uploads/<uploadid> to ../../uploads/<uploadid>
+		// better to keep uploads on a fast / volatile storage before a workflow finally moves them to the nodes dir
+		filepath.Join(o.Root, "uploads"),
+		filepath.Join(o.Root, "trash"),
+	}
+	for _, v := range dataPaths {
+		if err := os.MkdirAll(v, 0700); err != nil {
+			logger.New().Error().Err(err).
+				Str("path", v).
+				Msg("could not create data dir")
+		}
+	}
+
+	// the root node has an empty name
+	// the root node has no parent
+	if err = createNode(
+		&Node{lu: lu, ID: "root"},
+		&userv1beta1.UserId{
+			OpaqueId: o.Owner,
+		},
+	); err != nil {
+		return nil, err
+	}
+
+	tp, err := NewTree(lu)
+	if err != nil {
+		return nil, err
+	}
+
+	return &s3ngfs{
+		tp:           tp,
+		lu:           lu,
+		o:            o,
+		p:            permissionsChecker,
+		chunkHandler: chunking.NewChunkHandler(filepath.Join(o.Root, "uploads")),
+	}, nil
+}
+
+type Blobstore struct{}
+type s3ngfs struct {
+	lu           *Lookup
+	Blobstore    *Blobstore
+	tp           TreePersistence
+	o            *Options
+	p            PermissionsChecker
+	chunkHandler *chunking.ChunkHandler
+}
+
+func (fs *s3ngfs) Shutdown(ctx context.Context) error {
+	return nil
+}
+
+func (fs *s3ngfs) GetQuota(ctx context.Context) (int, int, error) {
+	return 0, 0, nil
+}
+
+// CreateHome creates a new root node that has no parent id
+func (fs *s3ngfs) CreateHome(ctx context.Context) (err error) {
+	if !fs.o.EnableHome || fs.o.UserLayout == "" {
+		return errtypes.NotSupported("s3ngfs: CreateHome() home supported disabled")
+	}
+
+	var n, h *Node
+	if n, err = fs.lu.RootNode(ctx); err != nil {
+		return
+	}
+	h, err = fs.lu.WalkPath(ctx, n, fs.lu.mustGetUserLayout(ctx), func(ctx context.Context, n *Node) error {
+		if !n.Exists {
+			if err := fs.tp.CreateDir(ctx, n); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+
+	if fs.o.TreeTimeAccounting {
+		homePath := h.lu.toInternalPath(h.ID)
+		// mark the home node as the end of propagation
+		if err = xattr.Set(homePath, propagationAttr, []byte("1")); err != nil {
+			appctx.GetLogger(ctx).Error().Err(err).Interface("node", h).Msg("could not mark home as propagation root")
+			return
+		}
+	}
+	return
+}
+
+// GetHome is called to look up the home path for a user
+// It is NOT supposed to return the internal path but the external path
+func (fs *s3ngfs) GetHome(ctx context.Context) (string, error) {
+	if !fs.o.EnableHome || fs.o.UserLayout == "" {
+		return "", errtypes.NotSupported("s3ngfs: GetHome() home supported disabled")
+	}
+	u := user.ContextMustGetUser(ctx)
+	layout := templates.WithUser(u, fs.o.UserLayout)
+	return filepath.Join(fs.o.Root, layout), nil // TODO use a namespace?
+}
+
+// Tree persistence
+
+// GetPathByID returns the fn pointed by the file id, without the internal namespace
+func (fs *s3ngfs) GetPathByID(ctx context.Context, id *provider.ResourceId) (string, error) {
+	return fs.tp.GetPathByID(ctx, id)
+}
+
+func (fs *s3ngfs) CreateDir(ctx context.Context, fn string) (err error) {
+	var n *Node
+	if n, err = fs.lu.NodeFromPath(ctx, fn); err != nil {
+		return
+	}
+
+	if n.Exists {
+		return errtypes.AlreadyExists(fn)
+	}
+
+	pn, err := n.Parent()
+	if err != nil {
+		return errors.Wrap(err, "s3ngfs: error getting parent "+n.ParentID)
+	}
+	ok, err := fs.p.HasPermission(ctx, pn, func(rp *provider.ResourcePermissions) bool {
+		return rp.CreateContainer
+	})
+	switch {
+	case err != nil:
+		return errtypes.InternalError(err.Error())
+	case !ok:
+		return errtypes.PermissionDenied(filepath.Join(n.ParentID, n.Name))
+	}
+
+	err = fs.tp.CreateDir(ctx, n)
+
+	if fs.o.TreeTimeAccounting {
+		nodePath := n.lu.toInternalPath(n.ID)
+		// mark the home node as the end of propagation
+		if err = xattr.Set(nodePath, propagationAttr, []byte("1")); err != nil {
+			appctx.GetLogger(ctx).Error().Err(err).Interface("node", n).Msg("could not mark node to propagate")
+			return
+		}
+	}
+	return
+}
+
+// CreateReference creates a reference as a node folder with the target stored in extended attributes
+// There is no difference between the /Shares folder and normal nodes because the storage is not supposed to be accessible without the storage provider.
+// In effect everything is a shadow namespace.
+// To mimic the eos end owncloud driver we only allow references as children of the "/Shares" folder
+// TODO when home support is enabled should the "/Shares" folder still be listed?
+func (fs *s3ngfs) CreateReference(ctx context.Context, p string, targetURI *url.URL) (err error) {
+
+	p = strings.Trim(p, "/")
+	parts := strings.Split(p, "/")
+
+	if len(parts) != 2 {
+		return errtypes.PermissionDenied("s3ngfs: references must be a child of the share folder: share_folder=" + fs.o.ShareFolder + " path=" + p)
+	}
+
+	if parts[0] != strings.Trim(fs.o.ShareFolder, "/") {
+		return errtypes.PermissionDenied("s3ngfs: cannot create references outside the share folder: share_folder=" + fs.o.ShareFolder + " path=" + p)
+	}
+
+	// create Shares folder if it does not exist
+	var n *Node
+	if n, err = fs.lu.NodeFromPath(ctx, fs.o.ShareFolder); err != nil {
+		return errtypes.InternalError(err.Error())
+	} else if !n.Exists {
+		if err = fs.tp.CreateDir(ctx, n); err != nil {
+			return
+		}
+	}
+
+	if n, err = n.Child(parts[1]); err != nil {
+		return errtypes.InternalError(err.Error())
+	}
+
+	if n.Exists {
+		// TODO append increasing number to mountpoint name
+		return errtypes.AlreadyExists(p)
+	}
+
+	if err = fs.tp.CreateDir(ctx, n); err != nil {
+		return
+	}
+
+	internal := n.lu.toInternalPath(n.ID)
+	if err = xattr.Set(internal, referenceAttr, []byte(targetURI.String())); err != nil {
+		return errors.Wrapf(err, "s3ngfs: error setting the target %s on the reference file %s", targetURI.String(), internal)
+	}
+	return nil
+}
+
+func (fs *s3ngfs) Move(ctx context.Context, oldRef, newRef *provider.Reference) (err error) {
+	var oldNode, newNode *Node
+	if oldNode, err = fs.lu.NodeFromResource(ctx, oldRef); err != nil {
+		return
+	}
+
+	if !oldNode.Exists {
+		err = errtypes.NotFound(filepath.Join(oldNode.ParentID, oldNode.Name))
+		return
+	}
+
+	ok, err := fs.p.HasPermission(ctx, oldNode, func(rp *provider.ResourcePermissions) bool {
+		return rp.Move
+	})
+	switch {
+	case err != nil:
+		return errtypes.InternalError(err.Error())
+	case !ok:
+		return errtypes.PermissionDenied(oldNode.ID)
+	}
+
+	if newNode, err = fs.lu.NodeFromResource(ctx, newRef); err != nil {
+		return
+	}
+	if newNode.Exists {
+		err = errtypes.AlreadyExists(filepath.Join(newNode.ParentID, newNode.Name))
+		return
+	}
+
+	return fs.tp.Move(ctx, oldNode, newNode)
+}
+
+func (fs *s3ngfs) GetMD(ctx context.Context, ref *provider.Reference, mdKeys []string) (ri *provider.ResourceInfo, err error) {
+	var node *Node
+	if node, err = fs.lu.NodeFromResource(ctx, ref); err != nil {
+		return
+	}
+
+	if !node.Exists {
+		err = errtypes.NotFound(filepath.Join(node.ParentID, node.Name))
+		return
+	}
+
+	ok, err := fs.p.HasPermission(ctx, node, func(rp *provider.ResourcePermissions) bool {
+		return rp.Stat
+	})
+	switch {
+	case err != nil:
+		return nil, errtypes.InternalError(err.Error())
+	case !ok:
+		return nil, errtypes.PermissionDenied(node.ID)
+	}
+
+	return node.AsResourceInfo(ctx, mdKeys)
+}
+
+func (fs *s3ngfs) ListFolder(ctx context.Context, ref *provider.Reference, mdKeys []string) (finfos []*provider.ResourceInfo, err error) {
+	var node *Node
+	if node, err = fs.lu.NodeFromResource(ctx, ref); err != nil {
+		return
+	}
+
+	if !node.Exists {
+		err = errtypes.NotFound(filepath.Join(node.ParentID, node.Name))
+		return
+	}
+
+	ok, err := fs.p.HasPermission(ctx, node, func(rp *provider.ResourcePermissions) bool {
+		return rp.ListContainer
+	})
+	switch {
+	case err != nil:
+		return nil, errtypes.InternalError(err.Error())
+	case !ok:
+		return nil, errtypes.PermissionDenied(node.ID)
+	}
+
+	var children []*Node
+	children, err = fs.tp.ListFolder(ctx, node)
+	if err != nil {
+		return
+	}
+
+	for i := range children {
+		if ri, err := children[i].AsResourceInfo(ctx, mdKeys); err == nil {
+			finfos = append(finfos, ri)
+		}
+	}
+	return
+}
+
+func (fs *s3ngfs) Delete(ctx context.Context, ref *provider.Reference) (err error) {
+	var node *Node
+	if node, err = fs.lu.NodeFromResource(ctx, ref); err != nil {
+		return
+	}
+	if !node.Exists {
+		err = errtypes.NotFound(filepath.Join(node.ParentID, node.Name))
+		return
+	}
+
+	ok, err := fs.p.HasPermission(ctx, node, func(rp *provider.ResourcePermissions) bool {
+		return rp.Delete
+	})
+	switch {
+	case err != nil:
+		return errtypes.InternalError(err.Error())
+	case !ok:
+		return errtypes.PermissionDenied(filepath.Join(node.ParentID, node.Name))
+	}
+
+	return fs.tp.Delete(ctx, node)
+}
+
+// Data persistence
+
+func (fs *s3ngfs) ContentPath(n *Node) string {
+	return n.lu.toInternalPath(n.ID)
+}
+
+func (fs *s3ngfs) Download(ctx context.Context, ref *provider.Reference) (io.ReadCloser, error) {
+	node, err := fs.lu.NodeFromResource(ctx, ref)
+	if err != nil {
+		return nil, errors.Wrap(err, "s3ngfs: error resolving ref")
+	}
+
+	if !node.Exists {
+		err = errtypes.NotFound(filepath.Join(node.ParentID, node.Name))
+		return nil, err
+	}
+
+	ok, err := fs.p.HasPermission(ctx, node, func(rp *provider.ResourcePermissions) bool {
+		return rp.InitiateFileDownload
+	})
+	switch {
+	case err != nil:
+		return nil, errtypes.InternalError(err.Error())
+	case !ok:
+		return nil, errtypes.PermissionDenied(filepath.Join(node.ParentID, node.Name))
+	}
+
+	contentPath := fs.ContentPath(node)
+
+	r, err := os.Open(contentPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, errtypes.NotFound(contentPath)
+		}
+		return nil, errors.Wrap(err, "s3ngfs: error reading "+contentPath)
+	}
+	return r, nil
+}
+
+// arbitrary metadata persistence in metadata.go
+
+// Version persistence in revisions.go
+
+// Trash persistence in recycle.go
+
+// share persistence in grants.go
+
+func (fs *s3ngfs) copyMD(s string, t string) (err error) {
+	var attrs []string
+	if attrs, err = xattr.List(s); err != nil {
+		return err
+	}
+	for i := range attrs {
+		if strings.HasPrefix(attrs[i], ocisPrefix) {
+			var d []byte
+			if d, err = xattr.Get(s, attrs[i]); err != nil {
+				return err
+			}
+			if err = xattr.Set(t, attrs[i], d); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
