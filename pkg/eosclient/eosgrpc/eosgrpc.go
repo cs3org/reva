1e7d82302fe7e986eec998eb50d510c473a9df88
Fabrizio Furano <fabrizio.furano@gmail.com>
2021-03-19 12:52:59 +0100

    eosfs: Fix two minor warnings

--- pkg/eosclient/eosgrpc/eosgrpc.go
+++ pkg/eosclient/eosgrpc/eosgrpc.go
@@ -157,7 +157,7 @@ func (c *Client) GetHTTPCl() *ehttp.Client {
 	return ehttp.New(&c.htopts, c.httptransport)
 }
 
-// GetHttpCl creates an http client for immediate usage, using the already instantiated resources
+// GetHTTPCl creates an http client for immediate usage, using the already instantiated resources
 func (c *Client) GetHTTPCl() *ehttp.EosHttpClient {
 	return ehttp.New(&c.opt.httpopts)
 }
