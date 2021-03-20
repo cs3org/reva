5c4e4d69d987c15a25f2f2c8c503c725d610c80a
Jimil Desai <jimildesai42@gmail.com>
2021-03-20 13:04:10 +0530

    fix: license header
    Signed-off-by: Jimil Desai <jimildesai42@gmail.com>

--- pkg/cbox/utils/tokenmanagement.go
+++ pkg/cbox/utils/tokenmanagement.go
@@ -1,3 +1,21 @@
+// Copyright 2018-2021 CERN
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// In applying this license, CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
 package utils
 
 import (
@@ -12,6 +30,7 @@ import (
 	"time"
 )
 
+// APITokenManager stores config related to api management
 type APITokenManager struct {
 	oidcApiToken            string
 	oidcTokenExpirationTime time.Time
@@ -80,6 +99,7 @@ func (a *APITokenManager) getAPIToken(ctx context.Context) (string, time.Time, e
 	return result["access_token"].(string), expirationTime, nil
 }
 
+// SendAPIGetRequest makes an API GET Request to the passed URL
 func (a *APITokenManager) SendAPIGetRequest(ctx context.Context, url string, forceRenewal bool) ([]interface{}, error) {
 	err := a.renewAPIToken(ctx, forceRenewal)
 	if err != nil {
