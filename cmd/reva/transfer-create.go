4e83040cf1440bddd66bbdca8c34c0bb10985f76
Antoon Prins <antoon.prins@surfsara.nl>
2021-03-22 19:04:40 +0100

    granteeType,idp added

--- cmd/reva/transfer-create.go
+++ cmd/reva/transfer-create.go
@@ -20,23 +20,38 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"io"
+	"strings"
 
+	userpb "github.com/cs3org/go-cs3apis/cs3/identity/user/v1beta1"
 	rpc "github.com/cs3org/go-cs3apis/cs3/rpc/v1beta1"
+	provider "github.com/cs3org/go-cs3apis/cs3/storage/provider/v1beta1"
 	tx "github.com/cs3org/go-cs3apis/cs3/tx/v1beta1"
 )
 
 func transferCreateCommand() *command {
 	cmd := newCommand("transfer-create")
-	cmd.Description = func() string { return "create transfer between 2 remotes" }
-	cmd.Usage = func() string { return "Usage: transfer-create [-flags]" }
+	cmd.Description = func() string { return "create transfer between 2 sites" }
+	cmd.Usage = func() string { return "Usage: transfer-create [-flags] <path>" }
 	grantee := cmd.String("grantee", "", "the grantee, receiver of the transfer")
+	granteeType := cmd.String("granteeType", "user", "the grantee type, one of: user, group")
+	idp := cmd.String("idp", "", "the idp of the grantee, default to same idp as the user triggering the action")
 
 	cmd.Action = func(w ...io.Writer) error {
-		// validate flags
+		if cmd.NArg() < 1 {
+			return errors.New("Invalid arguments: " + cmd.Usage())
+		}
+
 		if *grantee == "" {
 			return errors.New("Grantee cannot be empty: use -grantee flag\n" + cmd.Usage())
 		}
+		if *idp == "" {
+			return errors.New("Idp cannot be empty: use -idp flag\n" + cmd.Usage())
+		}
+
+		// the resource to transfer; the path
+		fn := cmd.Args()[0]
 
 		ctx := getAuthContext()
 		client, err := getClient()
@@ -44,8 +59,30 @@ func transferCreateCommand() *command {
 			return err
 		}
 
-		transferRequest := &tx.CreateTransferRequest{}
+		gt := provider.GranteeType_GRANTEE_TYPE_USER
+		if strings.ToLower(*granteeType) == "group" {
+			gt = provider.GranteeType_GRANTEE_TYPE_GROUP
+		}
 
+		transferRequest := &tx.CreateTransferRequest{
+			Ref: &provider.Reference{
+				Spec: &provider.Reference_Path{
+					Path: fn,
+				},
+			},
+			Grantee: &provider.Grantee{
+				Type: gt,
+				Id: &provider.Grantee_UserId{
+					UserId: &userpb.UserId{
+						Idp:      *idp,
+						OpaqueId: *grantee,
+					},
+				},
+			},
+		}
+
+		fmt.Println("transfer-create:")
+		fmt.Println("------------------------------------------------------------------------")
 		transferResponse, err := client.CreateTransfer(ctx, transferRequest)
 		if err != nil {
 			return err
@@ -53,6 +90,11 @@ func transferCreateCommand() *command {
 		if transferResponse.Status.Code != rpc.Code_CODE_OK {
 			return formatError(transferResponse.Status)
 		}
+
+		fmt.Printf(" response status: %v\n", transferResponse.Status)
+		fmt.Printf(" transfer ID    : %v\n", transferResponse.TxInfo.Id.OpaqueId)
+		fmt.Printf(" transfer status: %v\n", transferResponse.TxInfo.Status)
+		fmt.Println("------------------------------------------------------------------------")
 
 		return nil
 	}
