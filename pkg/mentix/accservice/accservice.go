5e795b6732ab0b04f249d19322f940ad69405dd9
Daniel Müller <daniel.mueller@uni-muenster.de>
2021-03-25 16:22:31 +0100

    Check accounts service response status

--- pkg/mentix/accservice/accservice.go
+++ pkg/mentix/accservice/accservice.go
@@ -53,7 +53,7 @@ func Query(endpoint string, params network.URLParams) (*RequestResponse, error) 
 		return nil, errors.Wrap(err, "error while building the service accounts query URL")
 	}
 
-	data, err := network.ReadEndpoint(fullURL, &network.BasicAuth{User: settings.User, Password: settings.Password}, false)
+	data, err := network.ReadEndpoint(fullURL, &network.BasicAuth{User: settings.User, Password: settings.Password}, true)
 	if err != nil {
 		return nil, errors.Wrap(err, "unable to query the service accounts endpoint")
 	}
