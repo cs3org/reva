f6f2cc2e0c7a8d00b337f7989c14f86b77e6e610
Hugo Gonzalez Labrador <github@hugo.labkode.com>
2021-03-31 16:35:43 +0200

    allow full paths targets

--- internal/http/services/owncloud/ocs/handlers/apps/sharing/shares/shares.go
+++ internal/http/services/owncloud/ocs/handlers/apps/sharing/shares/shares.go
@@ -702,11 +702,12 @@ func (h *Handler) listSharesWithMe(w http.ResponseWriter, r *http.Request) {
 
 		if data.State == ocsStateAccepted {
 			// Needed because received shares can be jailed in a folder in the users home
-			data.FileTarget = path.Join(h.sharePrefix, path.Base(info.Path))
-			data.Path = path.Join(h.sharePrefix, path.Base(info.Path))
+			data.FileTarget = path.Join(h.sharePrefix, info.Path)
+			data.Path = path.Join(h.sharePrefix, info.Path)
 		}
 
 		shares = append(shares, data)
+		log.Info().Msgf("listSharesWithMe: %+v", *data)
 	}
 
 	response.WriteOCSSuccess(w, r, shares)
@@ -844,7 +845,7 @@ func (h *Handler) addFileInfo(ctx context.Context, s *conversions.ShareData, inf
 		// TODO Storage: int
 		s.ItemSource = wrapResourceID(info.Id)
 		s.FileSource = s.ItemSource
-		s.FileTarget = path.Join("/", path.Base(info.Path))
+		s.FileTarget = path.Join("/", info.Path)
 		s.Path = path.Join("/", path.Base(info.Path)) // TODO hm this might have to be relative to the users home ... depends on the webdav_namespace config
 		// TODO FileParent:
 		// item type
