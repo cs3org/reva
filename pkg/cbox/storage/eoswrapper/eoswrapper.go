ba1a1c17b24a25a5c4fa5bfdb1b16c146b9a51a9
Ishank Arora <ishank011@gmail.com>
2021-04-08 13:36:43 +0200

    Remove unused import

--- pkg/cbox/storage/eoswrapper/eoswrapper.go
+++ pkg/cbox/storage/eoswrapper/eoswrapper.go
@@ -23,7 +23,6 @@ import (
 
 	userpb "github.com/cs3org/go-cs3apis/cs3/identity/user/v1beta1"
 	provider "github.com/cs3org/go-cs3apis/cs3/storage/provider/v1beta1"
-	"github.com/cs3org/reva/pkg/appctx"
 	"github.com/cs3org/reva/pkg/storage"
 	"github.com/cs3org/reva/pkg/storage/fs/registry"
 	"github.com/cs3org/reva/pkg/storage/utils/eosfs"
