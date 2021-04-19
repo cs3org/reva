d3cab83ae22260ea442f889da221e1d425528bff
Daniel Müller <daniel.mueller@uni-muenster.de>
2021-04-19 13:12:21 +0200

    Add further labels to site scheduled metric

--- pkg/mentix/exchangers/exporters/metrics/metrics.go
+++ pkg/mentix/exchangers/exporters/metrics/metrics.go
@@ -39,9 +39,10 @@ type Metrics struct {
 }
 
 const (
-	keySiteID   = "site_id"
-	keySiteName = "site"
-	keySiteType = "site_type"
+	keySiteID      = "site_id"
+	keySiteName    = "site"
+	keySiteType    = "site_type"
+	keyServiceType = "service_type"
 )
 
 func (m *Metrics) initialize(conf *config.Configuration, log *zerolog.Logger) error {
@@ -69,7 +70,7 @@ func (m *Metrics) registerMetrics() error {
 		Name:        m.isScheduledStats.Name(),
 		Description: m.isScheduledStats.Description(),
 		Measure:     m.isScheduledStats,
-		TagKeys:     []tag.Key{tag.MustNewKey(keySiteID), tag.MustNewKey(keySiteName), tag.MustNewKey(keySiteType)},
+		TagKeys:     []tag.Key{tag.MustNewKey(keySiteID), tag.MustNewKey(keySiteName), tag.MustNewKey(keySiteType), tag.MustNewKey(keyServiceType)},
 		Aggregation: view.LastValue(),
 	}
 
@@ -96,6 +97,7 @@ func (m *Metrics) exportSiteMetrics(site *meshdata.Site) error {
 	mutators = append(mutators, tag.Insert(tag.MustNewKey(keySiteID), site.ID))
 	mutators = append(mutators, tag.Insert(tag.MustNewKey(keySiteName), site.Name))
 	mutators = append(mutators, tag.Insert(tag.MustNewKey(keySiteType), meshdata.GetSiteTypeName(site.Type)))
+	mutators = append(mutators, tag.Insert(tag.MustNewKey(keyServiceType), "SCIENCEMESH_HCHECK"))
 
 	// Create a new context to serve the metrics
 	if ctx, err := tag.New(context.Background(), mutators...); err == nil {
