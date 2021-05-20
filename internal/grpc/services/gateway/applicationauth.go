3d6941ed6db4120157eaae5346867c845ed3f2bf
Ishank Arora <ishank011@gmail.com>
2021-05-20 11:15:11 +0200

    Add stubs and driver interface for app token management

---
+++ internal/grpc/services/gateway/applicationauth.go
@@ -0,0 +1,51 @@
+// Copyright 2018-2021 CERN
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// In applying this license, CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+package gateway
+
+import (
+	"context"
+
+	appauthpb "github.com/cs3org/go-cs3apis/cs3/auth/applications/v1beta1"
+	"github.com/cs3org/reva/pkg/errtypes"
+	"github.com/cs3org/reva/pkg/rgrpc/status"
+)
+
+func (s *svc) GenerateAppPassword(ctx context.Context, req *appauthpb.GenerateAppPasswordRequest) (*appauthpb.GenerateAppPasswordResponse, error) {
+	return &appauthpb.GenerateAppPasswordResponse{
+		Status: status.NewUnimplemented(ctx, errtypes.NotSupported("GenerateAppPassword not implemented"), "GenerateAppPassword not implemented"),
+	}, nil
+}
+
+func (s *svc) ListAppPasswords(ctx context.Context, req *appauthpb.ListAppPasswordsRequest) (*appauthpb.ListAppPasswordsResponse, error) {
+	return &appauthpb.ListAppPasswordsResponse{
+		Status: status.NewUnimplemented(ctx, errtypes.NotSupported("GenerateAppPassword not implemented"), "GenerateAppPassword not implemented"),
+	}, nil
+}
+
+func (s *svc) InvalidateAppPassword(ctx context.Context, req *appauthpb.InvalidateAppPasswordRequest) (*appauthpb.InvalidateAppPasswordResponse, error) {
+	return &appauthpb.InvalidateAppPasswordResponse{
+		Status: status.NewUnimplemented(ctx, errtypes.NotSupported("GenerateAppPassword not implemented"), "GenerateAppPassword not implemented"),
+	}, nil
+}
+
+func (s *svc) GetAppPassword(ctx context.Context, req *appauthpb.GetAppPasswordRequest) (*appauthpb.GetAppPasswordResponse, error) {
+	return &appauthpb.GetAppPasswordResponse{
+		Status: status.NewUnimplemented(ctx, errtypes.NotSupported("GetAppPassword not implemented"), "GetAppPassword not implemented"),
+	}, nil
+}
