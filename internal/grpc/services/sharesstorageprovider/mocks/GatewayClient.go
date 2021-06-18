e3c0f18b3dd995d66f3af7a07d2d9c77fd8e4073
André Duffeck <andre.duffeck@firondu.de>
2021-06-18 09:29:30 +0200

    Implement Move

--- internal/grpc/services/sharesstorageprovider/mocks/GatewayClient.go
+++ internal/grpc/services/sharesstorageprovider/mocks/GatewayClient.go
@@ -138,6 +138,36 @@ func (_m *GatewayClient) ListContainer(ctx context.Context, in *providerv1beta1.
 	return r0, r1
 }
 
+// Move provides a mock function with given fields: ctx, in, opts
+func (_m *GatewayClient) Move(ctx context.Context, in *providerv1beta1.MoveRequest, opts ...grpc.CallOption) (*providerv1beta1.MoveResponse, error) {
+	_va := make([]interface{}, len(opts))
+	for _i := range opts {
+		_va[_i] = opts[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx, in)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	var r0 *providerv1beta1.MoveResponse
+	if rf, ok := ret.Get(0).(func(context.Context, *providerv1beta1.MoveRequest, ...grpc.CallOption) *providerv1beta1.MoveResponse); ok {
+		r0 = rf(ctx, in, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*providerv1beta1.MoveResponse)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, *providerv1beta1.MoveRequest, ...grpc.CallOption) error); ok {
+		r1 = rf(ctx, in, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // Stat provides a mock function with given fields: ctx, in, opts
 func (_m *GatewayClient) Stat(ctx context.Context, in *providerv1beta1.StatRequest, opts ...grpc.CallOption) (*providerv1beta1.StatResponse, error) {
 	_va := make([]interface{}, len(opts))
