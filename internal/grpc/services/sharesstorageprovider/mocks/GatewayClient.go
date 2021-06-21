c1f36d6328f84718aaeae013d6da56468b094e1c
André Duffeck <andre.duffeck@firondu.de>
2021-06-21 10:48:29 +0200

    Upload InitiateFileUpload

--- internal/grpc/services/sharesstorageprovider/mocks/GatewayClient.go
+++ internal/grpc/services/sharesstorageprovider/mocks/GatewayClient.go
@@ -108,6 +108,36 @@ func (_m *GatewayClient) InitiateFileDownload(ctx context.Context, req *provider
 	return r0, r1
 }
 
+// InitiateFileUpload provides a mock function with given fields: ctx, req, opts
+func (_m *GatewayClient) InitiateFileUpload(ctx context.Context, req *providerv1beta1.InitiateFileUploadRequest, opts ...grpc.CallOption) (*gatewayv1beta1.InitiateFileUploadResponse, error) {
+	_va := make([]interface{}, len(opts))
+	for _i := range opts {
+		_va[_i] = opts[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx, req)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	var r0 *gatewayv1beta1.InitiateFileUploadResponse
+	if rf, ok := ret.Get(0).(func(context.Context, *providerv1beta1.InitiateFileUploadRequest, ...grpc.CallOption) *gatewayv1beta1.InitiateFileUploadResponse); ok {
+		r0 = rf(ctx, req, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*gatewayv1beta1.InitiateFileUploadResponse)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, *providerv1beta1.InitiateFileUploadRequest, ...grpc.CallOption) error); ok {
+		r1 = rf(ctx, req, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // ListContainer provides a mock function with given fields: ctx, in, opts
 func (_m *GatewayClient) ListContainer(ctx context.Context, in *providerv1beta1.ListContainerRequest, opts ...grpc.CallOption) (*providerv1beta1.ListContainerResponse, error) {
 	_va := make([]interface{}, len(opts))
