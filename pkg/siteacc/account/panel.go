f5e7a74a4ee672e71cbbd8faf52f9bf634d3b8f0
Daniel Müller <daniel.mueller@uni-muenster.de>
2021-07-16 13:41:49 +0200

    Debugging

--- pkg/siteacc/account/panel.go
+++ pkg/siteacc/account/panel.go
@@ -19,6 +19,7 @@
 package account
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
 
@@ -128,6 +129,9 @@ func (panel *Panel) Execute(w http.ResponseWriter, r *http.Request, session *htm
 
 func (panel *Panel) redirect(path string, w http.ResponseWriter, r *http.Request) html.ExecutionResult {
 	// Modify the original request URI by replacing the path parameter
+	fmt.Println(r.RequestURI)
+	fmt.Println(r.URL)
+	fmt.Println(r.Host)
 	newURI, _ := url.Parse(r.RequestURI)
 	params := newURI.Query()
 	params.Del("path")
