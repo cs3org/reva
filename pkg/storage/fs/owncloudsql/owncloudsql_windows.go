c7bafcfe41f6cc5f0b516252df4aad0dda29b479
Jörn Friedrich Dreyer <jfd@butonic.de>
2021-08-11 13:04:13 +0000

    quota stubs for owncloudsql
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

--- pkg/storage/fs/owncloudsql/owncloudsql_windows.go
+++ pkg/storage/fs/owncloudsql/owncloudsql_windows.go
@@ -52,3 +52,22 @@ func calcEtag(ctx context.Context, fi os.FileInfo) string {
 	etag := fmt.Sprintf(`"%x"`, h.Sum(nil))
 	return fmt.Sprintf("\"%s\"", strings.Trim(etag, "\""))
 }
+
+func (fs *owncloudsqlfs) GetQuota(ctx context.Context) (uint64, uint64, error) {
+	// TODO quota of which storage space?
+	// we could use the logged in user, but when a user has access to multiple storages this falls short
+	// for now return quota of root
+	var free, total, avail uint64
+
+	pathPtr, err := windows.UTF16PtrFromString(fs.toInternalPath(ctx, "/"))
+	if err != nil {
+		return 0, 0, err
+	}
+	err = windows.GetDiskFreeSpaceEx(pathPtr, &avail, &total, &free)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	used := total - free
+	return total, used, nil
+}
