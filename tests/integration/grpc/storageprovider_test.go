8d6ced0c765f3611a43b860af7fd2a735a99254e
Michiel de Jong <michiel@unhosted.org>
2021-10-06 16:56:45 +0200

    restore

--- tests/integration/grpc/storageprovider_test.go
+++ tests/integration/grpc/storageprovider_test.go
@@ -466,7 +466,7 @@ var _ = Describe("storage providers", func() {
 		})
 	}
 
-	FDescribe("nextcloud", func() {
+	Describe("nextcloud", func() {
 		BeforeEach(func() {
 			dependencies = map[string]string{
 				"storage": "storageprovider-nextcloud.toml",
