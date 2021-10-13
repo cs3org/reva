c65a0ac0697ad6d53baa8308bc28f62e7253b296
Michiel de Jong <michiel@unhosted.org>
2021-10-13 10:48:47 +0200

    Copy nd adapt pkg/ocm/share/manager/nextcloud/ from pkg/share/manager/nextcloud/

--- pkg/ocm/share/manager/nextcloud/nextcloud_suite_test.go
+++ pkg/ocm/share/manager/nextcloud/nextcloud_suite_test.go
@@ -16,11 +16,16 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-package loader
+package nextcloud_test
 
 import (
-	// Load core share manager drivers.
-	_ "github.com/cs3org/reva/pkg/ocm/share/manager/json"
-	_ "github.com/cs3org/reva/pkg/ocm/share/manager/nextcloud"
-	// Add your own here
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
+
+func TestNextcloud(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Nextcloud Suite")
+}
