7100c8ed4ba3ead4ed4399ce212a0d31d1eacc79
David Christofas <dchristofas@owncloud.com>
2021-10-27 13:08:21 +0200

    load public shares and set the share-type for shared resources

--- internal/http/services/owncloud/ocdav/versions.go
+++ internal/http/services/owncloud/ocdav/versions.go
@@ -164,7 +164,7 @@ func (h *VersionsHandler) doListVersions(w http.ResponseWriter, r *http.Request,
 		infos = append(infos, vi)
 	}
 
-	propRes, err := s.multistatusResponse(ctx, &pf, infos, "")
+	propRes, err := s.multistatusResponse(ctx, &pf, infos, "", nil)
 	if err != nil {
 		sublog.Error().Err(err).Msg("error formatting propfind")
 		w.WriteHeader(http.StatusInternalServerError)
