4cc6eef697843de54cd6068b13bcd9a8c9c720aa
Ishank Arora <ishank011@gmail.com>
2021-11-10 12:49:46 +0100

    Return user type in the response of the ocs GET user call

--- internal/http/services/owncloud/ocs/handlers/cloud/user/user.go
+++ internal/http/services/owncloud/ocs/handlers/cloud/user/user.go
@@ -22,6 +22,7 @@ import (
 	"fmt"
 	"net/http"
 
+	userv1beta1 "github.com/cs3org/go-cs3apis/cs3/identity/user/v1beta1"
 	"github.com/cs3org/reva/internal/http/services/owncloud/ocs/response"
 	ctxpkg "github.com/cs3org/reva/pkg/ctx"
 )
@@ -45,6 +46,7 @@ func (h *Handler) GetSelf(w http.ResponseWriter, r *http.Request) {
 		ID:          u.Username,
 		DisplayName: u.DisplayName,
 		Email:       u.Mail,
+		UserType:    userTypeString(u.Id.Type),
 	})
 }
 
@@ -54,4 +56,25 @@ type User struct {
 	ID          string `json:"id" xml:"id"`
 	DisplayName string `json:"display-name" xml:"display-name"`
 	Email       string `json:"email" xml:"email"`
+	UserType    string `json:"user-type" xml:"user-type"`
+}
+
+func userTypeString(userType userv1beta1.UserType) string {
+	switch userType {
+	case userv1beta1.UserType_USER_TYPE_PRIMARY:
+		return "primary"
+	case userv1beta1.UserType_USER_TYPE_SECONDARY:
+		return "secondary"
+	case userv1beta1.UserType_USER_TYPE_SERVICE:
+		return "service"
+	case userv1beta1.UserType_USER_TYPE_APPLICATION:
+		return "application"
+	case userv1beta1.UserType_USER_TYPE_GUEST:
+		return "guest"
+	case userv1beta1.UserType_USER_TYPE_FEDERATED:
+		return "federated"
+	case userv1beta1.UserType_USER_TYPE_LIGHTWEIGHT:
+		return "lightweight"
+	}
+	return "invalid"
 }
