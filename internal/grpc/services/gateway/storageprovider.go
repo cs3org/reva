5cad7e1a8a5403c2f601a729750b9eb0eafe487e
Jörn Friedrich Dreyer <jfd@butonic.de>
2021-11-12 12:16:56 +0000

    better handle relative references
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

--- internal/grpc/services/gateway/storageprovider.go
+++ internal/grpc/services/gateway/storageprovider.go
@@ -645,7 +645,9 @@ func (s *svc) Stat(ctx context.Context, req *provider.StatRequest) (*provider.St
 
 		var currentInfo *provider.ResourceInfo
 		switch {
-		case req.Ref.Path == "", req.Ref.Path == ".": // id based or root of a space request
+		case req.Ref.Path == "": // id based request
+			fallthrough
+		case strings.HasPrefix(req.Ref.Path, "."): // space request
 			fallthrough
 		case providers[i].ProviderPath == req.Ref.Path: // matches
 			fallthrough
