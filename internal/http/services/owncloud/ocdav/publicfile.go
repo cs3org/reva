14b10231443ee6cf691f4799651a1269f8b52d37
Jörn Friedrich Dreyer <jfd@butonic.de>
2021-11-22 14:01:42 +0000

    make content disposition reflect request base path
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

--- internal/http/services/owncloud/ocdav/publicfile.go
+++ internal/http/services/owncloud/ocdav/publicfile.go
@@ -55,6 +55,7 @@ func (h *PublicFileHandler) Handler(s *svc) http.Handler {
 				return
 			}
 
+			r.URL.Path = path.Base(r.URL.Path)
 			switch r.Method {
 			case MethodPropfind:
 				s.handlePropfindOnToken(w, r, h.namespace, false)
@@ -119,6 +120,9 @@ func (s *svc) adjustResourcePathInURL(w http.ResponseWriter, r *http.Request) bo
 		w.WriteHeader(http.StatusConflict)
 		return false
 	}
+
+	// adjust path in request URL to point at the parent
+	r.URL.Path = path.Dir(r.URL.Path)
 
 	return true
 }
