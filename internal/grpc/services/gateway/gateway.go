ddd966191edfcec88529950724eeafd4155969fa
Jörn Friedrich Dreyer <jfd@butonic.de>
2021-11-24 15:34:29 +0000

    comment unused mount cache for now
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

--- internal/grpc/services/gateway/gateway.go
+++ internal/grpc/services/gateway/gateway.go
@@ -73,6 +73,7 @@ type config struct {
 	AllowedUserAgents   map[string][]string               `mapstructure:"allowed_user_agents"` // map[path][]user-agent
 	CreateHomeCacheTTL  int                               `mapstructure:"create_home_cache_ttl"`
 	ProviderCacheTTL    int                               `mapstructure:"provider_cache_ttl"`
+	//MountCacheTTL       int                               `mapstructure:"mount_cache_ttl"`
 }
 
 // sets defaults
@@ -126,6 +127,7 @@ type svc struct {
 	etagCache       *ttlcache.Cache `mapstructure:"etag_cache"`
 	createHomeCache *ttlcache.Cache `mapstructure:"create_home_cache"`
 	providerCache   *ttlcache.Cache `mapstructure:"provider_cache"`
+	//mountCache      *ttlcache.Cache `mapstructure:"mount_cache"`
 }
 
 // New creates a new gateway svc that acts as a proxy for any grpc operation.
@@ -163,6 +165,10 @@ func New(m map[string]interface{}, ss *grpc.Server) (rgrpc.Service, error) {
 	_ = providerCache.SetTTL(time.Duration(c.ProviderCacheTTL) * time.Second)
 	providerCache.SkipTTLExtensionOnHit(true)
 
+	//mountCache := ttlcache.NewCache()
+	//_ = mountCache.SetTTL(time.Duration(c.MountCacheTTL) * time.Second)
+	//mountCache.SkipTTLExtensionOnHit(true)
+
 	s := &svc{
 		c:               c,
 		dataGatewayURL:  *u,
@@ -170,6 +176,7 @@ func New(m map[string]interface{}, ss *grpc.Server) (rgrpc.Service, error) {
 		etagCache:       etagCache,
 		createHomeCache: createHomeCache,
 		providerCache:   providerCache,
+		//mountCache:      mountCache,
 	}
 
 	return s, nil
