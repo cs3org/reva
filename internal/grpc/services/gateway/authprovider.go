9f35dd4f3866d38cfed0a8e8e094a169bdf8cd3b
Ishank Arora <ishank011@gmail.com>
2022-01-10 15:30:53 +0100

    Fix lint

--- internal/grpc/services/gateway/authprovider.go
+++ internal/grpc/services/gateway/authprovider.go
@@ -21,7 +21,6 @@ package gateway
 import (
 	"context"
 	"fmt"
-	"strings"
 
 	authpb "github.com/cs3org/go-cs3apis/cs3/auth/provider/v1beta1"
 	provider "github.com/cs3org/go-cs3apis/cs3/auth/provider/v1beta1"
@@ -29,17 +28,13 @@ import (
 	gateway "github.com/cs3org/go-cs3apis/cs3/gateway/v1beta1"
 	userpb "github.com/cs3org/go-cs3apis/cs3/identity/user/v1beta1"
 	rpc "github.com/cs3org/go-cs3apis/cs3/rpc/v1beta1"
-	collaboration "github.com/cs3org/go-cs3apis/cs3/sharing/collaboration/v1beta1"
-	link "github.com/cs3org/go-cs3apis/cs3/sharing/link/v1beta1"
 	storageprovider "github.com/cs3org/go-cs3apis/cs3/storage/provider/v1beta1"
 	"github.com/cs3org/reva/pkg/appctx"
-	"github.com/cs3org/reva/pkg/auth/scope"
 	ctxpkg "github.com/cs3org/reva/pkg/ctx"
 	"github.com/cs3org/reva/pkg/errtypes"
 	"github.com/cs3org/reva/pkg/rgrpc/status"
 	"github.com/cs3org/reva/pkg/rgrpc/todo/pool"
 	"github.com/cs3org/reva/pkg/sharedconf"
-	"github.com/cs3org/reva/pkg/utils"
 	"github.com/pkg/errors"
 	"google.golang.org/grpc/metadata"
 )
@@ -121,8 +116,11 @@ func (s *svc) Authenticate(ctx context.Context, req *gateway.AuthenticateRequest
 	ctx = ctxpkg.ContextSetToken(ctx, token)
 	ctx = ctxpkg.ContextSetUser(ctx, res.User)
 	ctx = metadata.AppendToOutgoingContext(ctx, ctxpkg.TokenHeader, token)
-	/* Commenting out as the token size can get too big
-	scope, err := s.expandScopes(ctx, res.TokenScope)
+
+	// Commenting out as the token size can get too big
+	// For now, we'll try to resolve all resources on every request
+	// TODO(ishank011): Add a cache for these
+	/* scope, err := s.expandScopes(ctx, res.TokenScope)
 	if err != nil {
 		err = errors.Wrap(err, "authsvc: error expanding token scope")
 		return &gateway.AuthenticateResponse{
@@ -244,6 +242,7 @@ func (s *svc) findAuthProvider(ctx context.Context, authType string) (provider.P
 	return nil, errtypes.InternalError("gateway: error finding an auth provider for type: " + authType)
 }
 
+/*
 func (s *svc) expandScopes(ctx context.Context, scopeMap map[string]*authpb.Scope) (map[string]*authpb.Scope, error) {
 	log := appctx.GetLogger(ctx)
 	newMap := make(map[string]*authpb.Scope)
@@ -314,3 +313,4 @@ func (s *svc) statAndAddResource(ctx context.Context, r *storageprovider.Resourc
 
 	return scope.AddResourceInfoScope(statResponse.Info, role, scopeMap)
 }
+*/
