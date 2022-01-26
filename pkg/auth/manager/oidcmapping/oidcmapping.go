ef8531ea924ec6b1b99cf44e5fbd0005af8a1d8d
Giuseppe Lo Presti <giuseppe.lopresti@cern.ch>
2022-01-26 11:56:42 +0100

    Removed incorrect config option for oidcmapping

--- pkg/auth/manager/oidcmapping/oidcmapping.go
+++ pkg/auth/manager/oidcmapping/oidcmapping.go
@@ -56,15 +56,14 @@ type mgr struct {
 }
 
 type config struct {
-	Insecure        bool   `mapstructure:"insecure" docs:"false;Whether to skip certificate checks when sending requests."`
-	Issuer          string `mapstructure:"issuer" docs:";The issuer of the OIDC token."`
-	IDClaim         string `mapstructure:"id_claim" docs:"sub;The claim containing the ID of the user."`
-	UIDClaim        string `mapstructure:"uid_claim" docs:";The claim containing the UID of the user."`
-	GIDClaim        string `mapstructure:"gid_claim" docs:";The claim containing the GID of the user."`
-	GatewaySvc      string `mapstructure:"gatewaysvc" docs:";The endpoint at which the GRPC gateway is exposed."`
-	UserProviderSvc string `mapstructure:"userprovidersvc" docs:";The endpoint at which the GRPC userprovider is exposed."`
-	UsersMapping    string `mapstructure:"users_mapping" docs:"; The optional OIDC users mapping file path"`
-	GroupClaim      string `mapstructure:"group_claim" docs:"; The group claim to be looked up to map the user (default to 'groups')."`
+	Insecure     bool   `mapstructure:"insecure" docs:"false;Whether to skip certificate checks when sending requests."`
+	Issuer       string `mapstructure:"issuer" docs:";The issuer of the OIDC token."`
+	IDClaim      string `mapstructure:"id_claim" docs:"sub;The claim containing the ID of the user."`
+	UIDClaim     string `mapstructure:"uid_claim" docs:";The claim containing the UID of the user."`
+	GIDClaim     string `mapstructure:"gid_claim" docs:";The claim containing the GID of the user."`
+	GatewaySvc   string `mapstructure:"gatewaysvc" docs:";The endpoint at which the GRPC gateway is exposed."`
+	UsersMapping string `mapstructure:"users_mapping" docs:"; The optional OIDC users mapping file path"`
+	GroupClaim   string `mapstructure:"group_claim" docs:"; The group claim to be looked up to map the user (default to 'groups')."`
 }
 
 type oidcUserMapping struct {
@@ -318,7 +317,7 @@ func (am *mgr) resolveUser(ctx context.Context, claims map[string]interface{}) e
 			username = am.oidcUsersMapping[m.(string)].Username
 		}
 
-		upsc, err := pool.GetUserProviderServiceClient(am.c.UserProviderSvc)
+		upsc, err := pool.GetUserProviderServiceClient(am.c.GatewaySvc)
 		if err != nil {
 			return errors.Wrap(err, "error getting user provider grpc client")
 		}
