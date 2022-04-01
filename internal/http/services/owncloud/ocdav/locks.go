6733bcfd37def173467a2507fa5aecbcf6db3e96
Willy Kloucek <wkloucek@owncloud.com>
2022-04-01 15:13:45 +0200

    use precondition failed in webdav too

--- internal/http/services/owncloud/ocdav/locks.go
+++ internal/http/services/owncloud/ocdav/locks.go
@@ -488,7 +488,7 @@ func (s *svc) lockReference(ctx context.Context, w http.ResponseWriter, r *http.
 		//      this actually is a name based lock ... ugh
 		token, err = s.LockSystem.Create(ctx, now, ld)
 		if err != nil {
-			if _, ok := err.(errtypes.Locked); ok {
+			if _, ok := err.(errtypes.PreconditionFailed); ok {
 				return http.StatusLocked, err
 			}
 			return http.StatusInternalServerError, err
