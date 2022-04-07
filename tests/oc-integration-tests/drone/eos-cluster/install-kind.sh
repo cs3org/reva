14efa55e4b8fb0973d03d1849820c8466d43694a
Samuel Alfageme Sainz <samuel@alfage.me>
2022-04-07 10:41:47 +0200

    Remove leftover scripts and intall helm alongside kind/kubectl

--- tests/oc-integration-tests/drone/eos-cluster/install-kind.sh
+++ tests/oc-integration-tests/drone/eos-cluster/install-kind.sh
@@ -7,17 +7,29 @@ set -x;
 
 KIND=v0.11.0
 KUBECTL=v1.21.0
+HELM=v3.8.1
 
 install(){
   wget -O /usr/local/bin/$1 $2
   chmod +x /usr/local/bin/$1
 }
 
+install_helm(){
+  wget $1
+  tar -zxvf helm-${HELM}-linux-amd64.tar.gz
+  mv linux-amd64/helm /usr/local/bin/helm
+  rm -rf helm-${HELM}-linux-amd64.tar.gz linux-amd64
+  chmod +x /usr/local/bin/helm
+}
+
 # installing kind
 install "kind" "https://github.com/kubernetes-sigs/kind/releases/download/${KIND}/kind-linux-amd64"
 
-#installing kubectl
+# installing kubectl
 install "kubectl" "https://storage.googleapis.com/kubernetes-release/release/${KUBECTL}/bin/linux/amd64/kubectl"
+
+# installing helm
+install_helm "https://get.helm.sh/helm-${HELM}-linux-amd64.tar.gz"
 
 #installing nginx ingress controller
 #https://kind.sigs.k8s.io/docs/user/ingress/#ingress-nginx
