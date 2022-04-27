ee981ccd9a2cf628e6e20473f8253a604e1c8e17
jkoberg <jkoberg@owncloud.com>
2022-04-27 19:42:30 +0200

    fallback to providerid if empty
    Signed-off-by: jkoberg <jkoberg@owncloud.com>

--- internal/grpc/services/storageprovider/storageprovider.go
+++ internal/grpc/services/storageprovider/storageprovider.go
@@ -715,6 +715,9 @@ func (s *service) Stat(ctx context.Context, req *provider.StatRequest) (*provide
 		}, nil
 	}
 
+	if providerID == "" {
+		providerID = s.conf.MountID
+	}
 	md.Id.StorageId = resourceid.StorageIDWrap(md.Id.GetStorageId(), providerID)
 	return &provider.StatResponse{
 		Status: status.NewOK(ctx),
