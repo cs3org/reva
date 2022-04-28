b4615a9e43afd7e477c565038515c0263a5c601d
Ishank Arora <ishank011@gmail.com>
2022-04-28 10:56:05 +0200

    Minor fixes in cephfs and eosfs

--- pkg/storage/utils/eosfs/eosfs.go
+++ pkg/storage/utils/eosfs/eosfs.go
@@ -1269,11 +1269,23 @@ func (fs *eosfs) ListFolder(ctx context.Context, ref *provider.Reference, mdKeys
 		return nil, errors.Wrap(err, "eosfs: error resolving reference")
 	}
 
-	// if path is home we need to add in the response any shadow folder in the shadow homedirectory.
 	if fs.conf.EnableHome {
 		return fs.listWithHome(ctx, p)
 	}
 
+	return fs.listWithNominalHome(ctx, p)
+}
+
+func (fs *eosfs) listWithHome(ctx context.Context, p string) ([]*provider.ResourceInfo, error) {
+	if fs.isShareFolderRoot(ctx, p) {
+		return fs.listShareFolderRoot(ctx, p)
+	}
+
+	if fs.isShareFolderChild(ctx, p) {
+		return nil, errtypes.PermissionDenied("eos: error listing folders inside the shared folder, only file references are stored inside")
+	}
+
+	// path points to a resource in the nominal home
 	return fs.listWithNominalHome(ctx, p)
 }
 
@@ -1306,66 +1318,12 @@ func (fs *eosfs) listWithNominalHome(ctx context.Context, p string) (finfos []*p
 		}
 
 		// Remove the hidden folders in the topmost directory
-		if finfo, err := fs.convertToResourceInfo(ctx, eosFileInfo); err == nil && finfo.Path != "/" && !strings.HasPrefix(finfo.Path, "/.") {
+		if finfo, err := fs.convertToResourceInfo(ctx, eosFileInfo); err == nil &&
+			finfo.Path != "/" && !strings.HasPrefix(finfo.Path, "/.") {
 			finfos = append(finfos, finfo)
 		}
 	}
 
-	return finfos, nil
-}
-
-func (fs *eosfs) listWithHome(ctx context.Context, p string) ([]*provider.ResourceInfo, error) {
-	if p == "/" {
-		return fs.listHome(ctx)
-	}
-
-	if fs.isShareFolderRoot(ctx, p) {
-		return fs.listShareFolderRoot(ctx, p)
-	}
-
-	if fs.isShareFolderChild(ctx, p) {
-		return nil, errtypes.PermissionDenied("eosfs: error listing folders inside the shared folder, only file references are stored inside")
-	}
-
-	// path points to a resource in the nominal home
-	return fs.listWithNominalHome(ctx, p)
-}
-
-func (fs *eosfs) listHome(ctx context.Context) ([]*provider.ResourceInfo, error) {
-	fns := []string{fs.wrap(ctx, "/"), fs.wrapShadow(ctx, "/")}
-
-	u, err := getUser(ctx)
-	if err != nil {
-		return nil, errors.Wrap(err, "eosfs: no user in ctx")
-	}
-	// lightweight accounts don't have home folders, so we're passing an empty string as path
-	auth, err := fs.getUserAuth(ctx, u, "")
-	if err != nil {
-		return nil, err
-	}
-
-	finfos := []*provider.ResourceInfo{}
-	for _, fn := range fns {
-		eosFileInfos, err := fs.c.List(ctx, auth, fn)
-		if err != nil {
-			return nil, errors.Wrap(err, "eosfs: error listing")
-		}
-
-		for _, eosFileInfo := range eosFileInfos {
-			// filter out sys files
-			if !fs.conf.ShowHiddenSysFiles {
-				base := path.Base(eosFileInfo.File)
-				if hiddenReg.MatchString(base) {
-					continue
-				}
-			}
-
-			if finfo, err := fs.convertToResourceInfo(ctx, eosFileInfo); err == nil && finfo.Path != "/" && !strings.HasPrefix(finfo.Path, "/.") {
-				finfos = append(finfos, finfo)
-			}
-		}
-
-	}
 	return finfos, nil
 }
 
@@ -2174,8 +2132,8 @@ func (fs *eosfs) convert(ctx context.Context, eosFileInfo *eosclient.FileInfo) (
 	}
 
 	var xs provider.ResourceChecksum
-	if eosFileInfo.XS != nil {
-		xs.Sum = eosFileInfo.XS.XSSum
+	if eosFileInfo.Size != 0 && eosFileInfo.XS != nil {
+		xs.Sum = strings.TrimLeft(eosFileInfo.XS.XSSum, "0")
 		switch eosFileInfo.XS.XSType {
 		case "adler":
 			xs.Type = provider.ResourceChecksumType_RESOURCE_CHECKSUM_TYPE_ADLER32
