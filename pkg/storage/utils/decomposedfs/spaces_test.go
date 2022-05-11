6b8165e3d68a750c9d19c9e619f52f79b95f1556
Jörn Friedrich Dreyer <jfd@butonic.de>
2022-05-11 10:19:06 +0000

    unfocus test
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

--- pkg/storage/utils/decomposedfs/spaces_test.go
+++ pkg/storage/utils/decomposedfs/spaces_test.go
@@ -113,7 +113,7 @@ var _ = Describe("Spaces", func() {
 		})
 	})
 
-	FDescribe("ReadSpaceAndNodeFromSpaceTypeLink", func() {
+	Describe("ReadSpaceAndNodeFromSpaceTypeLink", func() {
 		var (
 			tmpdir string
 		)
