1191605b88709b69d49381fe8d6c07882c73da3b
Christian Richter <crichter@owncloud.com>
2022-07-14 15:29:34 +0200

    add missing error output
    Signed-off-by: Christian Richter <crichter@owncloud.com>

--- pkg/storage/utils/decomposedfs/tree/migrations.go
+++ pkg/storage/utils/decomposedfs/tree/migrations.go
@@ -107,7 +107,12 @@ func (t *Tree) migration0002SpaceTypes() error {
 	_, err = d.Readdirnames(1) // Or f.Readdir(1)
 	if err == io.EOF {
 		// directory is empty we can delete
-		os.Remove(spaceTypesPath)
+		err := os.Remove(spaceTypesPath)
+		if err != nil {
+			logger.New().Error().Err(err).
+				Str("spacetypesdir", d.Name()).
+				Msg("could not delete")
+		}
 	} else {
 		logger.New().Error().Err(err).
 			Str("spacetypesdir", d.Name()).
