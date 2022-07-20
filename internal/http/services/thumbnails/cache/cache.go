c4883cd0a62fb135ebcc0b29695eb9aa257db477
Gianmaria Del Monte <g.macmount@gmail.com>
2022-07-20 18:24:41 +0200

    add etag in the cache

--- internal/http/services/thumbnails/cache/cache.go
+++ internal/http/services/thumbnails/cache/cache.go
@@ -1,8 +1,8 @@
 package cache
 
 type Cache interface {
-	Get(file string, width, height int) ([]byte, error)
-	Set(file string, width, height int, data []byte) error
+	Get(file, etag string, width, height int) ([]byte, error)
+	Set(file, etag string, width, height int, data []byte) error
 }
 
 type noCache struct{}
@@ -11,10 +11,10 @@ func NewNoCache() Cache {
 	return noCache{}
 }
 
-func (noCache) Get(_ string, _, _ int) ([]byte, error) {
+func (noCache) Get(_, _ string, _, _ int) ([]byte, error) {
 	return nil, ErrNotFound{}
 }
 
-func (noCache) Set(_ string, _, _ int, _ []byte) error {
+func (noCache) Set(_, _ string, _, _ int, _ []byte) error {
 	return nil
 }
