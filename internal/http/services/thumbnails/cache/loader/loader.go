e1a2b0c6c60a00d2dc014d8ce66e2d4285e92ece
Gianmaria Del Monte <g.macmount@gmail.com>
2022-07-20 09:12:00 +0200

    implemented inmemory lru cache for thumbnails

---
+++ internal/http/services/thumbnails/cache/loader/loader.go
@@ -0,0 +1,7 @@
+package loader
+
+import (
+	// Load cache driver for thumbnails service.
+	_ "github.com/cs3org/reva/internal/http/services/thumbnails/cache/lru"
+	// Add your own here
+)
---
+++ internal/http/services/thumbnails/cache/lru/lru.go
@@ -0,0 +1,68 @@
+package lru
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/bluele/gcache"
+	"github.com/cs3org/reva/internal/http/services/thumbnails/cache"
+	"github.com/cs3org/reva/internal/http/services/thumbnails/cache/registry"
+	"github.com/mitchellh/mapstructure"
+	"github.com/pkg/errors"
+)
+
+func init() {
+	registry.Register("lru", New)
+}
+
+type lru struct {
+	config *config
+	cache  gcache.Cache
+}
+
+type config struct {
+	Size       int `mapstructure:"size"`
+	Expiration int `mapstructure:"expiration"`
+}
+
+func New(conf map[string]interface{}) (cache.Cache, error) {
+	c := &config{}
+	err := mapstructure.Decode(conf, c)
+	if err != nil {
+		return nil, errors.Wrap(err, "lru: error decoding config")
+	}
+	c.init()
+
+	svc := &lru{
+		config: c,
+		cache:  gcache.New(c.Size).LRU().Build(),
+	}
+
+	return svc, nil
+}
+
+func (c *config) init() {
+	if c.Size == 0 {
+		c.Size = 1000000
+	}
+	if c.Expiration == 0 {
+		c.Expiration = 300
+	}
+}
+
+func getKey(file string, width, height int) string {
+	return fmt.Sprintf("%s:%d:%d", file, width, height)
+}
+
+func (l *lru) Get(file string, width, height int) ([]byte, error) {
+	key := getKey(file, width, height)
+	if value, err := l.cache.Get(key); err == nil {
+		return value.([]byte), nil
+	}
+	return nil, cache.ErrNotFound{}
+}
+
+func (l *lru) Set(file string, width, height int, data []byte) error {
+	key := getKey(file, width, height)
+	return l.cache.SetWithExpire(key, data, time.Duration(l.config.Expiration)*time.Second)
+}
