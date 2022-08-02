c9a0950284c04b7fe0c67f461b42a89f26239e84
Kyle Fearne <kyle.fearne.19@um.edu.mt>
2022-08-02 11:39:14 +0200

    Creating skeleton for http

---
+++ internal/http/services/cback/cback.go
@@ -0,0 +1,81 @@
+// Copyright 2018-2021 CERN
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// In applying this license, CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+package cback
+
+import (
+	"net/http"
+
+	"github.com/cs3org/reva/pkg/appctx"
+	"github.com/cs3org/reva/pkg/rhttp/global"
+	"github.com/mitchellh/mapstructure"
+	"github.com/rs/zerolog"
+)
+
+func init() {
+	global.Register("cback", New)
+}
+
+// New returns a new helloworld service
+func New(m map[string]interface{}, log *zerolog.Logger) (global.Service, error) {
+	conf := &config{}
+	if err := mapstructure.Decode(m, conf); err != nil {
+		return nil, err
+	}
+
+	conf.init()
+
+	return &svc{conf: conf}, nil
+}
+
+// Close performs cleanup.
+func (s *svc) Close() error {
+	return nil
+}
+
+type config struct {
+	Prefix string `mapstructure:"prefix"`
+}
+
+func (c *config) init() {
+
+	if c.Prefix == "" {
+		c.Prefix = "cback"
+	}
+}
+
+type svc struct {
+	conf *config
+}
+
+func (s *svc) Prefix() string {
+	return s.conf.Prefix
+}
+
+func (s *svc) Unprotected() []string {
+	return nil
+}
+
+func (s *svc) Handler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log := appctx.GetLogger(r.Context())
+		if _, err := w.Write([]byte("hello")); err != nil {
+			log.Err(err).Msg("error writing response")
+		}
+	})
+}
