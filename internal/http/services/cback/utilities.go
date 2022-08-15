42541114404c4ab0165196dc5a1f43f35a9c0d57
Kyle Fearne <kyle.fearne.19@um.edu.mt>
2022-08-15 16:47:48 +0200

    Fix of issues flagged by hound bot

--- internal/http/services/cback/utilities.go
+++ internal/http/services/cback/utilities.go
@@ -3,8 +3,12 @@ package cback
 import (
 	"encoding/json"
 	"errors"
+	"io"
+	"net/http"
 	"strconv"
 	"strings"
+
+	"github.com/cs3org/reva/pkg/errtypes"
 )
 
 type backUpResponse struct {
@@ -46,8 +50,7 @@ func (s *svc) pathTrimmer(snapshotList []snapshotResponse, resp *backUpResponse)
 func (s *svc) listSnapshots(userName string, backupID int) ([]snapshotResponse, error) {
 
 	url := s.conf.APIURL + "/backups/" + strconv.Itoa(backupID) + "/snapshots"
-	requestType := "GET"
-	responseData, err := s.Request(userName, url, requestType, nil)
+	responseData, err := s.getRequest(userName, url, http.MethodGet, nil)
 
 	if err != nil {
 		return nil, err
@@ -57,7 +60,11 @@ func (s *svc) listSnapshots(userName string, backupID int) ([]snapshotResponse, 
 
 	/*Unmarshalling the JSON response into the Response struct*/
 	responseObject := []snapshotResponse{}
-	json.NewDecoder(responseData).Decode(&responseObject)
+	err = json.NewDecoder(responseData).Decode(&responseObject)
+
+	if err != nil {
+		return nil, err
+	}
 
 	return responseObject, nil
 }
@@ -65,8 +72,7 @@ func (s *svc) listSnapshots(userName string, backupID int) ([]snapshotResponse, 
 func (s *svc) matchBackups(userName, pathInput string) (*backUpResponse, error) {
 
 	url := s.conf.APIURL + "/backups/"
-	requestType := "GET"
-	responseData, err := s.Request(userName, url, requestType, nil)
+	responseData, err := s.getRequest(userName, url, http.MethodGet, nil)
 
 	if err != nil {
 		return nil, err
@@ -76,42 +82,48 @@ func (s *svc) matchBackups(userName, pathInput string) (*backUpResponse, error) 
 
 	/*Unmarshalling the JSON response into the Response struct*/
 	responseObject := []backUpResponse{}
-	json.NewDecoder(responseData).Decode(&responseObject)
+	err = json.NewDecoder(responseData).Decode(&responseObject)
 
-	for i := range responseObject {
-		if responseObject[i].Detail != "" {
-			err = errors.New(responseObject[i].Detail)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, response := range responseObject {
+		if response.Detail != "" {
+			err = errors.New(response.Detail)
 			return nil, err
 		}
 
-		if strings.Compare(pathInput, responseObject[i].Source) == 0 {
-			return &responseObject[i], nil
+		if strings.Compare(pathInput, response.Source) == 0 {
+			return &response, nil
 		}
 	}
 
-	for i := range responseObject {
-		if responseObject[i].Detail != "" {
-			err = errors.New(responseObject[i].Detail)
+	for _, response := range responseObject {
+		if response.Detail != "" {
+			err = errors.New(response.Detail)
 			return nil, err
 		}
 
-		if strings.HasPrefix(pathInput, responseObject[i].Source) {
-			substr := strings.TrimPrefix(pathInput, responseObject[i].Source)
+		if strings.HasPrefix(pathInput, response.Source) {
+			substr := strings.TrimPrefix(pathInput, response.Source)
 			substr = strings.TrimLeft(substr, "/")
-			responseObject[i].Substring = substr
-			return &responseObject[i], nil
+			response.Substring = substr
+			return &response, nil
 		}
 	}
 
+	/*If there is no error, but also no match found in the backup path the response is nil.
+	This means that the LSFolder function will know that no match has been found using the exact path,
+	and will therefore start checking if there is a substring of the backup job included in the inputted path*/
 	return nil, nil
 }
 
-func (s *svc) statResource(backupID int, snapID, userName, path, source string) error {
+func (s *svc) checkFileType(backupID int, snapID, userName, path, source string) error {
 
 	url := s.conf.APIURL + "/backups/" + strconv.Itoa(backupID) + "/snapshots/" + snapID + "/" + path + "?content=false"
-	requestType := "OPTIONS"
 
-	responseData, err := s.Request(userName, url, requestType, nil)
+	responseData, err := s.getRequest(userName, url, http.MethodOptions, nil)
 
 	if err != nil {
 		return err
@@ -120,4 +132,44 @@ func (s *svc) statResource(backupID int, snapID, userName, path, source string) 
 	defer responseData.Close()
 
 	return nil
+}
+
+func (s *svc) getRequest(userName, url string, reqType string, body io.Reader) (io.ReadCloser, error) {
+
+	req, err := http.NewRequest(reqType, url, body)
+
+	req.SetBasicAuth(userName, s.conf.ImpersonatorToken)
+
+	if body != nil {
+		req.Header.Add("Content-Type", "application/json")
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("accept", `application/json`)
+
+	resp, err := s.client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode < 200 && resp.StatusCode >= 300 {
+
+		switch resp.StatusCode {
+		case http.StatusNotFound:
+			return nil, errtypes.NotFound("cback: resource not found")
+		case http.StatusForbidden:
+			return nil, errtypes.PermissionDenied("cback: user has no permissions to get the backup")
+		case http.StatusBadRequest:
+			return nil, errtypes.BadRequest("cback")
+		default:
+			return nil, errtypes.InternalError("cback: internal server error")
+		}
+
+	}
+
+	return resp.Body, nil
 }
