4064230eae7cff0d81e7cbc3cf2d95f95ff1a260
Gianmaria Del Monte <g.macmount@gmail.com>
2022-09-14 15:51:22 +0200

    Fix check nested resource for public shares

--- internal/grpc/interceptors/auth/scope.go
+++ internal/grpc/interceptors/auth/scope.go
@@ -186,6 +186,13 @@ func checkCacheForNestedResource(ctx context.Context, ref *provider.Reference, r
 	return errtypes.PermissionDenied("request is not for a nested resource")
 }
 
+func isRelativePathOrEmpty(path string) bool {
+	if len(path) == 0 {
+		return true
+	}
+	return path[0] != '/'
+}
+
 func checkIfNestedResource(ctx context.Context, ref *provider.Reference, parent *provider.ResourceId, client gateway.GatewayAPIClient, mgr token.Manager) (bool, error) {
 	// Since the resource ID is obtained from the scope, the current token
 	// has access to it.
@@ -200,30 +207,33 @@ func checkIfNestedResource(ctx context.Context, ref *provider.Reference, parent 
 
 	// We mint a token as the owner of the public share and try to stat the reference
 	// TODO(ishank011): We need to find a better alternative to this
+	childPath := ref.GetPath()
+	if isRelativePathOrEmpty(ref.GetPath()) {
+		userResp, err := client.GetUser(ctx, &userpb.GetUserRequest{UserId: statResponse.Info.Owner, SkipFetchingUserGroups: true})
+		if err != nil || userResp.Status.Code != rpc.Code_CODE_OK {
+			return false, err
+		}
 
-	userResp, err := client.GetUser(ctx, &userpb.GetUserRequest{UserId: statResponse.Info.Owner, SkipFetchingUserGroups: true})
-	if err != nil || userResp.Status.Code != rpc.Code_CODE_OK {
-		return false, err
-	}
+		scope, err := scope.AddOwnerScope(map[string]*authpb.Scope{})
+		if err != nil {
+			return false, err
+		}
+		token, err := mgr.MintToken(ctx, userResp.User, scope)
+		if err != nil {
+			return false, err
+		}
+		ctx = metadata.AppendToOutgoingContext(context.Background(), ctxpkg.TokenHeader, token)
 
-	scope, err := scope.AddOwnerScope(map[string]*authpb.Scope{})
-	if err != nil {
-		return false, err
-	}
-	token, err := mgr.MintToken(ctx, userResp.User, scope)
-	if err != nil {
-		return false, err
-	}
-	ctx = metadata.AppendToOutgoingContext(context.Background(), ctxpkg.TokenHeader, token)
+		childStat, err := client.Stat(ctx, &provider.StatRequest{Ref: ref})
+		if err != nil {
+			return false, err
+		}
+		if childStat.Status.Code != rpc.Code_CODE_OK {
+			return false, statuspkg.NewErrorFromCode(childStat.Status.Code, "auth interceptor")
+		}
+		childPath = childStat.Info.Path
 
-	childStat, err := client.Stat(ctx, &provider.StatRequest{Ref: ref})
-	if err != nil {
-		return false, err
 	}
-	if childStat.Status.Code != rpc.Code_CODE_OK {
-		return false, statuspkg.NewErrorFromCode(childStat.Status.Code, "auth interceptor")
-	}
-	childPath := childStat.Info.Path
 
 	return strings.HasPrefix(childPath, parentPath), nil
 
