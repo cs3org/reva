8465c0ae15025630cabb3a794045f602bf2a0b16
Gianmaria Del Monte <g.macmount@gmail.com>
2022-10-26 17:06:43 +0200

    Revert "stat as root"
    This reverts commit e865e6e2f8d75d3759f2e3e0c8ca657a2c6ade6b.

--- pkg/storage/utils/eosfs/eosfs.go
+++ pkg/storage/utils/eosfs/eosfs.go
@@ -1222,7 +1222,23 @@ func (fs *eosfs) GetMD(ctx context.Context, ref *provider.Reference, mdKeys []st
 	log := appctx.GetLogger(ctx)
 	log.Info().Msg("eosfs: get md for ref:" + ref.String())
 
-	auth, err := fs.getRootAuth(ctx)
+	u, err := getUser(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	fn := ""
+	if u.Id.Type == userpb.UserType_USER_TYPE_LIGHTWEIGHT ||
+		u.Id.Type == userpb.UserType_USER_TYPE_FEDERATED {
+		p, err := fs.resolve(ctx, ref)
+		if err != nil {
+			return nil, errors.Wrap(err, "eosfs: error resolving reference")
+		}
+
+		fn = fs.wrap(ctx, p)
+	}
+
+	auth, err := fs.getUserAuth(ctx, u, fn)
 	if err != nil {
 		return nil, err
 	}
@@ -1249,7 +1265,7 @@ func (fs *eosfs) GetMD(ctx context.Context, ref *provider.Reference, mdKeys []st
 		}
 	}
 
-	fn := fs.wrap(ctx, p)
+	fn = fs.wrap(ctx, p)
 	eosFileInfo, err := fs.c.GetFileInfoByPath(ctx, auth, fn)
 	if err != nil {
 		return nil, err
