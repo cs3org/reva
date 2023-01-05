688e97a4a6b3a3fe37fcbcd3b5b9ae0c609ef5eb
Miroslav Bauer <mirekys@users.noreply.github.com>
2023-01-05 11:12:46 +0100

    remove meshdirectory service from reva

--- internal/http/services/loader/loader.go
+++ internal/http/services/loader/loader.go
@@ -27,7 +27,6 @@ import (
 	_ "github.com/cs3org/reva/internal/http/services/helloworld"
 	_ "github.com/cs3org/reva/internal/http/services/mailer"
 	_ "github.com/cs3org/reva/internal/http/services/mentix"
-	_ "github.com/cs3org/reva/internal/http/services/meshdirectory"
 	_ "github.com/cs3org/reva/internal/http/services/metrics"
 	_ "github.com/cs3org/reva/internal/http/services/ocmd"
 	_ "github.com/cs3org/reva/internal/http/services/owncloud/ocdav"
