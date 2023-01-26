72336e6ececabad66b7066c05d854560780a34b7
Prarup Gurung <grgprarup@users.noreply.github.com>
2023-01-26 10:38:02 +0545

    Removed unused make target from Makefile, since Makefile of ocis is used to run test (#3622)

--- tests/acceptance/features/bootstrap/bootstrap.php
+++ tests/acceptance/features/bootstrap/bootstrap.php
@@ -1,14 +1,9 @@
 <?php
-$pathToApiTests = \getenv('PATH_TO_APITESTS');
-if ($pathToApiTests === false) {
-    $pathToApiTests = "../ocis";
-}
-
-require_once $pathToApiTests . '/tests/acceptance/features/bootstrap/bootstrap.php';
+require_once '../ocis/tests/acceptance/features/bootstrap/bootstrap.php';
 
 $classLoader = new \Composer\Autoload\ClassLoader();
 $classLoader->addPsr4(
-	"", $pathToApiTests . "/tests/acceptance/features/bootstrap", true
+	"", "../ocis/tests/acceptance/features/bootstrap", true
 );
 
 $classLoader->register();
