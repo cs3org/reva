84c11a53f207552442bbf637aa496eb77ccb1db3
jkoberg <jkoberg@owncloud.com>
2023-02-03 11:42:14 +0100

    add helper to create natsstream from config
    Signed-off-by: jkoberg <jkoberg@owncloud.com>

--- pkg/events/stream/stream.go
+++ pkg/events/stream/stream.go
@@ -4,33 +4,9 @@ package stream
 import (
 	"encoding/json"
 	"reflect"
-	"time"
 
-	"github.com/cenkalti/backoff"
-	"github.com/cs3org/reva/v2/pkg/logger"
 	"go-micro.dev/v4/events"
-
-	"github.com/go-micro/plugins/v4/events/natsjs"
 )
-
-// Nats returns a nats streaming client
-// retries exponentially to connect to a nats server
-func Nats(opts ...natsjs.Option) (events.Stream, error) {
-	b := backoff.NewExponentialBackOff()
-	var stream events.Stream
-	o := func() error {
-		n := b.NextBackOff()
-		s, err := natsjs.NewStream(opts...)
-		if err != nil && n > time.Second {
-			logger.New().Error().Err(err).Msgf("can't connect to nats (jetstream) server, retrying in %s", n)
-		}
-		stream = s
-		return err
-	}
-
-	err := backoff.Retry(o, b)
-	return stream, err
-}
 
 // Chan is a channel based streaming clients
 // Useful for tests or in memory applications
