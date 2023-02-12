11de9f5c671f5a8b956e09b624c64c42ff51684f
joramkruijer <j.kruijer@gmail.com>
2023-02-12 18:29:26 +0100

    Comment for default xattr namespace as per Codacy static analysis

--- pkg/storage/utils/decomposedfs/xattrs/xattrs_prefix.go
+++ pkg/storage/utils/decomposedfs/xattrs/xattrs_prefix.go
@@ -2,6 +2,10 @@
 
 package xattrs
 
+// The default namespace for ocis. As non root users can only manipulate
+// the user. namespace, which is what is used to store ownCloud specific
+// metadata. To prevent name collisions with other apps, we are going to
+// introduce a sub namespace "user.ocis."
 const (
 	OcisPrefix string = "user.ocis."
 )
