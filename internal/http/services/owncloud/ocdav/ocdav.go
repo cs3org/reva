c89632b82ef22178f732b500b444569c4b8af345
Gianmaria Del Monte <g.macmount@gmail.com>
2023-02-27 13:22:39 +0100

    enable webdav enpoint for ocm

--- internal/http/services/owncloud/ocdav/ocdav.go
+++ internal/http/services/owncloud/ocdav/ocdav.go
@@ -96,6 +96,7 @@ type Config struct {
 	// and received path is /docs the internal path will be:
 	// /users/<first char of username>/<username>/docs
 	WebdavNamespace string `mapstructure:"webdav_namespace"`
+	OCMNamespace    string `mapstructure:"ocm_namespace"`
 	GatewaySvc      string `mapstructure:"gatewaysvc"`
 	Timeout         int64  `mapstructure:"timeout"`
 	Insecure        bool   `mapstructure:"insecure" docs:"false;Whether to skip certificate checks when sending requests."`
