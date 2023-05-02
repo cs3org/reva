7d29aefea5329d99c2bd9f42572f9c5583da1ecb
Jörn Friedrich Dreyer <jfd@butonic.de>
2023-05-02 10:01:37 +0200

    Send quota when listing spaces in decomposedfs (#3828)
    * Send quota when listing spaces in decomposedfs
    
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>
    
    * Improve reading the quota from the opaque
    
    * Remove unused opaque map
    
    ---------
    
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>
    Co-authored-by: André Duffeck <andre.duffeck@firondu.de>

---
+++ changelog/unreleased/send-quota-when-listing-spaces.md
@@ -0,0 +1,5 @@
+Bugfix: Send quota when listing spaces in decomposedfs
+
+We now include free, used and remaining quota when listing spaces
+
+https://github.com/cs3org/reva/pull/3828
--- pkg/storage/utils/decomposedfs/decomposedfs.go
+++ pkg/storage/utils/decomposedfs/decomposedfs.go
@@ -438,11 +438,18 @@ func (fs *Decomposedfs) GetQuota(ctx context.Context, ref *provider.Reference) (
 		quotaStr = string(ri.Opaque.Map["quota"].Value)
 	}
 
+	// FIXME this reads remaining disk size from the local disk, not the blobstore
 	remaining, err = node.GetAvailableSize(n.InternalPath())
 	if err != nil {
 		return 0, 0, 0, err
 	}
 
+	return fs.calculateTotalUsedRemaining(quotaStr, ri.Size, remaining)
+}
+
+func (fs *Decomposedfs) calculateTotalUsedRemaining(quotaStr string, inUse, remaining uint64) (uint64, uint64, uint64, error) {
+	var err error
+	var total uint64
 	switch quotaStr {
 	case node.QuotaUncalculated, node.QuotaUnknown:
 		// best we can do is return current total
@@ -457,15 +464,14 @@ func (fs *Decomposedfs) GetQuota(ctx context.Context, ref *provider.Reference) (
 
 		if total <= remaining {
 			// Prevent overflowing
-			if ri.Size >= total {
+			if inUse >= total {
 				remaining = 0
 			} else {
-				remaining = total - ri.Size
+				remaining = total - inUse
 			}
 		}
 	}
-
-	return total, ri.Size, remaining, nil
+	return total, inUse, remaining, nil
 }
 
 // CreateHome creates a new home node for the given user
