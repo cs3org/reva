0d36cbb4970408f441cc5f0133e5f57cc7dedcb3
Gianmaria Del Monte <g.macmount@gmail.com>
2023-06-06 17:44:10 +0200

    update permissions of federated shares from ocs

--- internal/http/services/owncloud/ocs/handlers/apps/sharing/shares/shares.go
+++ internal/http/services/owncloud/ocs/handlers/apps/sharing/shares/shares.go
@@ -33,12 +33,14 @@ import (
 	"time"
 
 	"github.com/ReneKroon/ttlcache/v2"
+	providerv1beta1 "github.com/cs3org/go-cs3apis/cs3/app/provider/v1beta1"
 	gateway "github.com/cs3org/go-cs3apis/cs3/gateway/v1beta1"
 	grouppb "github.com/cs3org/go-cs3apis/cs3/identity/group/v1beta1"
 	userpb "github.com/cs3org/go-cs3apis/cs3/identity/user/v1beta1"
 	rpc "github.com/cs3org/go-cs3apis/cs3/rpc/v1beta1"
 	collaboration "github.com/cs3org/go-cs3apis/cs3/sharing/collaboration/v1beta1"
 	link "github.com/cs3org/go-cs3apis/cs3/sharing/link/v1beta1"
+	ocmv1beta1 "github.com/cs3org/go-cs3apis/cs3/sharing/ocm/v1beta1"
 	provider "github.com/cs3org/go-cs3apis/cs3/storage/provider/v1beta1"
 	"github.com/cs3org/reva/internal/http/services/owncloud/ocdav"
 	"github.com/cs3org/reva/internal/http/services/owncloud/ocs/config"
@@ -547,7 +549,11 @@ func (h *Handler) UpdateShare(w http.ResponseWriter, r *http.Request) {
 		h.updatePublicShare(w, r, shareID)
 		return
 	}
+	if h.isFederatedShare(r, shareID) {
+		h.updateFederatedShare(w, r, shareID)
+	}
 	h.updateShare(w, r, shareID) // TODO PUT is used with incomplete data to update a share}
+
 }
 
 func (h *Handler) updateShare(w http.ResponseWriter, r *http.Request, shareID string) {
@@ -644,6 +650,89 @@ func (h *Handler) updateShare(w http.ResponseWriter, r *http.Request, shareID st
 	h.mapUserIds(ctx, client, share)
 
 	response.WriteOCSSuccess(w, r, share)
+}
+
+func permissionsToViewMode(pint int) providerv1beta1.ViewMode {
+	if pint == 15 {
+		return providerv1beta1.ViewMode_VIEW_MODE_READ_WRITE
+	}
+	return providerv1beta1.ViewMode_VIEW_MODE_READ_ONLY
+}
+
+func (h *Handler) updateFederatedShare(w http.ResponseWriter, r *http.Request, shareID string) {
+	ctx := r.Context()
+
+	pval := r.FormValue("permissions")
+	if pval == "" {
+		response.WriteOCSError(w, r, response.MetaBadRequest.StatusCode, "permissions missing", nil)
+		return
+	}
+
+	pint, err := strconv.Atoi(pval)
+	if err != nil {
+		response.WriteOCSError(w, r, response.MetaBadRequest.StatusCode, "permissions must be an integer", nil)
+		return
+	}
+	permissions, err := conversions.NewPermissions(pint)
+	if err != nil {
+		response.WriteOCSError(w, r, response.MetaBadRequest.StatusCode, err.Error(), nil)
+		return
+	}
+
+	client, err := pool.GetGatewayServiceClient(pool.Endpoint(h.gatewayAddr))
+	if err != nil {
+		response.WriteOCSError(w, r, response.MetaServerError.StatusCode, "error getting grpc gateway client", err)
+		return
+	}
+
+	updateRes, err := client.UpdateOCMShare(ctx, &ocmv1beta1.UpdateOCMShareRequest{
+		Ref: &ocmv1beta1.ShareReference{
+			Spec: &ocmv1beta1.ShareReference_Id{
+				Id: &ocmv1beta1.ShareId{
+					OpaqueId: h.sharePrefix,
+				},
+			},
+		},
+		Field: []*ocmv1beta1.UpdateOCMShareRequest_UpdateField{
+			{
+				Field: &ocmv1beta1.UpdateOCMShareRequest_UpdateField_AccessMethods{
+					AccessMethods: &ocmv1beta1.AccessMethod{
+						Term: &ocmv1beta1.AccessMethod_WebdavOptions{
+							WebdavOptions: &ocmv1beta1.WebDAVAccessMethod{
+								Permissions: conversions.RoleFromOCSPermissions(permissions).CS3ResourcePermissions(),
+							},
+						},
+					},
+				},
+			},
+			{
+				Field: &ocmv1beta1.UpdateOCMShareRequest_UpdateField_AccessMethods{
+					AccessMethods: &ocmv1beta1.AccessMethod{
+						Term: &ocmv1beta1.AccessMethod_WebappOptions{
+							WebappOptions: &ocmv1beta1.WebappAccessMethod{
+								ViewMode: permissionsToViewMode(pint),
+							},
+						},
+					},
+				},
+			},
+		},
+	})
+	if err != nil {
+		response.WriteOCSError(w, r, response.MetaServerError.StatusCode, "error sending a grpc update share request", err)
+		return
+	}
+
+	if updateRes.Status.Code != rpc.Code_CODE_OK {
+		if updateRes.Status.Code == rpc.Code_CODE_NOT_FOUND {
+			response.WriteOCSError(w, r, response.MetaNotFound.StatusCode, "not found", nil)
+			return
+		}
+		response.WriteOCSError(w, r, response.MetaServerError.StatusCode, "grpc update share request failed", err)
+		return
+	}
+
+	response.WriteOCSSuccess(w, r, []*conversions.ShareData{})
 }
 
 // RemoveShare handles DELETE requests on /apps/files_sharing/api/v1/shares/(shareid).
