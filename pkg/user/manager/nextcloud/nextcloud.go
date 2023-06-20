c2c7a4d7f8b861e00eac9b6c31995d73ba194463
Michiel de Jong <michiel@unhosted.org>
2023-06-20 10:54:25 +0000

    Include appctx

--- pkg/user/manager/nextcloud/nextcloud.go
+++ pkg/user/manager/nextcloud/nextcloud.go
@@ -29,6 +29,7 @@ import (
 
 	userpb "github.com/cs3org/go-cs3apis/cs3/identity/user/v1beta1"
 	ctxpkg "github.com/cs3org/reva/pkg/ctx"
+	"github.com/cs3org/reva/pkg/appctx"
 	"github.com/cs3org/reva/pkg/errtypes"
 	"github.com/cs3org/reva/pkg/user"
 	"github.com/cs3org/reva/pkg/user/manager/registry"
