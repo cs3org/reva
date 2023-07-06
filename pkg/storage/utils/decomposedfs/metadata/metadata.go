ba857f2a10d2d38fbf5bf8ca6dd0e2c9d7bfb7a5
Jörn Friedrich Dreyer <jfd@butonic.de>
2023-07-06 15:56:16 +0200

    remove dead code
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

--- pkg/storage/utils/decomposedfs/metadata/metadata.go
+++ pkg/storage/utils/decomposedfs/metadata/metadata.go
@@ -89,11 +89,6 @@ func (NullBackend) Set(ctx context.Context, path string, key string, val []byte)
 	return errUnconfiguredError
 }
 
-// SetMultipleWithContext sets a set of attribute for the given path
-func (NullBackend) SetMultipleWithContext(ctx context.Context, path string, attribs map[string][]byte, acquireLock bool) error {
-	return errUnconfiguredError
-}
-
 // SetMultiple sets a set of attribute for the given path
 func (NullBackend) SetMultiple(ctx context.Context, path string, attribs map[string][]byte, acquireLock bool) error {
 	return errUnconfiguredError
