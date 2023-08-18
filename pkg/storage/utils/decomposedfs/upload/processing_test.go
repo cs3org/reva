5d521588ddc79cdd28f43a3a8c46a7c56ddc9245
Jörn Friedrich Dreyer <jfd@butonic.de>
2023-08-18 13:57:42 +0200

    fix unreachable file handle
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

--- pkg/storage/utils/decomposedfs/upload/processing_test.go
+++ pkg/storage/utils/decomposedfs/upload/processing_test.go
@@ -33,16 +33,18 @@ func TestInitNewNode(t *testing.T) {
 	}
 	n := node.New("e48c4e7a-beac-4b82-b991-a5cff7b8c39c", "930b7a2e-b745-41e1-8a9b-712582021842", "e48c4e7a-beac-4b82-b991-a5cff7b8c39c", "newchild", 10, "26493c53-2634-45f8-949f-dc07b88df9b0", providerv1beta1.ResourceType_RESOURCE_TYPE_FILE, &userv1beta1.UserId{}, lookup)
 	n.SpaceRoot = rootNode
-	_, err = initNewNode(&Upload{Ctx: context.Background(), lu: lookup, Info: handler.FileInfo{MetaData: handler.MetaData{}}}, n, 10)
+	f, err := initNewNode(&Upload{Ctx: context.Background(), lu: lookup, Info: handler.FileInfo{MetaData: handler.MetaData{}}}, n, 10)
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
+	defer f.Close()
 
 	// try initializing the same new node again in case a concurrent requests tries to create a file with the same name
 	n = node.New("e48c4e7a-beac-4b82-b991-a5cff7b8c39c", "a6ede986-cfcd-41c5-a820-6eee955a1c2b", "e48c4e7a-beac-4b82-b991-a5cff7b8c39c", "newchild", 10, "26493c53-2634-45f8-949f-dc07b88df9b0", providerv1beta1.ResourceType_RESOURCE_TYPE_FILE, &userv1beta1.UserId{}, lookup)
 	n.SpaceRoot = rootNode
-	_, err = initNewNode(&Upload{Ctx: context.Background(), lu: lookup, Info: handler.FileInfo{MetaData: handler.MetaData{}}}, n, 10)
+	f2, err := initNewNode(&Upload{Ctx: context.Background(), lu: lookup, Info: handler.FileInfo{MetaData: handler.MetaData{}}}, n, 10)
 	if _, ok := err.(errtypes.IsAlreadyExists); !ok {
 		t.Fatalf(`initNewNode(with same 'newchild' name), %v, want %v`, err, errtypes.AlreadyExists("newchild"))
 	}
+	f2.Close()
 }
