996367ad045476c998385aeb20b13964eb0c2620
Adriana Baldacchino <adriana.baldacchino@cern.ch>
2023-08-24 17:06:23 +0200

    Amending error message to include case where resource is a container

--- internal/http/services/appprovider/appprovider.go
+++ internal/http/services/appprovider/appprovider.go
@@ -372,7 +372,7 @@ func (s *svc) handleOpen(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if statRes.Info.Type != storagepb.ResourceType_RESOURCE_TYPE_FILE && statRes.Info.Type != storagepb.ResourceType_RESOURCE_TYPE_CONTAINER {
-		writeError(w, r, appErrorInvalidParameter, "the given file id does not point to a file", nil)
+		writeError(w, r, appErrorInvalidParameter, "the given file id does not point to a file or a container", nil)
 		return
 	}
 
