e5a035532f2a5fcd390423204a426ae080b855a4
Giuseppe Lo Presti <giuseppe.lopresti@cern.ch>
2023-08-31 10:44:34 +0200

    Revert "Fix some tests (#4136)"
    This reverts commit e3acfe8a08dfd5159dbdb852dee2aca8fd9e32a0.

--- pkg/ocm/share/repository/nextcloud/nextcloud_test.go
+++ pkg/ocm/share/repository/nextcloud/nextcloud_test.go
@@ -258,7 +258,6 @@ var _ = Describe("Nextcloud", func() {
 			Expect(err).ToNot(HaveOccurred())
 			Expect(*share).To(Equal(ocm.Share{
 				Id: &ocm.ShareId{},
-				ResourceId: &provider.ResourceId{},
 				Grantee: &provider.Grantee{
 					Id: &provider.Grantee_UserId{
 						UserId: &userpb.UserId{
@@ -293,7 +292,7 @@ var _ = Describe("Nextcloud", func() {
 					XXX_sizecache:        0,
 				},
 			}))
-			checkCalled(called, `POST /apps/sciencemesh/~nobody/api/ocm/GetSentShareByToken {"Spec":{"Id":{"opaque_id":"some-share-id"}}}`)
+			checkCalled(called, `POST /apps/sciencemesh/~tester/api/ocm/GetShare {"Spec":{"Id":{"opaque_id":"some-share-id"}}}`)
 		})
 	})
 
