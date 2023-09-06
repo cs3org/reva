2a0501ecc656165678a58096405954b99db23ca4
Javier Ferrer <javier.ferrer@cern.ch>
2023-09-06 08:58:17 +0200

    Add dynamic storage provider to loader

--- pkg/storage/registry/loader/loader.go
+++ pkg/storage/registry/loader/loader.go
@@ -20,6 +20,7 @@ package loader
 
 import (
 	// Load core storage broker drivers.
+	_ "github.com/cs3org/reva/pkg/storage/registry/dynamic"
 	_ "github.com/cs3org/reva/pkg/storage/registry/static"
 	// Add your own here.
 )
