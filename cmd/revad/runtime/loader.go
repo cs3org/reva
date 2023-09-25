cd53d2666d2d5b9c2763aebd866059c0922d426a
Gianmaria Del Monte <g.macmount@gmail.com>
2023-09-25 14:46:27 +0200

    Revert "removed cernbox plugins"
    This reverts commit 570554a06f57dcf23b616fde17c58ffff156d5c0.

--- cmd/revad/runtime/loader.go
+++ cmd/revad/runtime/loader.go
@@ -33,6 +33,7 @@ import (
 	_ "github.com/cs3org/reva/pkg/appauth/manager/loader"
 	_ "github.com/cs3org/reva/pkg/auth/manager/loader"
 	_ "github.com/cs3org/reva/pkg/auth/registry/loader"
+	_ "github.com/cs3org/reva/pkg/cbox/loader"
 	_ "github.com/cs3org/reva/pkg/datatx/manager/loader"
 	_ "github.com/cs3org/reva/pkg/group/manager/loader"
 	_ "github.com/cs3org/reva/pkg/metrics/driver/loader"
