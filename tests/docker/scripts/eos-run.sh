618802ce80d27d91dc2df12f8b6751876170d901
Gianmaria Del Monte <g.macmount@gmail.com>
2023-10-02 17:13:59 +0200

    deleted drone tests

--- tests/docker/scripts/eos-run.sh
+++ tests/docker/scripts/eos-run.sh
@@ -33,8 +33,8 @@ done
 
 eos vid set membership 0 +sudo
 eos vid set membership 99 +sudo
-eos vid set map -tident "*@storage-home-ocis" vuid:0 vgid:0
-eos vid set map -tident "*@storage-users-ocis" vuid:0 vgid:0
+eos vid set map -tident "*@storage-home" vuid:0 vgid:0
+eos vid set map -tident "*@storage-users" vuid:0 vgid:0
 eos vid set map -tident "*@storage-local-1" vuid:0 vgid:0
 eos vid set map -tident "*@storage-local-2" vuid:0 vgid:0
 
