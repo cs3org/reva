38aaefaf299b8f762273c7c274759621690691b2
Giuseppe Lo Presti <giuseppe.lopresti@cern.ch>
2023-10-13 15:46:09 +0200

    Moved to scripts

--- tests/sciencemesh/scripts/init-reva.sh
+++ tests/sciencemesh/scripts/init-reva.sh
@@ -3,7 +3,6 @@
 set -e
 ENV_ROOT=$(pwd)
 export ENV_ROOT=${ENV_ROOT}
-[ ! -d "./scripts" ] && echo "Directory ./scripts DOES NOT exist inside $ENV_ROOT, are you running this from the repo root?" && exit 1
 
 docker run --rm  -it                              \
   -v "${ENV_ROOT}/../..:/reva"                    \
@@ -15,4 +14,3 @@ docker run --rm  -it                              \
 
 docker network inspect testnet >/dev/null 2>&1 || docker network create testnet
 
-[ ! -d "temp" ] && mkdir --parents temp
