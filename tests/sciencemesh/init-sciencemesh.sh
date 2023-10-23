f2b555394313add9abb3bd6e88c2b00af5bf3855
Giuseppe Lo Presti <giuseppe.lopresti@cern.ch>
2023-10-23 16:44:29 +0200

    cernbox web extensions are temporarily served from a custom tgz

--- tests/sciencemesh/init-sciencemesh.sh
+++ tests/sciencemesh/init-sciencemesh.sh
@@ -10,7 +10,9 @@ REPO_OWNCLOUD_APP=https://github.com/sciencemesh/nc-sciencemesh
 BRANCH_OWNCLOUD_APP=owncloud
 
 CBOX_WEB=https://github.com/cernbox/web-release/releases/latest/download
-REPO_CBOX_EXT=https://github.com/cernbox/web-extensions
+# TODO(lopresti) the CERNBox extensions are not released as a 'latest' bundle
+# and are for now pushed from a tgz file in this repo
+#CBOX_EXT=https://github.com/...
 
 REPO_WOPISERVER=https://github.com/cs3org/wopiserver
 TAG_WOPISERVER=master
@@ -55,10 +57,13 @@ TAG_WOPISERVER=master
     wget ${CBOX_WEB}/web.tar.gz &&                                              \
     tar xf web.tar.gz -C ./web --strip-components=1 &&                          \
     rm -rf web.tar.gz &&                                                        \
-    git clone ${REPO_CBOX_EXT} cernbox &&                                       \
+    tar xf ../cernbox/cernbox-extensions-bundle.tgz &&                          \
+    sed -i "s|\"web-app-draw-io\":\"./web-app-draw-io-.........mjs\",|,|" web/index.html && \
     chmod -R 755 ./web ./cernbox &&                                             \
     chown -R 101:101 ./web/ ./cernbox &&                                        \
     cd -
+
+#sed -i "s|WEB_APPS_MAP = {\"web-app-draw-io\":\"./web-app-draw-io-.........mjs\",|WEB_APPS_MAP = {\"web-app-sciencemesh-app\":\".\/web-app-sciencemesh-app-a5ecef59.mjs\",|" web/index.html && \
 
 # wopiserver source code for the config.
 [ ! -d "wopi-sciencemesh" ] &&                                                       \
