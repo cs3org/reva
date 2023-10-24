adb77786a1b5d1e9fc0fee8232643c66a4f45273
Gianmaria Del Monte <g.macmount@gmail.com>
2023-10-24 15:45:55 +0200

    add memory driver for spaces

--- pkg/spaces/manager/memory/memory.go
+++ pkg/spaces/manager/memory/memory.go
@@ -17,3 +17,136 @@
 // or submit itself to any jurisdiction.
 
 package memory
+
+import (
+	"context"
+	"errors"
+	"slices"
+
+	"github.com/cs3org/reva/pkg/spaces"
+	"github.com/cs3org/reva/pkg/spaces/manager/registry"
+	"github.com/cs3org/reva/pkg/storage/utils/templates"
+	"github.com/cs3org/reva/pkg/utils"
+	"github.com/cs3org/reva/pkg/utils/cfg"
+
+	userpb "github.com/cs3org/go-cs3apis/cs3/identity/user/v1beta1"
+	provider "github.com/cs3org/go-cs3apis/cs3/storage/provider/v1beta1"
+)
+
+func init() {
+	registry.Register("memory", New)
+}
+
+type SpaceDescription struct {
+	ID      string `mapstructure:"id"      validate:"required"`
+	Path    string `mapstructure:"path"    validate:"required"`
+	Name    string `mapstructure:"name"    validate:"required"`
+	Type    string `mapstructure:"type"    validate:"required"`
+	Owner   string `mapstructure:"owner"   validate:"required"`
+	Readers string `mapstructure:"readers" validate:"required"`
+	Writers string `mapstructure:"writers" validate:"required"`
+	Admins  string `mapstructure:"admins"  validate:"required"`
+}
+
+type Config struct {
+	Spaces    []SpaceDescription `mapstructure:"spaces"`
+	UserSpace string             `mapstructure:"user_space"`
+}
+
+func (c *Config) ApplyDefaults() {
+	if c.UserSpace == "" {
+		c.UserSpace = "/home"
+	}
+}
+
+type service struct {
+	c *Config
+}
+
+func New(ctx context.Context, m map[string]any) (spaces.Manager, error) {
+	var c Config
+	if err := cfg.Decode(m, &c); err != nil {
+		return nil, err
+	}
+	return NewWithConfig(ctx, &c)
+}
+
+func NewWithConfig(ctx context.Context, c *Config) (spaces.Manager, error) {
+	return &service{c: c}, nil
+}
+
+func (s *service) StoreSpace(ctx context.Context, owner *userpb.UserId, path, name string, quota *provider.Quota) error {
+	return errors.New("not yet implemented")
+}
+
+func (s *service) ListSpaces(ctx context.Context, user *userpb.User) ([]*provider.StorageSpace, error) {
+	sp := []*provider.StorageSpace{}
+
+	// home space
+	if space := s.userSpace(ctx, user); space != nil {
+		sp = append(sp, space)
+	}
+
+	// project spaces
+	projects := s.projectSpaces(ctx, user)
+	sp = append(sp, projects...)
+
+	return sp, nil
+}
+
+func (s *service) userSpace(ctx context.Context, user *userpb.User) *provider.StorageSpace {
+	if utils.UserIsLightweight(user) {
+		return nil // lightweight accounts and federated do not have a user space
+	}
+	path := templates.WithUser(user, s.c.UserSpace)
+	return &provider.StorageSpace{
+		Id: &provider.StorageSpaceId{
+			OpaqueId: path,
+		},
+		Owner:     user,
+		Name:      user.Username,
+		SpaceType: spaces.SpaceTypeHome.AsString(),
+		RootInfo: &provider.ResourceInfo{
+			Path: path,
+		},
+	}
+}
+
+func (s *service) projectSpaces(ctx context.Context, user *userpb.User) []*provider.StorageSpace {
+	projects := []*provider.StorageSpace{}
+	for _, space := range s.c.Spaces {
+		if projectBelongToUser(user, &space) {
+			projects = append(projects, &provider.StorageSpace{
+				Id: &provider.StorageSpaceId{
+					OpaqueId: space.ID,
+				},
+				Owner: &userpb.User{
+					Id: &userpb.UserId{
+						OpaqueId: space.Owner,
+					},
+				},
+				Name:      space.Name,
+				SpaceType: spaces.SpaceTypeProject.AsString(),
+				RootInfo: &provider.ResourceInfo{
+					Path: space.Path,
+				},
+			})
+		}
+	}
+	return projects
+}
+
+func projectBelongToUser(user *userpb.User, project *SpaceDescription) bool {
+	return user.Id.OpaqueId == project.Owner ||
+		slices.Contains(user.Groups, project.Admins) ||
+		slices.Contains(user.Groups, project.Readers) ||
+		slices.Contains(user.Groups, project.Writers)
+}
+
+func (s *service) UpdateSpace(ctx context.Context, space *provider.StorageSpace) error {
+	return errors.New("not yet implemented")
+}
+
+func (s *service) DeleteSpace(ctx context.Context, spaceID *provider.StorageSpaceId) error {
+	return errors.New("not yet implemented")
+}
