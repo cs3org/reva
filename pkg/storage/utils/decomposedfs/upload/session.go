9217478860b57566915b4fc152fbc1cb871f6981
Jörn Friedrich Dreyer <jfd@butonic.de>
2023-12-21 10:25:28 +0100

    use better function receiver variable name
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

--- pkg/storage/utils/decomposedfs/upload/session.go
+++ pkg/storage/utils/decomposedfs/upload/session.go
@@ -61,45 +61,45 @@ type OcisSession struct {
 	info tusd.FileInfo
 }
 
-func (m *OcisSession) Context(ctx context.Context) context.Context { // restore logger from file info
+func (s *OcisSession) Context(ctx context.Context) context.Context { // restore logger from file info
 	log, _ := logger.FromConfig(&logger.LogConf{
 		Output: "stderr", // TODO use config from decomposedfs
 		Mode:   "json",   // TODO use config from decomposedfs
-		Level:  m.info.Storage["LogLevel"],
+		Level:  s.info.Storage["LogLevel"],
 	})
 	sub := log.With().Int("pid", os.Getpid()).Logger()
 	ctx = appctx.WithLogger(ctx, &sub)
-	ctx = ctxpkg.ContextSetLockID(ctx, m.LockID())
-	return ctxpkg.ContextSetUser(ctx, m.ExecutantUser())
+	ctx = ctxpkg.ContextSetLockID(ctx, s.LockID())
+	return ctxpkg.ContextSetUser(ctx, s.ExecutantUser())
 }
 
-func (m *OcisSession) Purge(ctx context.Context) error {
-	if err := os.Remove(sessionPath(m.store.root, m.info.ID)); err != nil {
+func (s *OcisSession) Purge(ctx context.Context) error {
+	if err := os.Remove(sessionPath(s.store.root, s.info.ID)); err != nil {
 		return err
 	}
-	if err := os.Remove(m.binPath()); err != nil {
+	if err := os.Remove(s.binPath()); err != nil {
 		return err
 	}
 	return nil
 }
 
-func (m *OcisSession) TouchBin() error {
-	file, err := os.OpenFile(m.binPath(), os.O_CREATE|os.O_WRONLY, defaultFilePerm)
+func (s *OcisSession) TouchBin() error {
+	file, err := os.OpenFile(s.binPath(), os.O_CREATE|os.O_WRONLY, defaultFilePerm)
 	if err != nil {
 		return err
 	}
 	return file.Close()
 }
 
-func (m *OcisSession) Persist(ctx context.Context) error {
-	uploadPath := sessionPath(m.store.root, m.info.ID)
+func (s *OcisSession) Persist(ctx context.Context) error {
+	uploadPath := sessionPath(s.store.root, s.info.ID)
 	// create folder structure (if needed)
 	if err := os.MkdirAll(filepath.Dir(uploadPath), 0700); err != nil {
 		return err
 	}
 
 	var d []byte
-	d, err := json.Marshal(m.info)
+	d, err := json.Marshal(s.info)
 	if err != nil {
 		return err
 	}
@@ -107,67 +107,67 @@ func (m *OcisSession) Persist(ctx context.Context) error {
 	return os.WriteFile(uploadPath, d, 0600)
 }
 
-func (m *OcisSession) ToFileInfo() tusd.FileInfo {
-	return m.info
+func (s *OcisSession) ToFileInfo() tusd.FileInfo {
+	return s.info
 }
-func (m *OcisSession) ProviderID() string {
-	return m.info.MetaData["providerID"]
+func (s *OcisSession) ProviderID() string {
+	return s.info.MetaData["providerID"]
 }
-func (m *OcisSession) SpaceID() string {
-	return m.info.Storage["SpaceRoot"]
+func (s *OcisSession) SpaceID() string {
+	return s.info.Storage["SpaceRoot"]
 }
-func (m *OcisSession) NodeID() string {
-	return m.info.Storage["NodeId"]
+func (s *OcisSession) NodeID() string {
+	return s.info.Storage["NodeId"]
 }
-func (m *OcisSession) NodeParentID() string {
-	return m.info.Storage["NodeParentId"]
+func (s *OcisSession) NodeParentID() string {
+	return s.info.Storage["NodeParentId"]
 }
-func (m *OcisSession) NodeExists() bool {
-	return m.info.Storage["NodeExists"] == "true"
+func (s *OcisSession) NodeExists() bool {
+	return s.info.Storage["NodeExists"] == "true"
 }
-func (m *OcisSession) LockID() string {
-	return m.info.MetaData["lockid"]
+func (s *OcisSession) LockID() string {
+	return s.info.MetaData["lockid"]
 }
-func (m *OcisSession) ETag() string {
-	return m.info.MetaData["etag"]
+func (s *OcisSession) ETag() string {
+	return s.info.MetaData["etag"]
 }
-func (m *OcisSession) HeaderIfMatch() string {
-	return m.info.MetaData["if-match"]
+func (s *OcisSession) HeaderIfMatch() string {
+	return s.info.MetaData["if-match"]
 }
-func (m *OcisSession) HeaderIfNoneMatch() string {
-	return m.info.MetaData["if-none-match"]
+func (s *OcisSession) HeaderIfNoneMatch() string {
+	return s.info.MetaData["if-none-match"]
 }
-func (m *OcisSession) HeaderIfUnmodifiedSince() string {
-	return m.info.MetaData["if-unmodified-since"]
+func (s *OcisSession) HeaderIfUnmodifiedSince() string {
+	return s.info.MetaData["if-unmodified-since"]
 }
-func (m *OcisSession) Node(ctx context.Context) (*node.Node, error) {
-	n, err := node.ReadNode(ctx, m.store.lu, m.SpaceID(), m.info.Storage["NodeId"], false, nil, true)
+func (s *OcisSession) Node(ctx context.Context) (*node.Node, error) {
+	n, err := node.ReadNode(ctx, s.store.lu, s.SpaceID(), s.info.Storage["NodeId"], false, nil, true)
 	if err != nil {
 		return nil, err
 	}
 	return n, nil
 }
 
-func (m *OcisSession) ID() string {
-	return m.info.ID
+func (s *OcisSession) ID() string {
+	return s.info.ID
 }
-func (m *OcisSession) Filename() string {
-	return m.info.Storage["NodeName"]
+func (s *OcisSession) Filename() string {
+	return s.info.Storage["NodeName"]
 }
-func (m *OcisSession) Chunk() string {
-	return m.info.Storage["Chunk"]
+func (s *OcisSession) Chunk() string {
+	return s.info.Storage["Chunk"]
 }
-func (m *OcisSession) SetMetadata(key, value string) {
-	m.info.MetaData[key] = value
+func (s *OcisSession) SetMetadata(key, value string) {
+	s.info.MetaData[key] = value
 }
-func (m *OcisSession) SetStorageValue(key, value string) {
-	m.info.Storage[key] = value
+func (s *OcisSession) SetStorageValue(key, value string) {
+	s.info.Storage[key] = value
 }
-func (m *OcisSession) SetSize(size int64) {
-	m.info.Size = size
+func (s *OcisSession) SetSize(size int64) {
+	s.info.Size = size
 }
-func (m *OcisSession) SetSizeIsDeferred(value bool) {
-	m.info.SizeIsDeferred = value
+func (s *OcisSession) SetSizeIsDeferred(value bool) {
+	s.info.SizeIsDeferred = value
 }
 
 // TODO get rid of dir, whoever consumes the reference should be able to deal with a relative reference
@@ -178,89 +178,89 @@ func (m *OcisSession) SetSizeIsDeferred(value bool) {
 //   - search needs to index from the root anyway. and it only needs the most recent path to put it in the index
 //   - audit on the other hand needs to log events with the path at the state of the event ... so it does need the full path.
 //     I think we can safely read determine the path later, right before emitting the event. and maybe make it configurable, because only audit needs it, anyway.
-func (m *OcisSession) Dir() string {
-	return m.info.Storage["Dir"]
+func (s *OcisSession) Dir() string {
+	return s.info.Storage["Dir"]
 }
 
-func (m *OcisSession) Size() int64 {
-	return m.info.Size
+func (s *OcisSession) Size() int64 {
+	return s.info.Size
 }
-func (m *OcisSession) SizeDiff() int64 {
-	sizeDiff, _ := strconv.ParseInt(m.info.MetaData["sizeDiff"], 10, 64)
+func (s *OcisSession) SizeDiff() int64 {
+	sizeDiff, _ := strconv.ParseInt(s.info.MetaData["sizeDiff"], 10, 64)
 	return sizeDiff
 }
 
-func (m *OcisSession) ResourceID() provider.ResourceId {
+func (s *OcisSession) ResourceID() provider.ResourceId {
 	return provider.ResourceId{
-		StorageId: m.info.MetaData["providerID"],
-		SpaceId:   m.info.Storage["SpaceRoot"],
-		OpaqueId:  m.info.Storage["NodeId"],
+		StorageId: s.info.MetaData["providerID"],
+		SpaceId:   s.info.Storage["SpaceRoot"],
+		OpaqueId:  s.info.Storage["NodeId"],
 	}
 }
-func (m *OcisSession) Reference() provider.Reference {
+func (s *OcisSession) Reference() provider.Reference {
 	return provider.Reference{
 		ResourceId: &provider.ResourceId{
-			StorageId: m.info.MetaData["providerID"],
-			SpaceId:   m.info.Storage["SpaceRoot"],
-			OpaqueId:  m.info.Storage["NodeId"],
+			StorageId: s.info.MetaData["providerID"],
+			SpaceId:   s.info.Storage["SpaceRoot"],
+			OpaqueId:  s.info.Storage["NodeId"],
 		},
 		// Path is not used
 	}
 }
-func (m *OcisSession) Executant() userpb.UserId {
+func (s *OcisSession) Executant() userpb.UserId {
 	return userpb.UserId{
-		Type:     userpb.UserType(userpb.UserType_value[m.info.Storage["UserType"]]),
-		Idp:      m.info.Storage["Idp"],
-		OpaqueId: m.info.Storage["UserId"],
+		Type:     userpb.UserType(userpb.UserType_value[s.info.Storage["UserType"]]),
+		Idp:      s.info.Storage["Idp"],
+		OpaqueId: s.info.Storage["UserId"],
 	}
 }
-func (m *OcisSession) ExecutantUser() *userpb.User {
+func (s *OcisSession) ExecutantUser() *userpb.User {
 	return &userpb.User{
 		Id: &userpb.UserId{
-			Type:     userpb.UserType(userpb.UserType_value[m.info.Storage["UserType"]]),
-			Idp:      m.info.Storage["Idp"],
-			OpaqueId: m.info.Storage["UserId"],
+			Type:     userpb.UserType(userpb.UserType_value[s.info.Storage["UserType"]]),
+			Idp:      s.info.Storage["Idp"],
+			OpaqueId: s.info.Storage["UserId"],
 		},
-		Username: m.info.Storage["UserName"],
+		Username: s.info.Storage["UserName"],
 	}
 }
-func (m *OcisSession) SetExecutant(u *userpb.User) {
-	m.info.Storage["Idp"] = u.GetId().GetIdp()
-	m.info.Storage["UserId"] = u.GetId().GetOpaqueId()
-	m.info.Storage["UserType"] = utils.UserTypeToString(u.GetId().Type)
-	m.info.Storage["UserName"] = u.GetUsername()
+func (s *OcisSession) SetExecutant(u *userpb.User) {
+	s.info.Storage["Idp"] = u.GetId().GetIdp()
+	s.info.Storage["UserId"] = u.GetId().GetOpaqueId()
+	s.info.Storage["UserType"] = utils.UserTypeToString(u.GetId().Type)
+	s.info.Storage["UserName"] = u.GetUsername()
 }
-func (m *OcisSession) Offset() int64 {
-	return m.info.Offset
+func (s *OcisSession) Offset() int64 {
+	return s.info.Offset
 }
-func (m *OcisSession) SpaceOwner() *userpb.UserId {
+func (s *OcisSession) SpaceOwner() *userpb.UserId {
 	return &userpb.UserId{
 		// idp and type do not seem to be consumed and the node currently only stores the user id anyway
-		OpaqueId: m.info.Storage["SpaceOwnerOrManager"],
+		OpaqueId: s.info.Storage["SpaceOwnerOrManager"],
 	}
 
 }
-func (m *OcisSession) Expires() time.Time {
+func (s *OcisSession) Expires() time.Time {
 	var t time.Time
-	if value, ok := m.info.MetaData["expires"]; ok {
+	if value, ok := s.info.MetaData["expires"]; ok {
 		t, _ = utils.MTimeToTime(value)
 	}
 	return t
 }
-func (m *OcisSession) MTime() time.Time {
+func (s *OcisSession) MTime() time.Time {
 	var t time.Time
-	if value, ok := m.info.MetaData["mtime"]; ok {
+	if value, ok := s.info.MetaData["mtime"]; ok {
 		t, _ = utils.MTimeToTime(value)
 	}
 	return t
 }
-func (m *OcisSession) IsProcessing() bool {
-	return m.info.Storage["processing"] != "" // FIXME
+func (s *OcisSession) IsProcessing() bool {
+	return s.info.Storage["processing"] != "" // FIXME
 }
 
 // binPath returns the path to the file storing the binary data.
-func (m *OcisSession) binPath() string {
-	return filepath.Join(m.store.root, "uploads", m.info.ID)
+func (s *OcisSession) binPath() string {
+	return filepath.Join(s.store.root, "uploads", s.info.ID)
 }
 
 // infoPath returns the path to the .info file storing the file's info.
