02537f79eb8ffece134bf405d0631e5016795ebd
Hugo Gonzalez <github@hugo.labkode.com>
2024-01-30 15:59:39 +0100

    remove print

--- internal/grpc/interceptors/auth/auth.go
+++ internal/grpc/interceptors/auth/auth.go
@@ -200,7 +200,6 @@ func (ss *wrappedServerStream) Context() context.Context {
 }
 
 func dismantleToken(ctx context.Context, tkn string, req interface{}, mgr token.Manager, gatewayAddr string, unprotected bool) (*userpb.User, map[string]*authpb.Scope, error) {
-	fmt.Println(tkn)
 	u, tokenScope, err := mgr.DismantleToken(ctx, tkn)
 	if err != nil {
 		return nil, nil, err
