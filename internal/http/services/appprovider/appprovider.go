46d6c5c031faa96b7a0868d8f356f57e9889f818
Gianmaria Del Monte <g.macmount@gmail.com>
2024-02-15 14:59:44 +0100

    make app provider spaces aware

--- internal/http/services/appprovider/appprovider.go
+++ internal/http/services/appprovider/appprovider.go
@@ -38,9 +38,9 @@ import (
 	"github.com/cs3org/reva/pkg/rgrpc/todo/pool"
 	"github.com/cs3org/reva/pkg/rhttp/global"
 	"github.com/cs3org/reva/pkg/sharedconf"
+	"github.com/cs3org/reva/pkg/spaces"
 	"github.com/cs3org/reva/pkg/utils"
 	"github.com/cs3org/reva/pkg/utils/cfg"
-	"github.com/cs3org/reva/pkg/utils/resourceid"
 	"github.com/go-chi/chi/v5"
 	ua "github.com/mileusna/useragent"
 	"github.com/pkg/errors"
@@ -142,8 +142,8 @@ func (s *svc) handleNew(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	parentContainerRef := resourceid.OwnCloudResourceIDUnwrap(parentContainerID)
-	if parentContainerRef == nil {
+	parentContainerRef, ok := spaces.ParseResourceID(parentContainerID)
+	if !ok {
 		writeError(w, r, appErrorInvalidParameter, "invalid parent container ID", nil)
 		return
 	}
@@ -277,7 +277,7 @@ func (s *svc) handleNew(w http.ResponseWriter, r *http.Request) {
 
 	js, err := json.Marshal(
 		map[string]interface{}{
-			"file_id": resourceid.OwnCloudResourceIDWrap(statRes.Info.Id),
+			"file_id": spaces.EncodeResourceID(statFileRes.Info.Id),
 		},
 	)
 	if err != nil {
@@ -349,8 +349,8 @@ func (s *svc) handleOpen(w http.ResponseWriter, r *http.Request) {
 		}
 		fileRef.Path = path
 	} else {
-		resourceID := resourceid.OwnCloudResourceIDUnwrap(fileID)
-		if resourceID == nil {
+		resourceID, ok := spaces.ParseResourceID(fileID)
+		if !ok {
 			writeError(w, r, appErrorInvalidParameter, "invalid file ID", nil)
 			return
 		}
@@ -461,8 +461,8 @@ func (s *svc) handleNotify(w http.ResponseWriter, r *http.Request) {
 		}
 		fileRef.Path = path
 	} else {
-		resourceID := resourceid.OwnCloudResourceIDUnwrap(fileID)
-		if resourceID == nil {
+		resourceID, ok := spaces.ParseResourceID(fileID)
+		if !ok {
 			writeError(w, r, appErrorInvalidParameter, "invalid file ID", nil)
 			return
 		}
