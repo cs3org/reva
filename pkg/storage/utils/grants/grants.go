b498493b38b86650fae9f9eab2b2299b9785f0fb
Gianmaria Del Monte <g.macmount@gmail.com>
2024-02-15 11:27:25 +0100

    use more robust proto.Equal for comparing protobuf messages

--- pkg/storage/utils/grants/grants.go
+++ pkg/storage/utils/grants/grants.go
@@ -24,15 +24,16 @@ import (
 
 	provider "github.com/cs3org/go-cs3apis/cs3/storage/provider/v1beta1"
 	"github.com/cs3org/reva/pkg/storage/utils/acl"
-	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"
+	"google.golang.org/protobuf/proto"
 )
+
+var noPermissions = provider.ResourcePermissions{}
 
 // GetACLPerm generates a string representation of CS3APIs' ResourcePermissions
 // TODO(labkode): fine grained permission controls.
 func GetACLPerm(set *provider.ResourcePermissions) (string, error) {
 	// resource permission is denied
-	if cmp.Equal(&provider.ResourcePermissions{}, set, cmpopts.IgnoreUnexported(set)) {
+	if proto.Equal(&noPermissions, set) {
 		return "!r!w!x!m!u!d", nil
 	}
 
@@ -136,10 +137,10 @@ func GetGranteeType(aclType string) provider.GranteeType {
 
 // PermissionsEqual returns true if the permissions are equal.
 func PermissionsEqual(p1, p2 *provider.ResourcePermissions) bool {
-	return p1 != nil && p2 != nil && cmp.Equal(p1, p2, cmpopts.IgnoreUnexported(p1, p2))
+	return p1 != nil && p2 != nil && proto.Equal(p1, p2)
 }
 
 // GranteeEqual returns true if the grantee are equal.
 func GranteeEqual(g1, g2 *provider.Grantee) bool {
-	return g1 != nil && g2 != nil && cmp.Equal(g1, g2, cmpopts.IgnoreUnexported(g1, g2))
+	return g1 != nil && g2 != nil && proto.Equal(g1, g2)
 }
