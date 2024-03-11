b27a7f81f38c104534e2f139748fa8a49e3fe15e
André Duffeck <andre.duffeck@firondu.de>
2024-03-11 10:37:50 +0100

    Improve error logging

--- pkg/storage/fs/posix/tree/tree.go
+++ pkg/storage/fs/posix/tree/tree.go
@@ -178,7 +178,7 @@ func (t *Tree) assimilate(item scanItem) error {
 	// lock the file for assimilation
 	unlock, err := t.lookup.MetadataBackend().Lock(item.Path)
 	if err != nil {
-		return fmt.Errorf("failed to lock item for assimilation")
+		return errors.Wrap(err, "failed to lock item for assimilation")
 	}
 	defer func() {
 		_ = unlock()
@@ -221,7 +221,7 @@ func (t *Tree) assimilate(item scanItem) error {
 		id := uuid.New().String()
 		stat, err := os.Stat(item.Path)
 		if err != nil {
-			return fmt.Errorf("failed to stat item")
+			return errors.Wrap(err, "failed to stat item")
 		}
 
 		attributes := node.Attributes{
@@ -248,7 +248,7 @@ func (t *Tree) assimilate(item scanItem) error {
 		}
 		err = t.lookup.MetadataBackend().SetMultiple(context.Background(), item.Path, attributes, false)
 		if err != nil {
-			return fmt.Errorf("failed to set attributes")
+			return errors.Wrap(err, "failed to set attributes")
 		}
 
 		if !item.ForceRescan {
@@ -259,7 +259,7 @@ func (t *Tree) assimilate(item scanItem) error {
 
 	info, err := os.Stat(item.Path)
 	if err != nil {
-		return fmt.Errorf("failed to stat item for recursive scanning")
+		return errors.Wrap(err, "failed to stat item for recursive scanning")
 	}
 
 	// rescan the directory recursively
