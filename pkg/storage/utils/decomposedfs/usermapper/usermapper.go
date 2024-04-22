aba72a301b1f76b5f3c89b0c516c5c4b56e790de
André Duffeck <andre.duffeck@firondu.de>
2024-04-22 08:51:21 +0200

    Do not switch the group id anymore

--- pkg/storage/utils/decomposedfs/usermapper/usermapper.go
+++ pkg/storage/utils/decomposedfs/usermapper/usermapper.go
@@ -32,7 +32,6 @@ import (
 
 type Mapper struct {
 	baseUid int
-	baseGid int
 }
 
 type UnscopeFunc func() error
@@ -40,11 +39,9 @@ type UnscopeFunc func() error
 // New returns a new user mapper
 func New() *Mapper {
 	baseUid, _ := unix.SetfsuidRetUid(-1)
-	baseGid, _ := unix.SetfsgidRetGid(-1)
 
 	return &Mapper{
 		baseUid: baseUid,
-		baseGid: baseGid,
 	}
 }
 
@@ -65,45 +62,36 @@ func (um *Mapper) RunInBaseScope(f func() error) error {
 
 // ScopeBase returns to the base uid and gid returning a function that can be used to restore the previous scope
 func (um *Mapper) ScopeBase() (func() error, error) {
-	return um.ScopeUserByIds(um.baseUid, um.baseGid)
+	return um.ScopeUserByIds(um.baseUid)
 }
 
 // MapUser returns the user and group ids for the given username
-func (u *Mapper) MapUser(username string) (int, int, error) {
+func (u *Mapper) MapUser(username string) (int, error) {
 	userDetails, err := user.Lookup(username)
 	if err != nil {
-		return 0, 0, err
+		return 0, err
 	}
 
 	uid, err := strconv.Atoi(userDetails.Uid)
 	if err != nil {
-		return 0, 0, err
+		return 0, err
 	}
 
-	gid, err := strconv.Atoi(userDetails.Gid)
-	if err != nil {
-		return 0, 0, err
-	}
-
-	return uid, gid, nil
+	return uid, nil
 }
 
 func (um *Mapper) ScopeUser(ctx context.Context) (func() error, error) {
 	u := revactx.ContextMustGetUser(ctx)
 
-	uid, gid, err := um.MapUser(u.Username)
+	uid, err := um.MapUser(u.Username)
 	if err != nil {
 		return nil, err
 	}
-	return um.ScopeUserByIds(uid, gid)
+	return um.ScopeUserByIds(uid)
 }
-func (um *Mapper) ScopeUserByIds(uid, gid int) (func() error, error) {
+func (um *Mapper) ScopeUserByIds(uid int) (func() error, error) {
 	runtime.LockOSThread() // Lock this Goroutine to the current OS thread
 
-	prevGid, err := unix.SetfsgidRetGid(gid)
-	if err != nil {
-		return nil, err
-	}
 	prevUid, err := unix.SetfsuidRetUid(uid)
 	if err != nil {
 		return nil, err
@@ -113,7 +101,6 @@ func (um *Mapper) ScopeUserByIds(uid, gid int) (func() error, error) {
 	}
 
 	return func() error {
-		_ = unix.Setfsgid(prevGid)
 		_ = unix.Setfsuid(prevUid)
 		runtime.UnlockOSThread()
 		return nil
