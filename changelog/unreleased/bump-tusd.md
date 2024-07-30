c6a5c699449d56f8091ee487801851326296eeef
jkoberg <jkoberg@owncloud.com>
2024-07-30 13:55:27 +0200

    feat(tusd): Bump tusd pkg to v2.4.0
    Signed-off-by: jkoberg <jkoberg@owncloud.com>

---
+++ changelog/unreleased/bump-tusd.md
@@ -0,0 +1,5 @@
+Enhancement: Bump tusd to v2
+
+Bump tusd pkg to v2.4.0
+
+https://github.com/cs3org/reva/pull/4784
--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -23,7 +23,7 @@ import (
 	"io"
 	"net/url"
 
-	tusd "github.com/tus/tusd/pkg/handler"
+	tusd "github.com/tus/tusd/v2/pkg/handler"
 
 	provider "github.com/cs3org/go-cs3apis/cs3/storage/provider/v1beta1"
 	registry "github.com/cs3org/go-cs3apis/cs3/storage/registry/v1beta1"
