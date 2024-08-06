6d39b23ccb8a8e09d68a4781b1783a9c238d23b3
André Duffeck <andre.duffeck@firondu.de>
2024-08-06 11:16:01 +0200

    Fix linter issues

--- pkg/storage/fs/posix/timemanager/timemanager.go
+++ pkg/storage/fs/posix/timemanager/timemanager.go
@@ -1,4 +1,4 @@
-// Copyright 2018-2021 CERN
+// Copyright 2018-2024 CERN
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -28,13 +28,16 @@ import (
 	"github.com/cs3org/reva/v2/pkg/storage/utils/decomposedfs/node"
 )
 
+// Manager is responsible for managing time-related operations on files and directories.
 type Manager struct {
 }
 
+// OverrideMtime overrides the modification time (mtime) of a node with the specified time.
 func (m *Manager) OverrideMtime(ctx context.Context, n *node.Node, _ *node.Attributes, mtime time.Time) error {
 	return os.Chtimes(n.InternalPath(), mtime, mtime)
 }
 
+// MTime returns the modification time (mtime) of a node.
 func (m *Manager) MTime(ctx context.Context, n *node.Node) (time.Time, error) {
 	fi, err := os.Stat(n.InternalPath())
 	if err != nil {
@@ -43,10 +46,13 @@ func (m *Manager) MTime(ctx context.Context, n *node.Node) (time.Time, error) {
 	return fi.ModTime(), nil
 }
 
+// SetMTime sets the modification time (mtime) of a node to the specified time.
 func (m *Manager) SetMTime(ctx context.Context, n *node.Node, mtime *time.Time) error {
 	return os.Chtimes(n.InternalPath(), *mtime, *mtime)
 }
 
+// TMTime returns the tree modification time (tmtime) of a node.
+// If the tmtime is not set, it falls back to the modification time (mtime).
 func (m *Manager) TMTime(ctx context.Context, n *node.Node) (time.Time, error) {
 	b, err := n.XattrString(ctx, prefixes.TreeMTimeAttr)
 	if err == nil {
@@ -57,6 +63,8 @@ func (m *Manager) TMTime(ctx context.Context, n *node.Node) (time.Time, error) {
 	return m.MTime(ctx, n)
 }
 
+// SetTMTime sets the tree modification time (tmtime) of a node to the specified time.
+// If tmtime is nil, the tmtime attribute is removed.
 func (m *Manager) SetTMTime(ctx context.Context, n *node.Node, tmtime *time.Time) error {
 	if tmtime == nil {
 		return n.RemoveXattr(ctx, prefixes.TreeMTimeAttr, true)
@@ -64,6 +72,7 @@ func (m *Manager) SetTMTime(ctx context.Context, n *node.Node, tmtime *time.Time
 	return n.SetXattrString(ctx, prefixes.TreeMTimeAttr, tmtime.UTC().Format(time.RFC3339Nano))
 }
 
+// CTime returns the creation time (ctime) of a node.
 func (m *Manager) CTime(ctx context.Context, n *node.Node) (time.Time, error) {
 	fi, err := os.Stat(n.InternalPath())
 	if err != nil {
@@ -74,17 +83,21 @@ func (m *Manager) CTime(ctx context.Context, n *node.Node) (time.Time, error) {
 	return time.Unix(int64(stat.Ctim.Sec), int64(stat.Ctim.Nsec)), nil
 }
 
+// TCTime returns the tree creation time (tctime) of a node.
+// Since decomposedfs does not differentiate between ctime and mtime, it falls back to TMTime.
 func (m *Manager) TCTime(ctx context.Context, n *node.Node) (time.Time, error) {
 	// decomposedfs does not differentiate between ctime and mtime
 	return m.TMTime(ctx, n)
 }
 
+// SetTCTime sets the tree creation time (tctime) of a node to the specified time.
+// Since decomposedfs does not differentiate between ctime and mtime, it falls back to SetTMTime.
 func (m *Manager) SetTCTime(ctx context.Context, n *node.Node, tmtime *time.Time) error {
 	// decomposedfs does not differentiate between ctime and mtime
 	return m.SetTMTime(ctx, n, tmtime)
 }
 
-// GetDTime reads the dtime from the extended attributes
+// DTime returns the deletion time (dtime) of a node.
 func (m *Manager) DTime(ctx context.Context, n *node.Node) (tmTime time.Time, err error) {
 	b, err := n.XattrString(ctx, prefixes.DTimeAttr)
 	if err != nil {
@@ -93,7 +106,8 @@ func (m *Manager) DTime(ctx context.Context, n *node.Node) (tmTime time.Time, er
 	return time.Parse(time.RFC3339Nano, b)
 }
 
-// SetDTime writes the UTC dtime to the extended attributes or removes the attribute if nil is passed
+// SetDTime sets the deletion time (dtime) of a node to the specified time.
+// If t is nil, the dtime attribute is removed.
 func (m *Manager) SetDTime(ctx context.Context, n *node.Node, t *time.Time) (err error) {
 	if t == nil {
 		return n.RemoveXattr(ctx, prefixes.DTimeAttr, true)
--- pkg/storage/utils/decomposedfs/timemanager/timemanager.go
+++ pkg/storage/utils/decomposedfs/timemanager/timemanager.go
@@ -1,10 +1,10 @@
-// Copyright 2018-2021 CERN
+// Copyright 2018-2024 CERN
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,7 +15,6 @@
 // In applying this license, CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
-
 package timemanager
 
 import (
@@ -27,14 +26,18 @@ import (
 	"github.com/cs3org/reva/v2/pkg/storage/utils/decomposedfs/node"
 )
 
+// Manager is responsible for managing time-related attributes of nodes in a decomposed file system.
 type Manager struct {
 }
 
+// OverrideMtime overrides the modification time (mtime) attribute of a node with the given time.
 func (m *Manager) OverrideMtime(ctx context.Context, _ *node.Node, attrs *node.Attributes, mtime time.Time) error {
 	attrs.SetString(prefixes.MTimeAttr, mtime.UTC().Format(time.RFC3339Nano))
 	return nil
 }
 
+// MTime retrieves the modification time (mtime) attribute of a node.
+// If the attribute is not set, it falls back to the file's last modification time.
 func (dtm *Manager) MTime(ctx context.Context, n *node.Node) (time.Time, error) {
 	b, err := n.XattrString(ctx, prefixes.MTimeAttr)
 	if err != nil {
@@ -47,6 +50,8 @@ func (dtm *Manager) MTime(ctx context.Context, n *node.Node) (time.Time, error) 
 	return time.Parse(time.RFC3339Nano, b)
 }
 
+// SetMTime sets the modification time (mtime) attribute of a node to the given time.
+// If the time is nil, the attribute is removed.
 func (dtm *Manager) SetMTime(ctx context.Context, n *node.Node, mtime *time.Time) error {
 	if mtime == nil {
 		return n.RemoveXattr(ctx, prefixes.MTimeAttr, true)
@@ -54,6 +59,8 @@ func (dtm *Manager) SetMTime(ctx context.Context, n *node.Node, mtime *time.Time
 	return n.SetXattrString(ctx, prefixes.MTimeAttr, mtime.UTC().Format(time.RFC3339Nano))
 }
 
+// TMTime retrieves the tree modification time (tmtime) attribute of a node.
+// If the attribute is not set, it falls back to the node's modification time (mtime).
 func (dtm *Manager) TMTime(ctx context.Context, n *node.Node) (time.Time, error) {
 	b, err := n.XattrString(ctx, prefixes.TreeMTimeAttr)
 	if err == nil {
@@ -64,6 +71,8 @@ func (dtm *Manager) TMTime(ctx context.Context, n *node.Node) (time.Time, error)
 	return dtm.MTime(ctx, n)
 }
 
+// SetTMTime sets the tree modification time (tmtime) attribute of a node to the given time.
+// If the time is nil, the attribute is removed.
 func (dtm *Manager) SetTMTime(ctx context.Context, n *node.Node, tmtime *time.Time) error {
 	if tmtime == nil {
 		return n.RemoveXattr(ctx, prefixes.TreeMTimeAttr, true)
@@ -71,27 +80,35 @@ func (dtm *Manager) SetTMTime(ctx context.Context, n *node.Node, tmtime *time.Ti
 	return n.SetXattrString(ctx, prefixes.TreeMTimeAttr, tmtime.UTC().Format(time.RFC3339Nano))
 }
 
+// CTime retrieves the creation time (ctime) attribute of a node.
+// Since decomposedfs does not differentiate between ctime and mtime, it falls back to the node's modification time (mtime).
 func (dtm *Manager) CTime(ctx context.Context, n *node.Node) (time.Time, error) {
 	// decomposedfs does not differentiate between ctime and mtime
 	return dtm.MTime(ctx, n)
 }
 
+// SetCTime sets the creation time (ctime) attribute of a node to the given time.
+// Since decomposedfs does not differentiate between ctime and mtime, it sets the modification time (mtime) instead.
 func (dtm *Manager) SetCTime(ctx context.Context, n *node.Node, mtime *time.Time) error {
 	// decomposedfs does not differentiate between ctime and mtime
 	return dtm.SetMTime(ctx, n, mtime)
 }
 
+// TCTime retrieves the tree creation time (tctime) attribute of a node.
+// Since decomposedfs does not differentiate between ctime and mtime, it falls back to the tree modification time (tmtime).
 func (dtm *Manager) TCTime(ctx context.Context, n *node.Node) (time.Time, error) {
 	// decomposedfs does not differentiate between ctime and mtime
 	return dtm.TMTime(ctx, n)
 }
 
+// SetTCTime sets the tree creation time (tctime) attribute of a node to the given time.
+// Since decomposedfs does not differentiate between ctime and mtime, it sets the tree modification time (tmtime) instead.
 func (dtm *Manager) SetTCTime(ctx context.Context, n *node.Node, tmtime *time.Time) error {
 	// decomposedfs does not differentiate between ctime and mtime
 	return dtm.SetTMTime(ctx, n, tmtime)
 }
 
-// GetDTime reads the dtime from the extended attributes
+// DTime retrieves the deletion time (dtime) attribute of a node.
 func (dtm *Manager) DTime(ctx context.Context, n *node.Node) (tmTime time.Time, err error) {
 	b, err := n.XattrString(ctx, prefixes.DTimeAttr)
 	if err != nil {
@@ -100,7 +117,8 @@ func (dtm *Manager) DTime(ctx context.Context, n *node.Node) (tmTime time.Time, 
 	return time.Parse(time.RFC3339Nano, b)
 }
 
-// SetDTime writes the UTC dtime to the extended attributes or removes the attribute if nil is passed
+// SetDTime sets the deletion time (dtime) attribute of a node to the given time.
+// If the time is nil, the attribute is removed.
 func (dtm *Manager) SetDTime(ctx context.Context, n *node.Node, t *time.Time) (err error) {
 	if t == nil {
 		return n.RemoveXattr(ctx, prefixes.DTimeAttr, true)
