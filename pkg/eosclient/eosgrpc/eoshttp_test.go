287333f5662819160cf8f51ca3010ed320608c17
Jesse Geens <jgeens@cern.ch>
2024-10-04 09:07:21 +0200

    Implemented X-Disable-Versioning header to disable versioning on EOS
    This commit introduces a new header, `X-Disable-Versioning`, that is available on PUT requests. This header will disable versioning for this file save if the storage backend is EOS.
    
    Additionally, this commit introduces two unit tests related to this functionality:
    - TestDisableVersioningLeadsToCorrectQueryParams: test whether disabling versioning leads to correct query parameteers for the EOS HTTP / GRPC client
    - TestDisableVersioningHeaderPassedAlong: test whether a header passed to the initial endpoint is propagated to the actual upload endpoint
    
    Finally, this commit also fixes a bug that was already present, where
    the app was not passed along in the case of token-based authz

---
+++ pkg/eosclient/eosgrpc/eoshttp_test.go
@@ -0,0 +1,59 @@
+package eosgrpc
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/cs3org/reva/pkg/eosclient"
+)
+
+// Test that, when the PUTFile method is called with disableVersioning
+// set to true, the url for the EOS endpoint contains the right query param
+func TestDisableVersioningLeadsToCorrectQueryParams(t *testing.T) {
+
+	stream := io.NopCloser(strings.NewReader("Hello world!"))
+	length := int64(12)
+	app := "my-app"
+	urlpath := "/my-file.txt?queryparam=1"
+	token := "my-secret-token"
+
+	// Create fake HTTP server that acts as the EOS endpoint
+	calls := 0
+	mockServerUpload := httptest.NewServer(
+		http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				calls++
+				queryValues := r.URL.Query()
+				if queryValues.Get("eos.versioning") == "" {
+					t.Errorf("Query parameter eos.versioning not set")
+				}
+				if q := queryValues.Get("eos.versioning"); q != strconv.Itoa(0) {
+					t.Errorf("Query parameter eos.versioning set to wrong value; got %s, expected 0", q)
+				}
+			},
+		),
+	)
+
+	// Create EOS HTTP Client
+	// TODO: right now, expects files to be on the FS
+	client, err := NewEOSHTTPClient(&HTTPOptions{
+		BaseURL: mockServerUpload.URL,
+	})
+	if err != nil {
+		t.Errorf("Failed to construct client: %s", err.Error())
+	}
+
+	// Test actual PUTFile call
+	client.PUTFile(context.Background(), "remote-user", eosclient.Authorization{
+		Token: token}, urlpath, stream, length, app, true)
+
+	// If no connection was made to the EOS endpoint, something is wrong
+	if calls == 0 {
+		t.Errorf("EOS endpoint was not called. ")
+	}
+}
