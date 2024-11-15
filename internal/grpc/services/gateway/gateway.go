c71d70cbdadf6249f86fbc2b778c1a460b079b25
Giuseppe Lo Presti <giuseppe.lopresti@cern.ch>
2024-11-15 13:24:06 +0100

    Partial revert of PR #4911 (#4939)
    * Partial revert of PR #4911
    
    * changelog

--- internal/grpc/services/gateway/gateway.go
+++ internal/grpc/services/gateway/gateway.go
@@ -71,7 +71,6 @@ type config struct {
 	EtagCacheTTL        int                               `mapstructure:"etag_cache_ttl"`
 	AllowedUserAgents   map[string][]string               `mapstructure:"allowed_user_agents"` // map[path][]user-agent
 	CreateHomeCacheTTL  int                               `mapstructure:"create_home_cache_ttl"`
-	HomeLayout          string                            `mapstructure:"home_layout"`
 }
 
 // sets defaults.
@@ -111,11 +110,6 @@ func (c *config) ApplyDefaults() {
 	// lifetime for the transfer token (TUS upload)
 	if c.TransferExpires == 0 {
 		c.TransferExpires = 100 * 60 // seconds
-	}
-
-	// default to /home
-	if c.HomeLayout == "" {
-		c.HomeLayout = "/home"
 	}
 }
 
