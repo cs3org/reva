e6930f22361f7051cb9c3387167fecaac5237df8
André Duffeck <a.duffeck@opencloud.eu>
2025-02-12 08:32:42 +0100

    Add copyright headers

--- pkg/storage/fs/posix/blobstore/blobstore.go
+++ pkg/storage/fs/posix/blobstore/blobstore.go
@@ -1,4 +1,5 @@
 // Copyright 2018-2021 CERN
+// Copyright 2025 OpenCloud GmbH <mail@opencloud.eu>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
