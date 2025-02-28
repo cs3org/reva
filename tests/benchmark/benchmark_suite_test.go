67b8d722494c1025ec30c831f235e43ac26ae7a5
André Duffeck <a.duffeck@opencloud.eu>
2025-02-28 10:52:13 +0100

    Add basic fs benchmarks

---
+++ tests/benchmark/benchmark_suite_test.go
@@ -0,0 +1,13 @@
+package benchmark
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestBenchmark(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Benchmark Suite")
+}
