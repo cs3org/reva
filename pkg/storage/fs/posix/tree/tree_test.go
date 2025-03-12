dc8c69e8f11595a761cec7920a3d823ca7854d2c
Jörn Friedrich Dreyer <jfd@butonic.de>
2025-03-12 12:08:28 +0100

    revert test timeout
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

--- pkg/storage/fs/posix/tree/tree_test.go
+++ pkg/storage/fs/posix/tree/tree_test.go
@@ -81,7 +81,7 @@ var _ = Describe("Tree", func() {
 	)
 
 	BeforeEach(func() {
-		SetDefaultEventuallyTimeout(30 * time.Second)
+		SetDefaultEventuallyTimeout(15 * time.Second)
 
 		var err error
 		subtree, err = generateRandomString(10)
