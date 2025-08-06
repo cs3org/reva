d207f31638d899144f0293f0be8983d1236b696f
André Duffeck <a.duffeck@opencloud.eu>
2025-08-06 15:22:21 +0200

    Fix test

--- pkg/user/manager/demo/demo_test.go
+++ pkg/user/manager/demo/demo_test.go
@@ -37,7 +37,13 @@ func TestUserManager(t *testing.T) {
 	manager, _ := New(nil)
 
 	// setup test data
-	uidEinstein := &userpb.UserId{Idp: "http://localhost:9998", OpaqueId: "4c510ada-c86b-4815-8820-42cdf82c3d51", Type: userpb.UserType_USER_TYPE_PRIMARY}
+	uidEinstein := &userpb.UserId{
+		Idp:      "http://localhost:9998",
+		OpaqueId: "4c510ada-c86b-4815-8820-42cdf82c3d51",
+		TenantId: "c239389d-c249-499d-ae80-07558429769a",
+		Type:     userpb.UserType_USER_TYPE_PRIMARY,
+	}
+
 	userEinstein := &userpb.User{
 		Id:          uidEinstein,
 		Username:    "einstein",
