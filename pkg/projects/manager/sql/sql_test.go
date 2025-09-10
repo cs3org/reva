aa7df7a0859d775d19a5bbcf6dffc8d61c7342ce
Jesse Geens <jgeens@cern.ch>
2025-09-10 14:32:58 +0200

    fix unit tests

--- pkg/projects/manager/sql/sql_test.go
+++ pkg/projects/manager/sql/sql_test.go
@@ -97,6 +97,7 @@ func TestListProjects(t *testing.T) {
 						Path:          "/path/to/project",
 						PermissionSet: conversions.NewManagerRole().CS3ResourcePermissions(),
 					},
+					PermissionSet: conversions.NewManagerRole().CS3ResourcePermissions(),
 				},
 			},
 		},
@@ -130,6 +131,7 @@ func TestListProjects(t *testing.T) {
 						Path:          "/path/to/project",
 						PermissionSet: conversions.NewViewerRole().CS3ResourcePermissions(),
 					},
+					PermissionSet: conversions.NewViewerRole().CS3ResourcePermissions(),
 				},
 			},
 		},
@@ -163,6 +165,7 @@ func TestListProjects(t *testing.T) {
 						Path:          "/path/to/project",
 						PermissionSet: conversions.NewEditorRole().CS3ResourcePermissions(),
 					},
+					PermissionSet: conversions.NewEditorRole().CS3ResourcePermissions(),
 				},
 			},
 		},
@@ -196,6 +199,7 @@ func TestListProjects(t *testing.T) {
 						Path:          "/path/to/project",
 						PermissionSet: conversions.NewManagerRole().CS3ResourcePermissions(),
 					},
+					PermissionSet: conversions.NewManagerRole().CS3ResourcePermissions(),
 				},
 			},
 		},
@@ -229,6 +233,7 @@ func TestListProjects(t *testing.T) {
 						Path:          "/path/to/project",
 						PermissionSet: conversions.NewManagerRole().CS3ResourcePermissions(),
 					},
+					PermissionSet: conversions.NewManagerRole().CS3ResourcePermissions(),
 				},
 			},
 		},
@@ -275,7 +280,7 @@ func TestListProjects(t *testing.T) {
 			}
 
 			if !reflect.DeepEqual(got.StorageSpaces, tt.expected) {
-				t.Fatalf("projects' list do not match. got=%#v expected=%#v", got.StorageSpaces, tt.expected)
+				t.Fatalf("projects' list do not match. got=%+v expected=%+v", got.StorageSpaces, tt.expected)
 			}
 
 			err = teardown(t)
