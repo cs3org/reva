a3790ae98082e8a29f341451c9bb084d965febbf
Rasmus Welander <rasmus.oscar.welander@cern.ch>
2025-09-25 16:19:31 +0200

    WIP

--- pkg/share/manager/sql/conversions.go
+++ pkg/share/manager/sql/conversions.go
@@ -30,96 +30,38 @@ import (
 	types "github.com/cs3org/go-cs3apis/cs3/types/v1beta1"
 	"github.com/cs3org/reva/v3/internal/http/services/owncloud/ocs/conversions"
 	"github.com/cs3org/reva/v3/pkg/ocm/share"
+	model "github.com/cs3org/reva/v3/pkg/share/manager/sql/model"
 )
 
-// ShareType is the type of the share.
-type ShareType int
-
-// ItemType is the type of the shares resource.
-type ItemType int
-
-// AccessMethod is method granted by the sharer to access
-// the shared resource.
-type AccessMethod int
-
-// Protocol is the protocol the recipient of the share
-// uses to access the shared resource.
-type Protocol int
-
-// ShareState is the state of the share.
-type ShareState int
-
-const (
-	// ShareTypeUser is used for a share to an user.
-	ShareTypeUser ShareType = iota
-	// ShareTypeGroup is used for a share to a group.
-	ShareTypeGroup
-)
-
-const (
-	// ShareStatePending is the state for a pending share.
-	ShareStatePending ShareState = iota
-	// ShareStateAccepted is the share for an accepted share.
-	ShareStateAccepted
-	// ShareStateRejected is the share for a rejected share.
-	ShareStateRejected
-)
-
-const (
-	// WebDAVAccessMethod indicates an access using WebDAV to the share.
-	WebDAVAccessMethod AccessMethod = iota
-	// WebappAccessMethod indicates an access using a collaborative
-	// application to the share.
-	WebappAccessMethod
-	// TransferAccessMethod indicates a share for a transfer.
-	TransferAccessMethod
-)
-
-const (
-	// WebDAVProtocol is the WebDav protocol.
-	WebDAVProtocol Protocol = iota
-	// WebappProtocol is the Webapp protocol.
-	WebappProtocol
-	// TransferProtocol is the Transfer protocol.
-	TransferProtocol
-)
-
-const (
-	// ItemTypeFile is used when the shared resource is a file.
-	ItemTypeFile ItemType = iota
-	// ItemTypeFolder is used when the shared resource is a folder.
-	ItemTypeFolder
-)
-
-func convertFromCS3OCMShareType(shareType ocm.ShareType) ShareType {
+func convertFromCS3OCMShareType(shareType ocm.ShareType) model.ShareType {
 	switch shareType {
 	case ocm.ShareType_SHARE_TYPE_USER:
-		return ShareTypeUser
+		return model.ShareTypeUser
 	case ocm.ShareType_SHARE_TYPE_GROUP:
-		return ShareTypeGroup
+		return model.ShareTypeGroup
 	}
 	return -1
 }
 
-func convertFromCS3OCMShareState(shareState ocm.ShareState) ShareState {
+func convertFromCS3OCMShareState(shareState ocm.ShareState) model.OcmShareState {
 	switch shareState {
 	case ocm.ShareState_SHARE_STATE_ACCEPTED:
-		return ShareStateAccepted
+		return model.ShareStateAccepted
 	case ocm.ShareState_SHARE_STATE_PENDING:
-		return ShareStatePending
+		return model.ShareStatePending
 	case ocm.ShareState_SHARE_STATE_REJECTED:
-		return ShareStateRejected
+		return model.ShareStateRejected
 	}
 	return -1
 }
 
-func convertToCS3OCMShareState(state ShareState) ocm.ShareState {
+func convertToCS3OCMShareState(state model.OcmShareState) ocm.ShareState {
 	switch state {
-	case ShareStateAccepted:
+	case model.ShareStateAccepted:
 		return ocm.ShareState_SHARE_STATE_ACCEPTED
-	case ShareStatePending:
+	case model.ShareStatePending:
 		return ocm.ShareState_SHARE_STATE_PENDING
-	case ShareStateRejected:
+	case model.ShareStateRejected:
 		return ocm.ShareState_SHARE_STATE_REJECTED
 	}
 	return ocm.ShareState_SHARE_STATE_INVALID
@@ -137,12 +79,12 @@ type dbShare struct {
 	Ctime      int
 	Mtime      int
 	Expiration sql.NullInt64
-	ShareType  ShareType
+	ShareType  model.ShareType
 }
 
 type dbAccessMethod struct {
 	ShareID           string
-	Type              AccessMethod
+	Type              model.AccessMethod
 	WebDAVPermissions *int
 	WebAppViewMode    *int
 }
@@ -151,21 +93,21 @@ type dbReceivedShare struct {
 	ID            int
 	Name          string
 	RemoteShareID string
-	ItemType      ItemType
+	ItemType      model.OcmItemType
 	ShareWith     string
 	Owner         string
 	Initiator     string
 	Ctime         int
 	Mtime         int
 	Expiration    sql.NullInt64
-	Type          ShareType
-	State         ShareState
+	Type          model.ShareType
+	State         model.OcmShareState
 }
 
 type dbProtocol struct {
 	ID                   string
 	ShareID              string
-	Type                 Protocol
+	Type                 model.Protocol
 	WebDAVURI            *string
 	WebDAVSharedSecret   *string
 	WebDavPermissions    *int
@@ -188,13 +130,13 @@ func convertFederatedUserID(s string) *userpb.UserId {
 	}
 }
 
-func convertToCS3OCMShare(s *dbShare, am []*ocm.AccessMethod) *ocm.Share {
+func convertToCS3OCMShare(s *model.OcmShare, am []*ocm.AccessMethod) *ocm.Share {
 	share := &ocm.Share{
 		Id: &ocm.ShareId{
-			OpaqueId: strconv.Itoa(s.ID),
+			OpaqueId: strconv.Itoa(int(s.Id)),
 		},
 		ResourceId: &provider.ResourceId{
-			StorageId: s.Prefix,
+			StorageId: s.FileidPrefix,
 			OpaqueId:  s.ItemSource,
 		},
 		Name:  s.Name,
@@ -228,12 +170,12 @@ func convertToCS3OCMShare(s *dbShare, am []*ocm.AccessMethod) *ocm.Share {
 	return share
 }
 
-func convertToCS3OCMReceivedShare(s *dbReceivedShare, p []*ocm.Protocol) *ocm.ReceivedShare {
+func convertToCS3OCMReceivedShare(s *model.OcmReceivedShare, p []*ocm.Protocol) *ocm.ReceivedShare {
 	share := &ocm.ReceivedShare{
 		Id: &ocm.ShareId{
-			OpaqueId: strconv.Itoa(s.ID),
+			OpaqueId: strconv.Itoa(s.RemoteShareID),
 		},
-		RemoteShareId: s.RemoteShareID,
+		RemoteShareId: strconv.Itoa(s.RemoteShareID),
 		Name:          s.Name,
 		Grantee: &provider.Grantee{
 			Type: provider.GranteeType_GRANTEE_TYPE_USER,
@@ -264,50 +206,50 @@ func convertToCS3OCMReceivedShare(s *dbReceivedShare, p []*ocm.Protocol) *ocm.Re
 	return share
 }
 
-func convertToCS3AccessMethod(m *dbAccessMethod) *ocm.AccessMethod {
+func convertToCS3AccessMethod(m *model.OcmSharesAccessMethod) *ocm.AccessMethod {
 	switch m.Type {
-	case WebDAVAccessMethod:
+	case model.WebDAVAccessMethod:
 		return share.NewWebDavAccessMethod(
-			conversions.RoleFromOCSPermissions(conversions.Permissions(*m.WebDAVPermissions)).CS3ResourcePermissions(),
+			conversions.RoleFromOCSPermissions(conversions.Permissions(*&m.Permissions)).CS3ResourcePermissions(),
 			[]string{}) // TODO persist requirements
-	case WebappAccessMethod:
-		return share.NewWebappAccessMethod(appprovider.ViewMode(*m.WebAppViewMode))
-	case TransferAccessMethod:
+	case model.WebappAccessMethod:
+		return share.NewWebappAccessMethod(appprovider.ViewMode(*&m.ViewMode))
+	case model.TransferAccessMethod:
 		return share.NewTransferAccessMethod()
 	}
 	return nil
 }
 
-func convertToCS3Protocol(p *dbProtocol) *ocm.Protocol {
+func convertToCS3Protocol(p *model.OcmReceivedShareProtocol) *ocm.Protocol {
 	switch p.Type {
-	case WebDAVProtocol:
-		return share.NewWebDAVProtocol(*p.WebDAVURI, *p.WebDAVSharedSecret, &ocm.SharePermissions{
-			Permissions: conversions.RoleFromOCSPermissions(conversions.Permissions(*p.WebDavPermissions)).CS3ResourcePermissions(),
+	case model.WebDAVProtocol:
+		return share.NewWebDAVProtocol(p.Uri, p.SharedSecret, &ocm.SharePermissions{
+			Permissions: conversions.RoleFromOCSPermissions(conversions.Permissions(p.Permissions)).CS3ResourcePermissions(),
 		}, []string{}) // TODO persist requirements
-	case WebappProtocol:
-		return share.NewWebappProtocol(*p.WebappURI, appprovider.ViewMode(*p.WebappViewMode))
-	case TransferProtocol:
-		return share.NewTransferProtocol(*p.TransferSourceURI, *p.TransferSharedSecret, uint64(*p.TransferSize))
+	case model.WebappProtocol:
+		return share.NewWebappProtocol(p.Uri, appprovider.ViewMode(p.Permissions))
+	case model.TransferProtocol:
+		return share.NewTransferProtocol(p.Uri, p.SharedSecret, uint64(p.Size))
 	}
 	return nil
 }
 
-func convertToCS3ResourceType(t ItemType) provider.ResourceType {
+func convertToCS3ResourceType(t model.OcmItemType) provider.ResourceType {
 	switch t {
-	case ItemTypeFile:
+	case model.OcmItemTypeFile:
 		return provider.ResourceType_RESOURCE_TYPE_FILE
-	case ItemTypeFolder:
+	case model.OcmItemTypeFolder:
 		return provider.ResourceType_RESOURCE_TYPE_CONTAINER
 	}
 	return provider.ResourceType_RESOURCE_TYPE_INVALID
 }
 
-func convertFromCS3ResourceType(t provider.ResourceType) ItemType {
+func convertFromCS3ResourceType(t provider.ResourceType) model.OcmItemType {
 	switch t {
 	case provider.ResourceType_RESOURCE_TYPE_FILE:
-		return ItemTypeFile
+		return model.OcmItemTypeFile
 	case provider.ResourceType_RESOURCE_TYPE_CONTAINER:
-		return ItemTypeFolder
+		return model.OcmItemTypeFolder
 	}
 	return -1
 }
