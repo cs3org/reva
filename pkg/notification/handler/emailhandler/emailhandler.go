89df1683f04ae362aa4c096aa7940538ab5b18be
Jesse Geens <jgeens@cern.ch>
2025-12-16 09:40:05 +0100

    add support for CID in emailhandler

--- pkg/notification/handler/emailhandler/emailhandler.go
+++ pkg/notification/handler/emailhandler/emailhandler.go
@@ -21,15 +21,20 @@ package emailhandler
 import (
 	"context"
 	"fmt"
-	"net/smtp"
-	"regexp"
+	"os"
+	"path"
+	"strconv"
 	"strings"
+
+	stdmail "net/mail"
 
 	"github.com/cs3org/reva/v3/pkg/appctx"
 	"github.com/cs3org/reva/v3/pkg/notification/handler"
 	"github.com/cs3org/reva/v3/pkg/notification/handler/registry"
 	"github.com/cs3org/reva/v3/pkg/utils/cfg"
+	"github.com/pkg/errors"
 	"github.com/rs/zerolog"
+	"gopkg.in/mail.v2"
 )
 
 func init() {
@@ -40,6 +45,10 @@ func init() {
 type EmailHandler struct {
 	conf *config
 	log  *zerolog.Logger
+	// Paths of CID files to be embedded, relative to the CIDFolder
+	// so in principle, should always be just the filename
+	CIDRelPaths []string
+	CIDFolder   string
 }
 
 type config struct {
@@ -48,11 +57,15 @@ type config struct {
 	SenderPassword string `docs:";The sender's password."                                                    mapstructure:"sender_password"`
 	DisableAuth    bool   `docs:"false;Whether to disable SMTP auth."                                        mapstructure:"disable_auth"`
 	DefaultSender  string `docs:"no-reply@cernbox.cern.ch;Default sender when not specified in the trigger." mapstructure:"default_sender"`
+	CIDFolder      string `docs:"/etc/revad/cid/;Folder on the local filesystem that includes files to be embedded as CIDs in emails." mapstructure:"cid_folder"`
 }
 
 func (c *config) ApplyDefaults() {
 	if c.DefaultSender == "" {
 		c.DefaultSender = "no-reply@cernbox.cern.ch"
+	}
+	if c.CIDFolder == "" {
+		c.CIDFolder = "/etc/revad/cid/"
 	}
 }
 
@@ -64,56 +77,102 @@ func New(ctx context.Context, m map[string]any) (handler.Handler, error) {
 	}
 
 	log := appctx.GetLogger(ctx)
-	return &EmailHandler{
+	handler := &EmailHandler{
 		conf: &c,
 		log:  log,
-	}, nil
+	}
+
+	if len(strings.Split(c.SMTPAddress, ":")) != 2 {
+		return nil, fmt.Errorf("Invalid SMTP address: %s, must be in the format host:port", c.SMTPAddress)
+	}
+
+	// Find CID files that we can embed in emails
+	handler.CIDFolder = c.CIDFolder
+	files, err := listFilenames(c.CIDFolder)
+	if err != nil {
+		log.Error().Err(err).Msgf("Failed to list files in CID folder %s, continuing without embedding anything!", c.CIDFolder)
+	} else {
+		log.Info().Msgf("Registered %d files from the CID folder with the email handler", len(files))
+		handler.CIDRelPaths = files
+	}
+	return handler, nil
+
 }
 
-// Send is the method run when a notification is triggered for this handler.
+// Find the files in the given CID Folder
+func listFilenames(dir string) ([]string, error) {
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	files := make([]string, 0, len(entries))
+	for _, e := range entries {
+		files = append(files, e.Name())
+	}
+	return files, nil
+}
+
+// SendEmail, obviously, sends an email.
+// CIDPaths are files that are to be embedded in the email.
+// They can be referenced in the body using e.g. `<img src="cid:file.png">`
+// Note that paths specified here must be relative to the asset directory!
 func (e *EmailHandler) Send(sender, recipient, subject, body string) error {
-	if sender == "" {
+	if strings.TrimSpace(sender) == "" {
 		sender = e.conf.DefaultSender
 	}
+	if strings.TrimSpace(recipient) == "" {
+		return errors.New("recipient address cannot be empty")
+	}
 
-	msg := e.generateMsg(sender, recipient, subject, body)
-	err := smtp.SendMail(e.conf.SMTPAddress, e.getAuth(), sender, []string{recipient}, msg)
+	// Validate email address format
+	if _, err := stdmail.ParseAddress(sender); err != nil {
+		sender = e.conf.DefaultSender
+	}
+	if _, err := stdmail.ParseAddress(recipient); err != nil {
+		return errors.Wrapf(err, "invalid recipient address format: %s", recipient)
+	}
+
+	message := mail.NewMessage()
+	message.SetHeader("From", sender)
+	message.SetHeader("To", recipient)
+	message.SetHeader("Subject", subject)
+
+	// Embed CID files with error handling
+	for _, cid := range e.CIDRelPaths {
+		if strings.Contains(body, cid) {
+			fullPath := path.Join(e.CIDFolder, cid)
+			message.Embed(fullPath)
+		}
+	}
+
+	message.SetBody("text/html", body)
+
+	// Parse SMTP address
+	splitAddress := strings.SplitN(e.conf.SMTPAddress, ":", 2)
+	if len(splitAddress) != 2 {
+		return errors.New("invalid SMTP address format: expected host:port")
+	}
+
+	host, portStr := splitAddress[0], splitAddress[1]
+	port, err := strconv.Atoi(portStr)
 	if err != nil {
-		return err
+		return errors.Wrapf(err, "invalid SMTP port: %s", portStr)
 	}
 
-	e.log.Debug().Msgf("mail sent to recipient %s", recipient)
-
-	return nil
-}
-
-func (e *EmailHandler) getAuth() smtp.Auth {
+	var d *mail.Dialer
 	if e.conf.DisableAuth {
-		return nil
+		d = &mail.Dialer{
+			Host: host,
+			Port: port,
+		}
+	} else {
+		d = &mail.Dialer{
+			Host:     host,
+			Port:     port,
+			Username: e.conf.SenderLogin,
+			Password: e.conf.SenderPassword,
+		}
 	}
 
-	return smtp.PlainAuth("", e.conf.SenderLogin, e.conf.SenderPassword, strings.SplitN(e.conf.SMTPAddress, ":", 2)[0])
-}
-
-func (e *EmailHandler) generateMsg(from, to, subject, body string) []byte {
-	re := regexp.MustCompile(`\r?\n`)
-	cleanSubject := re.ReplaceAllString(strings.TrimSpace(subject), " ")
-	headers := []string{
-		fmt.Sprintf("From: %s", from),
-		fmt.Sprintf("To: %s", to),
-		fmt.Sprintf("Subject: %s", cleanSubject),
-		"MIME-version: 1.0;",
-		"Content-Type: text/html; charset=\"UTF-8\";",
-	}
-
-	var sb strings.Builder
-
-	for _, h := range headers {
-		sb.WriteString(h)
-		sb.WriteString("\r\n")
-	}
-	sb.WriteString("\r\n")
-	sb.WriteString(body)
-
-	return []byte(sb.String())
+	return d.DialAndSend(message)
 }
