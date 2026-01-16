189dc9cc0edd2d320797dca74ba0ab6906cca833
Jesse Geens <jgeens@cern.ch>
2026-01-16 16:11:35 +0100

    implement support for EOS ownerless trashbin

--- pkg/eosclient/eosclient.go
+++ pkg/eosclient/eosclient.go
@@ -54,7 +54,7 @@ type EOSClient interface {
 	ListWithRegex(ctx context.Context, auth Authorization, path string, depth uint, regex string) ([]*FileInfo, error)
 	Read(ctx context.Context, auth Authorization, path string, ranges []storage.Range) (io.ReadCloser, error)
 	Write(ctx context.Context, auth Authorization, path string, stream io.ReadCloser, length int64, app string, disableVersioning bool) error
-	ListDeletedEntries(ctx context.Context, auth Authorization, maxentries int, from, to time.Time) ([]*DeletedEntry, error)
+	ListDeletedEntries(ctx context.Context, auth Authorization, recycleid string, maxentries int, from, to time.Time) ([]*DeletedEntry, error)
 	RestoreDeletedEntry(ctx context.Context, auth Authorization, key string) error
 	PurgeDeletedEntries(ctx context.Context, auth Authorization) error
 	ListVersions(ctx context.Context, auth Authorization, p string) ([]*FileInfo, error)
