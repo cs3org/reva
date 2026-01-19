a9e8f9179815aa88b48776b5ee5461d2f2c2cb64
Jesse Geens <jgeens@cern.ch>
2026-01-19 15:43:17 +0100

    write migration script for favorites db

--- pkg/favorite/sql/sql.go
+++ pkg/favorite/sql/sql.go
@@ -1,4 +1,4 @@
-// Copyright 2018-2024 CERN
+// Copyright 2018-2026 CERN
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-package cbox
+package sql
 
 import (
 	"context"
