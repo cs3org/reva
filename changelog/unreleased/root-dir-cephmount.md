838e5519433825f94d3d7d3b7aea98063582a349
Rasmus Oscar Welander <rasmus.oscar.welander@cern.ch>
2026-02-12 13:24:11 +0100

    cephmount: add root dir config option

---
+++ changelog/unreleased/root-dir-cephmount.md
@@ -0,0 +1,6 @@
+Enhancement: Cephmount add root dir config option
+
+* The `root_dir` config option is there to append a path on the current path that should be
+mounted by reva 
+
+https://github.com/cs3org/reva/pull/5503
--- pkg/storage/fs/cephmount/options.go
+++ pkg/storage/fs/cephmount/options.go
@@ -31,6 +31,7 @@ type Options struct {
 
 	// Simplified Ceph configuration - just paste the fstab entry
 	FstabEntry string `mapstructure:"fstabentry"` // Complete fstab line for Ceph mount
+	RootDir    string `mapstructure:"root_dir"`   // Root directory for the Ceph mount
 
 	// Testing-only option - allows running without Ceph configuration for local filesystem tests
 	TestingAllowLocalMode bool `mapstructure:"testing_allow_local_mode"` // Bypass fstab parsing requirement for tests only
