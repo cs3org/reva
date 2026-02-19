b3cd62189cab394186ca01d79d1999cb2221b4cd
Giuseppe Lo Presti <giuseppe.lopresti@cern.ch>
2026-02-19 09:43:08 +0100

    Revert "Do not clean namespace path; this should end in a `/` so that trashbin paths do not match for projects"
    This reverts commit 2e3e192c308c1ba4b636a19d94c608229778a52a.

--- pkg/storage/utils/eosfs/eosfs.go
+++ pkg/storage/utils/eosfs/eosfs.go
@@ -81,6 +81,7 @@ var hiddenReg = regexp.MustCompile(`\.sys\..#.`)
 var eosLockReg = regexp.MustCompile(`expires:\d+,type:[a-z]+,owner:.+:.+`)
 
 func (c *Config) ApplyDefaults() {
+	c.Namespace = path.Clean(c.Namespace)
 	if !strings.HasPrefix(c.Namespace, "/") {
 		c.Namespace = "/"
 	}
