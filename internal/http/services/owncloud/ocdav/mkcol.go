f6e83b5cd7ca1caca7131330a03e635f6583f44d
btwshivam <shivam.devline@gmail.com>
2026-08-06 05:09:43 +0530

    feat(ocdav): return fileid and etag in MKCOL response
    Signed-off-by: btwshivam <shivam.devline@gmail.com>

--- internal/http/services/owncloud/ocdav/mkcol.go
+++ internal/http/services/owncloud/ocdav/mkcol.go
@@ -28,6 +28,7 @@ import (
 	rpc "github.com/cs3org/go-cs3apis/cs3/rpc/v1beta1"
 	provider "github.com/cs3org/go-cs3apis/cs3/storage/provider/v1beta1"
 	"github.com/cs3org/reva/v3/pkg/appctx"
+	"github.com/cs3org/reva/v3/pkg/spaces"
 	"github.com/rs/zerolog"
 )
 
@@ -153,6 +154,15 @@ func (s *svc) handleMkcol(ctx context.Context, w http.ResponseWriter, r *http.Re
 	}
 	switch res.Status.Code {
 	case rpc.Code_CODE_OK:
+		// CreateContainer does not return the resource info, stat the new collection to expose its fileid and etag
+		createdStatRes, err := client.Stat(ctx, &provider.StatRequest{Ref: childRef})
+		if err == nil && createdStatRes.Status.Code == rpc.Code_CODE_OK {
+			w.Header().Set(HeaderOCFileID, spaces.EncodeToStringifiedResourceID(createdStatRes.Info.Id))
+			w.Header().Set(HeaderOCETag, createdStatRes.Info.Etag)
+		} else {
+			// the collection was created, return 201 with the headers missing
+			log.Warn().Err(err).Str("path", childRef.Path).Interface("status", createdStatRes.GetStatus()).Msg("error statting created collection")
+		}
 		w.WriteHeader(http.StatusCreated)
 	case rpc.Code_CODE_NOT_FOUND:
 		log.Debug().Str("path", childRef.Path).Interface("status", statRes.Status).Msg("conflict")
